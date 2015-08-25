9c612372893d6293b3484a41978bc1c49f835e38
Bart Vanbrabant <bart@vanbrabant.eu>
2015-08-25 09:47:42 +0200

    Add now to the parameter list to calculate the expire

--- src/impera/server.py
+++ src/impera/server.py
@@ -293,7 +293,7 @@ class Server(protocol.ServerEndpoint):
             del d["value"]
             return_value.append(d)
 
-        return 200, {"parameters": return_value, "expire": self._fact_expire}
+        return 200, {"parameters": return_value, "expire": self._fact_expire, "now": datetime.datetime.now()}
 
     @protocol.handle(methods.FileMethod.upload_file)
     def upload_file(self, id, content):
