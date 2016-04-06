597de71e40406ed693b821e336110a265d51686b
Bart Vanbrabant <bart@vanbrabant.eu>
2016-04-06 11:55:40 +0200

    Fix unit tests

--- tests/project.py
+++ tests/project.py
@@ -15,9 +15,10 @@
 
     Contact: bart@impera.io
 """
+
 from impera import protocol
 from server_test import ServerTest
-from nose.tools import assert_equal, assert_true
+from nose.tools import assert_equal, assert_true, assert_in
 
 
 class testRestServer(ServerTest):
@@ -27,101 +28,112 @@ class testRestServer(ServerTest):
 
     def setUp(self):
         ServerTest.setUp(self)
-
         # start the client
         self.client = protocol.Client("client", "client")
 
     def tearDown(self):
-        ServerTest.setUp(self)
+        ServerTest.tearDown(self)
 
     def test_project_api(self):
         result = self.client.create_project("project-test")
         assert_equal(result.code, 200)
-        assert_true("id" in result.result)
+        assert_in("project", result.result)
+        assert_in("id", result.result["project"])
 
-        project_id = result.result["id"]
+        project_id = result.result["project"]["id"]
 
         result = self.client.create_project("project-test")
         assert_equal(result.code, 500)
 
         result = self.client.list_projects()
         assert_equal(result.code, 200)
-        assert_equal(len(result.result), 1)
+        assert_in("projects", result.result)
+        assert_equal(len(result.result["projects"]), 1)
 
-        assert_equal(result.result[0]['id'], project_id)
+        assert_equal(result.result["projects"][0]['id'], project_id)
 
         result = self.client.get_project(id=project_id)
         assert_equal(result.code, 200)
-        assert_equal(result.result['id'], project_id)
-        assert_equal(result.result['name'], "project-test")
+        assert_in("project", result.result)
+        assert_equal(result.result["project"]['id'], project_id)
+        assert_equal(result.result["project"]['name'], "project-test")
 
         result = self.client.modify_project(id=project_id, name="project-test2")
         assert_equal(result.code, 200)
-        assert_equal(result.result['id'], project_id)
-        assert_equal(result.result['name'], "project-test2")
+        assert_in("project", result.result)
+        assert_equal(result.result["project"]['id'], project_id)
+        assert_equal(result.result["project"]['name'], "project-test2")
 
         result = self.client.get_project(id=project_id)
         assert_equal(result.code, 200)
-        assert_equal(result.result['id'], project_id)
-        assert_equal(result.result['name'], "project-test2")
+        assert_in("project", result.result)
+        assert_equal(result.result["project"]['id'], project_id)
+        assert_equal(result.result["project"]['name'], "project-test2")
 
         result = self.client.delete_project(id=project_id)
         assert_equal(result.code, 200)
 
         result = self.client.list_projects()
         assert_equal(result.code, 200)
-        assert_equal(len(result.result), 0)
+        assert_in("projects", result.result)
+        assert_equal(len(result.result["projects"]), 0)
 
-#     def test_env_api(self):
-#         result = self.client.create_project("env-test")
-#         assert_equal(result.code, 200)
-#         assert_true("id" in result.result)
-#         project_id = result.result["id"]
-# 
-#         result = self.client.create_environment(project_id=project_id, name="dev")
-#         assert_equal(result.code, 200)
-#         assert_true("id" in result.result)
-#         assert_true("project" in result.result)
-#         assert_equal(project_id, result.result["project"])
-#         assert_equal("dev", result.result["name"])
-# 
-#         env_id = result.result["id"]
-# 
-#         result = self.client.modify_environment(id=env_id, name="dev2")
-#         assert_equal(result.code, 200)
-#         assert_equal(result.result['id'], env_id)
-#         assert_equal(result.result['name'], "dev2")
-# 
-#         result = self.client.get_environment(id=env_id)
-#         assert_equal(result.code, 200)
-#         assert_equal(result.result['id'], env_id)
-#         assert_equal(result.result['project'], project_id)
-#         assert_equal(result.result['name'], "dev2")
-# 
-#         project_result = self.client.get_project(id=project_id)
-#         assert_equal(project_result.code, 200)
-#         assert_true(env_id in project_result.result['environments'])
-# 
-#         result = self.client.list_environments()
-#         assert_equal(result.code, 200)
-#         assert_equal(len(result.result), 1)
-# 
-#         result = self.client.delete_environment(id=env_id)
-#         assert_equal(result.code, 200)
-# 
-#         result = self.client.list_environments()
-#         assert_equal(result.code, 200)
-#         assert_equal(len(result.result), 0)
-# 
-#     def test_project_cascade(self):
-#         result = self.client.create_project("env-test")
-#         project_id = result.result["id"]
-# 
-#         result = self.client.create_environment(project_id=project_id, name="dev")
-#         result = self.client.create_environment(project_id=project_id, name="prod")
-# 
-#         result = self.client.delete_project(project_id)
-#         assert_equal(result.code, 200)
-# 
-#         result = self.client.list_environments()
-#         assert_equal(len(result.result), 0)
+    def test_env_api(self):
+        result = self.client.create_project("env-test")
+        assert_equal(result.code, 200)
+        assert_in("project", result.result)
+        assert_in("id", result.result["project"])
+        project_id = result.result["project"]["id"]
+
+        result = self.client.create_environment(project_id=project_id, name="dev")
+        assert_equal(result.code, 200)
+        assert_in("environment", result.result)
+        assert_in("id", result.result["environment"])
+        assert_in("project", result.result["environment"])
+        assert_equal(project_id, result.result["environment"]["project"])
+        assert_equal("dev", result.result["environment"]["name"])
+
+        env_id = result.result["environment"]["id"]
+
+        result = self.client.modify_environment(id=env_id, name="dev2")
+        assert_equal(result.code, 200)
+        assert_in("environment", result.result)
+        assert_equal(result.result["environment"]['id'], env_id)
+        assert_equal(result.result["environment"]['name'], "dev2")
+
+        result = self.client.get_environment(id=env_id)
+        assert_equal(result.code, 200)
+        assert_in("environment", result.result)
+        assert_equal(result.result["environment"]['id'], env_id)
+        assert_equal(result.result["environment"]['project'], project_id)
+        assert_equal(result.result["environment"]['name'], "dev2")
+
+        project_result = self.client.get_project(id=project_id)
+        assert_equal(project_result.code, 200)
+        assert_in("project", project_result.result)
+        assert_in(env_id, project_result.result["project"]["environments"])
+
+        result = self.client.list_environments()
+        assert_equal(result.code, 200)
+        assert_equal(len(result.result), 1)
+
+        result = self.client.delete_environment(id=env_id)
+        assert_equal(result.code, 200)
+
+        result = self.client.list_environments()
+        assert_equal(result.code, 200)
+        assert_in("environments", result.result)
+        assert_equal(len(result.result["environments"]), 0)
+
+    def test_project_cascade(self):
+        result = self.client.create_project("env-test")
+        project_id = result.result["project"]["id"]
+
+        result = self.client.create_environment(project_id=project_id, name="dev")
+        result = self.client.create_environment(project_id=project_id, name="prod")
+
+        result = self.client.delete_project(project_id)
+        assert_equal(result.code, 200)
+
+        result = self.client.list_environments()
+        assert_equal(len(result.result["environments"]), 0)
--- tests/server_test.py
+++ tests/server_test.py
@@ -20,36 +20,67 @@ import os
 import tempfile
 from concurrent import futures
 import shutil
+from http import client
+import time
+import logging
 
 
 from mongobox.unittest import MongoTestCase
 from impera import config
 from impera.server import Server
 
+LOGGER = logging.getLogger(__name__)
+
 
 class ServerTest(MongoTestCase):
+    state_dir = None
+    server_future = None
+    server = None
+
     def __init__(self, methodName='runTest'):
         super().__init__(methodName)
-        self.state_dir = None
-        self.server_future = None
-        self.server = None
 
-    def setUp(self):
-        self.state_dir = tempfile.mkdtemp()
+    @classmethod
+    def setUpClass(cls):
+        if cls.server is not None:
+            LOGGER.error("Server is already started")
+            return
+
+        cls.state_dir = tempfile.mkdtemp()
         config.Config.load_config()
-        config.Config.set("config", "state-dir", self.state_dir)
+        config.Config.set("config", "state-dir", cls.state_dir)
 
+        LOGGER.info("Starting server")
         mongo_port = os.getenv('MONGOBOX_PORT')
         if mongo_port is None:
             raise Exception("MONGOBOX_PORT env variable not available. Make sure test are executed with --with-mongobox")
 
-        self.server = Server(database_host="localhost", database_port=int(mongo_port))
+        cls.server = Server(database_host="localhost", database_port=int(mongo_port))
+        executor = futures.ThreadPoolExecutor(max_workers=1)
+        cls.server_future = executor.submit(cls.server.start)
 
-        executor = futures.ThreadPoolExecutor(max_workers=2)
-        self.server_future = executor.submit(self.server.start)
+        attempts = 10
+        while attempts > 0:
+            attempts -= 1
+            try:
+                LOGGER.info("Waiting for server to become availabe")
+                conn = client.HTTPConnection("localhost", 8888)
+                conn.request("GET", "/")
+                res = conn.getresponse()
+
+                if res.status == 404:
+                    return
+
+            except ConnectionRefusedError:
+                time.sleep(0.1)
 
     def tearDown(self):
-        self.server.stop()
-        self.server_future.result()
         self.purge_database()
-        shutil.rmtree(self.state_dir)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.stop()
+        cls.server_future.result()
+        shutil.rmtree(cls.state_dir)
+        cls.server = None
+        cls.server_future = None
