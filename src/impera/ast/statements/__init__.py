b08d5ee47b1ee7bf895221646606e67028a350d2
Bart Vanbrabant <bart@vanbrabant.eu>
2016-04-25 11:42:02 +0200

    Update license header with new company name, year and contact

--- src/impera/ast/statements/__init__.py
+++ src/impera/ast/statements/__init__.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2015 Impera
+    Copyright 2016 Inmanta
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 
-    Contact: bart@impera.io
+    Contact: code@inmanta.com
 """
 from impera.execute.runtime import ResultVariable, ExecutionUnit
 
