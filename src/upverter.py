1f467b7a29264cd877f70c0e94881a1bf4b29707
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2016-05-30 16:28:39 +0200

    pep8-ify

--- src/upverter.py
+++ src/upverter.py
@@ -1,11 +1,24 @@
-'''
-Created on May 30, 2016
+"""
+    Copyright 2016 Inmanta
 
-@author: wouter
-'''
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
+
+import os
 
 import ruamel.yaml
-import os
 
 
 def rewrite(out):
@@ -18,7 +31,7 @@ def run():
     with open("module.yml", "r") as fd:
         data = ruamel.yaml.load(fd.read(), ruamel.yaml.RoundTripLoader)
 
-    if not "requires" in data:
+    if "requires" not in data:
         return
 
     requires = data["requires"]
@@ -55,7 +68,6 @@ def run():
                 out = rewrite(out)
                 with open(name, 'w') as modified:
                     modified.write(out)
-
 
 
 if __name__ == '__main__':
