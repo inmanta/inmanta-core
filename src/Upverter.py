80b1dce2ebb0013b738fc95cbe51d7c768c02b55
Wouter De Borger <wouter@inmanta.com>
2016-05-30 15:06:09 +0200

    more rewriting

--- src/Upverter.py
+++ src/Upverter.py
@@ -8,6 +8,12 @@ import ruamel.yaml
 import os
 
 
+def rewrite(out):
+    out = out.replace("impera.io", "inmanta.com")
+    out = out.replace("Impera", "Inmanta")
+    return out
+
+
 def run():
     with open("module.yml", "r") as fd:
         data = ruamel.yaml.load(fd.read(), ruamel.yaml.RoundTripLoader)
@@ -20,10 +26,12 @@ def run():
     requires = [x for x in requires.keys()]
     data["requires"] = requires
 
-    impstmt = ''.join(["import %s\n" % x for x in requires if requires is not "std"])
+    impstmt = ''.join(["\nimport %s" % x for x in requires if requires is not "std"])
 
     with open("module.yml", "w") as fd:
-        fd.write(ruamel.yaml.dump(data, Dumper=ruamel.yaml.RoundTripDumper))
+        out = ruamel.yaml.dump(data, Dumper=ruamel.yaml.RoundTripDumper)
+        out = rewrite(out)
+        fd.write(out)
 
     for dirpath, dirnames, files in os.walk("model"):
         for name in files:
@@ -31,8 +39,23 @@ def run():
                 name = os.path.join(dirpath, name)
                 with open(name, 'r') as original:
                     fdata = original.read()
+                parts = fdata.split("\"\"\"")
+                parts[2] = impstmt + parts[2]
+                out = "\"\"\"".join(parts)
+                out = rewrite(out)
                 with open(name, 'w') as modified:
-                    modified.write(impstmt + fdata)
+                    modified.write(out)
+
+    for dirpath, dirnames, files in os.walk("plugins"):
+        for name in files:
+            if name.lower().endswith("py"):
+                name = os.path.join(dirpath, name)
+                with open(name, 'r') as original:
+                    fdata = original.read()
+                out = rewrite(out)
+                with open(name, 'w') as modified:
+                    modified.write(out)
+
 
 
 if __name__ == '__main__':
