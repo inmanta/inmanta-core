e57389cf642fbfe061aa4dfb7b079355712af04c
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2016-08-25 08:55:03 +0200

    Port to pytest

--- tests/io_test.py
+++ tests/io_test.py
@@ -24,27 +24,39 @@ import grp
 
 from inmanta.agent.io.local import LocalIO
 from inmanta.agent.io.local import BashIO
-from nose.tools import assert_equal
+import pytest
+
+io_list = [LocalIO(), BashIO(), BashIO(run_as="root")]
 
 
-def check_hash(io, testdir):
+@pytest.yield_fixture(scope="module")
+def testdir():
+    testdir = tempfile.mkdtemp()
+    yield testdir
+    shutil.rmtree(testdir)
+
+
+@pytest.mark.parametrize("io", io_list)
+def test_check_hash(io, testdir):
     filename = os.path.join(testdir, "hashfile")
     with open(filename, "w+") as fd:
         fd.write("test")
 
-    assert(io.hash_file(filename) == "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")
+    assert io.hash_file(filename) == "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
 
 
-def check_read(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_read(io, testdir):
     test_str = "hello é"
     filename = os.path.join(testdir, "readfile")
     with open(filename, "wb+") as fd:
         fd.write(test_str.encode())
 
-    assert_equal(io.read(filename), test_str)
+    assert io.read(filename) == test_str
 
 
-def check_read_binary(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_read_binary(io, testdir):
     test_str = b'\1\2\3\4\5\6'
     filename = os.path.join(testdir, "readbinaryfile")
     with open(filename, "wb+") as fd:
@@ -52,67 +64,74 @@ def check_read_binary(io, testdir):
 
     result = io.read_binary(filename)
 
-    assert(isinstance(result, bytes))
-    assert(result == test_str)
+    assert isinstance(result, bytes)
+    assert result == test_str
 
 
-def check_run(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_run(io, testdir):
     result = io.run("echo", ["world"])
-    assert(result[2] == 0)
-    assert(result[0] == "world")
+    assert result[2] == 0
+    assert result[0] == "world"
 
     # check cwd
     os.makedirs(os.path.join(testdir, "check_run" + str(io)))
     out, _, _ = io.run("ls", [], cwd=testdir)
-    assert("check_run" + str(io) in out)
+    assert "check_run" + str(io) in out
 
 
-def check_file_exists(io, testdir):
-    assert(io.file_exists(testdir))
-    assert(not io.file_exists("/blablablablablalbla"))
+@pytest.mark.parametrize("io", io_list)
+def test_check_file_exists(io, testdir):
+    assert io.file_exists(testdir)
+    assert not io.file_exists("/blablablablablalbla")
 
     filename = os.path.join(testdir, "testfile" + str(io))
-    assert(not io.file_exists(filename))
+    assert not io.file_exists(filename)
 
     with open(filename, "w+") as fd:
         fd.write("")
 
-    assert(io.file_exists(filename))
+    assert io.file_exists(filename)
 
 
-def check_symlink(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_symlink(io, testdir):
     dst = os.path.join(testdir, "ln" + str(io))
     io.symlink(testdir, dst)
 
-    assert(os.path.exists(dst))
-    assert(os.path.islink(dst))
-    assert(os.readlink(dst) == testdir)
+    assert os.path.exists(dst)
+    assert os.path.islink(dst)
+    assert os.readlink(dst) == testdir
 
 
-def check_readlink(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_readlink(io, testdir):
     dst = os.path.join(testdir, "rdln" + str(io))
     os.symlink(testdir, dst)
 
-    assert(io.readlink(dst) == testdir)
+    assert io.readlink(dst) == testdir
 
 
-def check_is_symlink(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_is_symlink(io, testdir):
     dst = os.path.join(testdir, "isln" + str(io))
     os.symlink(testdir, dst)
 
-    assert(io.is_symlink(dst))
-    assert(not io.is_symlink(testdir))
+    assert io.is_symlink(dst)
+    assert not io.is_symlink(testdir)
 
 
-def check_mkdir(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_mkdir(io, testdir):
     mkdir = os.path.join(testdir, "dir" + str(io))
     io.mkdir(mkdir)
 
-    assert(os.path.exists(mkdir))
-    assert(os.path.isdir(mkdir))
+    assert os.path.exists(mkdir)
+    assert os.path.isdir(mkdir)
 
 
-def check_rmdir(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_rmdir(io, testdir):
     path = os.path.join(testdir, "rmdir" + str(io))
     os.mkdir(path)
     assert(os.path.exists(path))
@@ -124,23 +143,26 @@ def check_rmdir(io, testdir):
 
     io.rmdir(path)
 
-    assert(not os.path.exists(path))
-    assert(not os.path.exists(path2))
+    assert not os.path.exists(path)
+    assert not os.path.exists(path2)
 
 
-def check_filestat(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_filestat(io, testdir):
     stat = io.file_stat("/etc")
-    assert_equal(stat["permissions"], 755)
-    assert_equal(stat["owner"], "root")
-    assert_equal(stat["group"], "root")
+    assert stat["permissions"] == 755
+    assert stat["owner"] == "root"
+    assert stat["group"] == "root"
 
 
-def check_chown(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_chown(io, testdir):
     # chown to the same user so we do not need root to run this test
     path = os.path.join(testdir, "chown" + str(io))
     with open(path, "w+") as fd:
         fd.write("")
-    assert(os.path.exists(path))
+
+    assert os.path.exists(path)
 
     user = pwd.getpwuid(os.getuid())[0]
     groups = [grp.getgrgid(g).gr_name for g in os.getgroups()]
@@ -151,15 +173,17 @@ def check_chown(io, testdir):
     # TODO(bart): add more checks
 
 
-def check_put(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_put(io, testdir):
     path = os.path.join(testdir, "put" + str(io))
     io.put(path, b"hello")
 
     with open(path, "rb") as fd:
-        assert(b"hello" == fd.read())
+        assert b"hello" == fd.read()
 
 
-def check_chmod(io, testdir):
+@pytest.mark.parametrize("io", io_list)
+def test_check_chmod(io, testdir):
     path = os.path.join(testdir, "chmod" + str(io))
     with open(path, "w+") as fd:
         fd.write("Test\n")
@@ -172,18 +196,18 @@ def check_chmod(io, testdir):
     st = os.stat(path)
     new_mode = str(oct(st.st_mode))[-4:]
 
-    assert(mode != new_mode)
-    assert_equal("0755", new_mode)
+    assert mode != new_mode
+    assert "0755" == new_mode
 
 
-def test_io():
-    classes = [LocalIO(), BashIO(), BashIO(run_as="root")]
-    tests = [check_hash, check_read, check_read_binary, check_run, check_file_exists, check_symlink, check_readlink,
-             check_is_symlink, check_mkdir, check_rmdir, check_filestat, check_chown, check_put, check_chmod]
-    testdir = tempfile.mkdtemp()
-
-    for test_fn in tests:
-        for cls in classes:
-            yield test_fn, cls, testdir
-
-    shutil.rmtree(testdir)
+# def test_io():
+#     classes = [LocalIO(), BashIO(), BashIO(run_as="root")]
+#     tests = [check_hash, check_read, check_read_binary, check_run, check_file_exists, check_symlink, check_readlink,
+#              check_is_symlink, check_mkdir, check_rmdir, check_filestat, check_chown, check_put, check_chmod]
+#     testdir = tempfile.mkdtemp()
+#
+#     for test_fn in tests:
+#         for cls in classes:
+#             yield test_fn, cls, testdir
+#
+#     shutil.rmtree(testdir)
