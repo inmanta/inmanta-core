14a0a8c03be3116bec09edd85af3a8632c3cf888
Wouter De Borger <wouter.deborger@inmanta.com>
2016-11-15 16:48:00 +0100

    added negative numbers

--- src/inmanta/parser/plyInmantaLex.py
+++ src/inmanta/parser/plyInmantaLex.py
@@ -99,13 +99,13 @@ def t_mls_MLS(t):
 
 
 def t_FLOAT(t):
-    r'[0-9]*[.][0-9]+'
+    r'[-]?[0-9]*[.][0-9]+'
     t.value = float(t.value)
     return t
 
 
 def t_INT(t):
-    r'[0-9]+'
+    r'[-]?[0-9]+'
     t.value = int(t.value)
     return t
 
