2ca96b5784192107a67ae716e5efe1d625442fd6
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2017-08-25 09:37:50 +0200

    Handle case where agent times out
    If the agent hangs its session expires. In that case a 404 or 500 will
    be returned

--- src/inmanta/agent/agent.py
+++ src/inmanta/agent/agent.py
@@ -226,6 +226,10 @@ class RemoteResourceAction(ResourceAction):
             result = yield self.scheduler.get_client().get_resource(self.scheduler.agent._env_id, str(self.resource_id),
                                                                     logs=True, log_action=const.ResourceAction.deploy,
                                                                     log_limit=1)
+            if result.code != 200:
+                LOGGER.error("Failed to get the status for remote resource %s (%s)", str(self.resource_id),
+                             result.result)
+
             status = const.ResourceState[result.result["resource"]["status"]]
             if status == const.ResourceState.available or self.future.done():
                 # wait for event
