076fb98ebb0f1eaf469cc6c1ab31e476c181e0b4
Wouter De Borger <wouter.deborger@inmanta.com>
2018-04-17 11:24:55 +0200

    temp commit for rebase

--- src/inmanta/export.py
+++ src/inmanta/export.py
@@ -274,17 +274,22 @@ class Exporter(object):
         if len(self._resources) == 0:
             LOGGER.warning("Empty deployment model.")
 
+        model = {}
+
         if self.options and self.options.json:
             with open(self.options.json, "wb+") as fd:
                 fd.write(protocol.json_encode(resources).encode("utf-8"))
-
+            if len(self._resources) > 0 or len(unknown_parameters) > 0:
+                model = ModelExporter(types).export_all()
+                with open(self.options.json+".types", "wb+") as fd:
+                    fd.write(protocol.json_encode(model).encode("utf-8"))
         elif len(self._resources) > 0 or len(unknown_parameters) > 0 and not no_commit:
             model = ModelExporter(types).export_all()
             self.commit_resources(self._version, resources, metadata, model)
             LOGGER.info("Committed resources with version %d" % self._version)
 
         if include_status:
-            return self._version, self._resources, self._resource_state
+            return self._version, self._resources, self._resource_state, model
         return self._version, self._resources
 
     def get_variable(self, name):
