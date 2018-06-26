e078969aa6fc87a4656833c596ef604e249950b7
Wouter De Borger <wouter.deborger@inmanta.com>
2018-06-26 14:38:55 +0200

    improved error reporting on bad assignment
    fixes #670

--- tests/test_compiler_errors.py
+++ tests/test_compiler_errors.py
@@ -15,6 +15,7 @@
 
     Contact: code@inmanta.com
 """
+from conftest import snippetcompiler
 
 
 def test_plugin_excn(snippetcompiler):
@@ -384,6 +385,19 @@ def test_610_multi_add(snippetcompiler):
         " attribute b ({dir}/main.cf:11:11) requires 2 values but only 1 are set")
 
 
+def test_670_assign_on_relation(snippetcompiler):
+    snippetcompiler.setup_for_error_re(
+        """
+        h = std::Host(name="test", os=std::linux)
+        f = std::ConfigFile(host=h, path="a", content="")
+
+        h.files.path = "1"
+
+        """,
+        "The object at h.files is not an Entity but a <class 'list'> with value \[std::ConfigFile [0-9a-fA-F]+\]"
+        " \(reported in h.files.path = '1' \({dir}/main.cf:5\)\)")
+
+
 def test_672_missing_type(snippetcompiler):
     snippetcompiler.setup_for_error(
         """
@@ -396,3 +410,5 @@ def test_672_missing_type(snippetcompiler):
         """,
         "could not find type Testt in namespace __config__"
         " (reported in Implementation(test) ({dir}/main.cf:5))")
+
+
