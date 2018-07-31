56a892b3b846f7dde96e2319c7a8e87da85b7188
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2018-07-31 11:32:22 +0200

    Release new version with ca file fix

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name="inmanta",
     package_dir={"" : "src"},
     packages=find_packages("src"),
-    version="2018.2",
+    version="2018.2.1",
     description="Inmanta deployment tool",
     author="Inmanta",
     author_email="code@inmanta.com",
