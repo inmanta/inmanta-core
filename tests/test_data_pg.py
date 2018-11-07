6a69a0f17cb4db972a87651648cb002de9cf4fe6
Wouter De Borger <wouter.deborger@inmanta.com>
2018-11-07 17:06:10 +0100

    async fixtures

--- tests/test_data_pg.py
+++ tests/test_data_pg.py
@@ -2,12 +2,12 @@ import pytest
 import asyncpg
 
 
+@pytest.fixture
 async def postgresql_client(postgresql_proc):
     connection = await asyncpg.connect('postgresql://%s@%s:%d/' % (postgresql_proc.user, postgresql_proc.host, postgresql_proc.port))
-    return connection
+    yield connection
+    await connection.close()
 
-@pytest.mark.gen_test
-def test_postgres_client2(postgresql_proc):
-    connection = yield postgresql_client(postgresql_proc)
-    yield connection.execute("CREATE database testx")
-    yield connection.close()
+@pytest.mark.asyncio
+async def test_postgres_client2(postgresql_client):
+    await postgresql_client.execute("CREATE database testx")
