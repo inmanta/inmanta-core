55cdfa812af81bfc99664d05db2f3759ecb8528f
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2018-12-07 12:44:50 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     name="inmanta",
     package_dir={"" : "src"},
     packages=find_packages("src"),
-    version="2018.3",
+    version="2018.4",
     description="Inmanta deployment tool",
     author="Inmanta",
     author_email="code@inmanta.com",
