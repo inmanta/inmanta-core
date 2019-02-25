29e9b9b2d060e46eda8741c8d8c0cffe2e5a592d
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2019-02-25 11:43:06 +0100

    Add test case to validate jwks pk fetch

--- tests/test_jwt.py
+++ tests/test_jwt.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2017 Inmanta
+    Copyright 2019 Inmanta
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -15,8 +15,14 @@
 
     Contact: code@inmanta.com
 """
-from inmanta import protocol
 import time
+import os
+import pytest
+import tornado
+
+from tornado import web
+
+from inmanta import protocol
 
 
 def test_jwt_create(inmanta_config):
@@ -38,3 +44,63 @@ def test_jwt_create(inmanta_config):
     assert jot3 != jot4
     assert jot1 != jot3
     assert jot2 != jot3
+
+
+class PKHandler(web.RequestHandler):
+    def get(self):
+        public_key = {
+            "keys": [
+                {
+                    "kid": "NMROYvAmygR5zzmtYXgmCbJd8cXHcvAtx_o-JxJNPoQ",
+                    "kty": "RSA",
+                    "alg": "RS256",
+                    "use": "sig",
+                    "n": "oA7zi9u230-e3atV4rW9oI6z3ea_rViKlBTqq4_v9E-PK47yPpUvnHH9eJrKBXcuX-cVO2pSmDQ65nAzEaobjBU8XPtm3sceY1GsC"
+                    "cP4Uo7gEbqLxsaqN1WUt1tnbV10wEbZzHmtAW_J_J5wlIB696ceEdwxNyj3Zscq15QIMsahDmV54fvwussuPgNhd0t4ng9BzaW-kG"
+                    "2-Z80blyxN3fUbXX1JRMOiX4a7W_UXN5Q9B4kE9vlqAm30FnhYvLLqKh-DFvxq49dbYTWR-pJSFkjRMD6u1MUzKQEmOLYTnDX42zA"
+                    "rTVhbvQl7OnW-OXtwx9zVqiQIIqt5IQgZQ7PfwQ",
+                    "e": "AQAB",
+                }
+            ]
+        }
+
+        time.sleep(1.1)
+
+
+@pytest.fixture(scope="function")
+async def jwks(unused_tcp_port):
+    http_app = web.Application([(r"/auth/realms/inmanta/protocol/openid-connect/certs", PKHandler)])
+    server = tornado.httpserver.HTTPServer(http_app)
+    server.bind(unused_tcp_port)
+    server.start()
+    yield server
+
+    server.stop()
+    await server.close_all_connections()
+
+
+@pytest.mark.asyncio(timeout=30)
+async def test_validate_rs256(jwks, tmp_path):
+    """
+        Test that inmanta can download a rs256 public key
+    """
+    port = str(list(jwks._sockets.values())[0].getsockname()[1])
+    config_file = os.path.join(tmp_path, "auth.cfg")
+    with open(config_file, "w+") as fd:
+        fd.write("""
+[auth_jwt_keycloak]
+algorithm=RS256
+sign=false
+client_types=api
+issuer=https://localhost:{0}/auth/realms/inmanta
+audience=sodev
+jwks_uri=http://localhost:{0}/auth/realms/inmanta/protocol/openid-connect/certs
+validate_cert=false
+""".format(port))
+
+    from inmanta.config import Config, AuthJWTConfig
+    Config.load_config(config_file)
+
+    cfg_list = AuthJWTConfig.list()
+
+    assert len(cfg_list) == 1
