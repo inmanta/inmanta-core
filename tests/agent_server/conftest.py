8a4c27abc1b19e56e5f193343f202a1ad6bce5e1
arnaudsjs <arnaudsjs@users.noreply.github.com>
2019-04-30 11:18:47 +0200

    Support scheduling of async-def based action in util.Scheduler. (#1136)
    * Support scheduling of async-def based action in util.Scheduler.

--- tests/agent_server/conftest.py
+++ tests/agent_server/conftest.py
@@ -72,50 +72,6 @@ def get_resource(version, key="key1", agent="agent1", value="value1"):
     }
 
 
-def log_contains(caplog, loggerpart, level, msg):
-    close = []
-    for logger_name, log_level, message in caplog.record_tuples:
-        if msg in message:
-            if loggerpart in logger_name and level == log_level:
-                return
-            else:
-                close.append((logger_name, log_level, message))
-    if close:
-        print("found nearly matching log entry")
-        for logger_name, log_level, message in close:
-            print(logger_name, log_level, message)
-        print("------------")
-
-    assert False
-
-
-def log_doesnt_contain(caplog, loggerpart, level, msg):
-    for logger_name, log_level, message in caplog.record_tuples:
-        if loggerpart in logger_name and level == log_level and msg in message:
-            assert False
-
-
-def log_index(caplog, loggerpart, level, msg, after=0):
-    """Find a log in line in the captured log, return the index of the first occurrence
-
-       :param after: only consider records after the given index"""
-    close = []
-    for i, (logger_name, log_level, message) in enumerate(caplog.record_tuples[after:]):
-        if msg in message:
-            if loggerpart in logger_name and level == log_level:
-                return i + after
-            else:
-                close.append((logger_name, log_level, message))
-
-    if close:
-        print("found nearly matching")
-        for logger_name, log_level, message in close:
-            print(logger_name, log_level, message)
-        print("------------")
-
-    assert False
-
-
 async def _deploy_resources(
     client, environment, resources, version, push, agent_trigger_method=None
 ):
