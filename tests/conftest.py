92f82b61db64e469bb0c2ce3fe3a4525a422f539
Wouter De Borger <wouter.deborger@inmanta.com>
2019-05-29 15:39:31 +0200

    more whitespace issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -148,18 +148,18 @@ async def postgress_get_custom_types(postgresql_client):
     # \dT
 
     get_custom_types = """
-   SELECT n.nspname as "Schema",
+    SELECT n.nspname as "Schema",
      pg_catalog.format_type(t.oid, NULL) AS "Name",
      pg_catalog.obj_description(t.oid, 'pg_type') as "Description"
-   FROM pg_catalog.pg_type t
+    FROM pg_catalog.pg_type t
         LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
-   WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
+    WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
      AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
          AND n.nspname <> 'pg_catalog'
          AND n.nspname <> 'information_schema'
      AND pg_catalog.pg_type_is_visible(t.oid)
-   ORDER BY 1, 2;
-   """
+    ORDER BY 1, 2;
+"""
 
     types_in_db = await postgresql_client.fetch(get_custom_types)
     type_names = [x["Name"] for x in types_in_db]
