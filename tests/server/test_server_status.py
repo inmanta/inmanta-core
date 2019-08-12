a5f455df633844d778fc55dc7c090af9100f6e7b
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2019-08-12 12:37:24 +0200

    Remove print

--- tests/server/test_server_status.py
+++ tests/server/test_server_status.py
@@ -31,5 +31,3 @@ async def test_server_status(server, client):
     db_status = [x for x in status["slices"] if x["name"] == "core.database"]
     assert len([x for x in status["slices"] if x["name"] == "core.database"]) == 1
     assert db_status[0]["status"]["connected"] is True
-
-    print(status["slices"])
