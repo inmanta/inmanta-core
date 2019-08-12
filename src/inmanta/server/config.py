e725b7375b8c2e1de45ca8c2b80b9731843ce04a
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2019-08-12 15:40:30 +0200

    Make pep8 angry when copyright headers are missing (#1305)
    * Add headers and license check

--- src/inmanta/server/config.py
+++ src/inmanta/server/config.py
@@ -18,7 +18,7 @@
 
 import logging
 
-from inmanta.config import Option, is_bool, is_int, is_map, is_str_opt, is_time, log_dir, state_dir
+from inmanta.config import Option, is_bool, is_int, is_map, is_str_opt, is_time, log_dir, state_dir, is_list
 
 LOGGER = logging.getLogger(__name__)
 
@@ -172,6 +172,25 @@ server_resource_action_log_prefix = Option(
     "resource-actions-",
     "File prefix in log-dir, containing the resource-action logs. The after the prefix the environment uuid and .log is added",
     is_str_opt,
+)
+
+server_enabled_extensions = Option(
+    "server",
+    "enabled_extensions",
+    None,
+    "A list of extensions the server must load. If this option is empty, the server will load all extensions available. "
+    "If an extension listed in this list is not available, the server will refuse to start.",
+    is_list,
+)
+
+server_disabled_extensions = Option(
+    "server",
+    "disabled_extensions",
+    None,
+    "A list of extensions the server must not load. If this option is empty, the server will load all extensions available. "
+    "The server will not start any extensions in this list. Extensions in this list take precedence of extension in the "
+    "enabled list. core is not allowed in the list of disabled extensions.",
+    is_list,
 )
 
 
