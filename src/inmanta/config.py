9c66df7fb7f0d6716efa52ece045aa33ada4a868
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2019-09-17 20:21:18 +0200

    Update src/inmanta/config.py
    Co-Authored-By: Wouter De Borger <wouter.deborger@inmanta.com>

--- src/inmanta/config.py
+++ src/inmanta/config.py
@@ -290,7 +290,7 @@ class Option(Generic[T]):
     def validate(self, value: str) -> T:
         return self.validator(value)
 
-    def get_default_value(self) -> Union[str, bool, int, None, Callable]:
+    def get_default_value(self) -> Union[str, bool, int, None]:
         defa = self.default
         if callable(defa):
             return defa()
