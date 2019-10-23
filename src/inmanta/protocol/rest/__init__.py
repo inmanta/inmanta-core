333c48717daca836060ba13a5a5eec945beac32c
Andras Kovari <andras.kovari@inmanta.com>
2019-10-23 09:29:57 +0200

    Don't log http errors as level error

--- src/inmanta/protocol/rest/__init__.py
+++ src/inmanta/protocol/rest/__init__.py
@@ -430,7 +430,7 @@ class RESTBase(util.TaskHandler):
             raise exceptions.ServerError("data validation error.")
 
         except exceptions.BaseHttpException:
-            LOGGER.exception("")
+            LOGGER.warning("An HTTP Error occurred", exc_info=True)
             raise
 
         except Exception as e:
