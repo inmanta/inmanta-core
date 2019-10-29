f215d595db225d6f0c8343fdf24213d3f2db0c6c
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2019-10-29 12:27:49 +0100

    Update

--- tests/db/test_db_schema.py
+++ tests/db/test_db_schema.py
@@ -77,14 +77,15 @@ async def assert_core_untouched(postgresql_client, corev=0):
 
 @pytest.mark.asyncio
 async def test_dbschema_clean(postgresql_client: asyncpg.Connection, get_columns_in_db_table, hard_clean_db, caplog):
-    dbm = schema.DBSchema("test_dbschema_clean", inmanta.db.versions, postgresql_client)
-    with pytest.raises(TableNotFound):
-        await dbm.get_current_version()
-    await dbm.ensure_self_update()
-    await run_updates_and_verify(get_columns_in_db_table, dbm, 0)
-    await assert_core_untouched(postgresql_client)
+    with caplog.at_level(logging.INFO):
+        dbm = schema.DBSchema("test_dbschema_clean", inmanta.db.versions, postgresql_client)
+        with pytest.raises(TableNotFound):
+            await dbm.get_current_version()
+        await dbm.ensure_self_update()
+        await run_updates_and_verify(get_columns_in_db_table, dbm, 0)
+        await assert_core_untouched(postgresql_client)
 
-    log_contains(caplog, "inmanta.data.schema.schema:test_dbschema_clean", logging.INFO, "Creating schema version table")
+        log_contains(caplog, "inmanta.data.schema.schema:test_dbschema_clean", logging.INFO, "Creating schema version table")
 
 
 @pytest.mark.asyncio
