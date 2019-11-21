f05a9c8b062c92d252c6779e5683afc0bd4868f3
Sander <sander.vanbalen@inmanta.com>
2019-11-21 11:49:22 +0100

    Lexer string regex (#1601)
    * improved lexer STRING regex to allow for strings ending with a backslash

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -455,6 +455,19 @@ a = /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}
     assert stmt.children[1].value == re.compile(r"[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")
 
 
+def test_regex_backslash():
+    statements = parse_code(
+        r"""
+a = /\\/
+"""
+    )
+
+    assert len(statements) == 1
+    stmt = statements[0].value
+    assert isinstance(stmt, Regex)
+    assert stmt.children[1].value == re.compile(r"\\")
+
+
 def test_regex_escape():
     statements = parse_code(
         r"""
@@ -780,6 +793,32 @@ a='jos'
     assert isinstance(stmt, Assign)
     assert isinstance(stmt.value, Literal)
     assert stmt.value.value == "jos"
+
+
+def test_string_backslash():
+    statements = parse_code(
+        """
+a="\\\\"
+"""
+    )
+    assert len(statements) == 1
+    stmt = statements[0]
+    assert isinstance(stmt, Assign)
+    assert isinstance(stmt.value, Literal)
+    assert stmt.value.value == "\\"
+
+
+def test_string_backslash_2():
+    statements = parse_code(
+        """
+a='\\\\'
+"""
+    )
+    assert len(statements) == 1
+    stmt = statements[0]
+    assert isinstance(stmt, Assign)
+    assert isinstance(stmt.value, Literal)
+    assert stmt.value.value == "\\"
 
 
 def test_empty():
