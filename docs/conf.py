846a37fd818f3b47cd19796840bde0566f9bc119
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-01-10 13:27:42 +0100

    Add extensions to docs

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ release = '2019.6dev'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['extensions.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
