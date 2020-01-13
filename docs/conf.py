a6d6f64aff62066734933abe8f8d0e495e07ab70
arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>
2020-01-13 08:13:56 +0100

    Update docs/conf.py
    Co-Authored-By: Bart Vanbrabant <bart.vanbrabant@inmanta.com>

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,7 @@ release = '2019.6dev'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
+# The documentation build tool overrides this when extensions are included in the documentation build.
 exclude_patterns = ['extensions.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
