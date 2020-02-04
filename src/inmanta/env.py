7672661e7145411ca362312f1d259dcbe8138d3d
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-02-04 08:00:28 +0100

    Call _install() only once + fix sorting issue

--- src/inmanta/env.py
+++ src/inmanta/env.py
@@ -258,18 +258,12 @@ class VirtualEnv(object):
         """
             Install requirements from a list of requirement strings
         """
-        # Install url-based requirements directly to prevent parsing issues
-        url_based_requirements = [r for r in requirements_list if "://" in r]
-        self._install(url_based_requirements)
-
-        # Install other requirements only when not already present in parent venv
-        requirements_list = sorted([r for r in requirements_list if "://" not in r])
         if detailed_cache:
             requirements_list = sorted(list(set(requirements_list) - self.__cache_done))
             if len(requirements_list) == 0:
                 return
 
-        requirements_list = self._remove_requirements_present_in_parent_env(requirements_list)
+        requirements_list = sorted(self._remove_requirements_present_in_parent_env(requirements_list))
 
         # hash it
         sha1sum = hashlib.sha1()
@@ -290,6 +284,9 @@ class VirtualEnv(object):
         reqs_to_remove = []
         packages_installed_in_parent = self.get_package_installed_in_parent_env()
         for r in requirements_list:
+            # Always install url-based requirements
+            if "://" in r:
+                continue
             parsed_req = list(pkg_resources.parse_requirements(r))[0]
             # Package is installed and its version fits the constraint
             if (
