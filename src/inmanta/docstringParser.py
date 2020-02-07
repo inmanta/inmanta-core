1c3eb90dc0c5c40918315af9969ea6801729cf51
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-02-07 14:39:41 +0100

    Add support to parse the docstring of a service entity

---
+++ src/inmanta/docstringParser.py
@@ -0,0 +1,52 @@
+"""
+    Copyright 2020 Inmanta
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
+
+from typing import Dict, Optional
+
+import docstring_parser
+
+
+class DocString:
+    def __init__(self, doc_string: docstring_parser.Docstring):
+        for attr in doc_string.meta:
+            if len(attr.args) != 2 or attr.args[0] != "attr":
+                raise Exception(f"Failed to parse attribute {' '.join(attr.args)}")
+        self._attr_description_map = {attr.args[1]: attr.description for attr in doc_string.meta}
+        self._doc_string = doc_string
+
+    def get_description(self) -> Optional[str]:
+        if self._doc_string.short_description is None:
+            return None
+        if self._doc_string.long_description is not None:
+            return f"{self._doc_string.short_description}\n\n{self._doc_string.long_description}"
+        else:
+            return self._doc_string.short_description
+
+    def get_description_for_attribute(self, attr_name: str) -> Optional[str]:
+        return self._attr_description_map.get(attr_name, None)
+
+    def get_attribute_description_map(self) -> Dict[str, str]:
+        return dict(self._attr_description_map)
+
+
+def parse_docstring(doc_string: str) -> DocString:
+    """
+        Parse the docstring of an entity.
+    """
+    doc_string = docstring_parser.parse(doc_string, style=docstring_parser.Style.rest)
+    return DocString(doc_string)
