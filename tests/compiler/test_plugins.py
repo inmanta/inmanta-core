950f5898e9e7399a8b200971191dd6a6e420f63b
Sander Van Balen <git@sandervanbalen.be>
2020-02-10 10:54:46 +0100

    added test case for context as kwarg

--- tests/compiler/test_plugins.py
+++ tests/compiler/test_plugins.py
@@ -198,3 +198,13 @@ x = null
         """,
     )
     compiler.do_compile()
+
+
+def test_1778_context_as_kwarg_reject(snippetcompiler):
+    snippetcompiler.setup_for_error(
+        """
+std::generate_password("pw_id", context=42)
+        """,
+        "Invalid keyword argument 'context' for 'generate_password()'"
+        " (reported in std::generate_password('pw_id',context=42) ({dir}/main.cf:2))"
+    )
