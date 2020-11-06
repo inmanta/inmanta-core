05de9a3d8b038ffcf9291cfc811d5b8d502fffe2
arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>
2020-11-06 14:21:01 +0100

    Ensure the sphinx link check works for sphinx 3.3.0 (#2502)

--- docs/check_sphinx.py
+++ docs/check_sphinx.py
@@ -1,14 +1,25 @@
+import os
 import subprocess
+import pytest
 
-
-def test_linkcheck(tmpdir):
+@pytest.fixture
+def build_docs(tmpdir):
     doctrees = tmpdir.join("doctrees")
     htmldir = tmpdir.join("html")
-    print(doctrees)
+    docs_dir = os.path.dirname(os.path.abspath(__file__))
+    # Ensure that all required files such as inmanta.pdf exist
+    subprocess.check_call(["make", "-C", docs_dir, "html", f"BUILDDIR={tmpdir}"])
+    return docs_dir, doctrees, htmldir
+
+
+def test_linkcheck(build_docs):
+    docs_dir, doctrees, htmldir = build_docs
+    # Execute link check
     subprocess.check_call(["sphinx-build", "-blinkcheck", "-d", str(doctrees), ".", str(htmldir)])
+    # The link check for openapi.html is ignored in the conf.py file, since
+    # a trick is used in the reference/index.rst because toctree doesn't
+    # offer support for relative links to something that isn't a sphinx document.
+    # This check verifies that the reference/openapi.html file is created.
+    openapi_html_file = htmldir.join("reference/openapi.html")
+    assert os.path.exists(openapi_html_file)
 
-
-def test_build_docs(tmpdir):
-    doctrees = tmpdir.join("doctrees")
-    htmldir = tmpdir.join("html")
-    subprocess.check_call(["sphinx-build", "-bhtml", "-d", str(doctrees), ".", str(htmldir)])
