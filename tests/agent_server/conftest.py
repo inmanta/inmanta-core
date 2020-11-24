0c77a28ecf7446ca13177c6308c668f97e51ae59
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-11-24 16:19:59 +0100

    Fix comment

--- tests/agent_server/conftest.py
+++ tests/agent_server/conftest.py
@@ -181,9 +181,8 @@ def resource_container():
     @resource("test::BadEventsStatus", agent="agent", id_attribute="key")
     class BadEventS(Resource):
         """
-        A file on a filesystem
+        Set `ctx.set_status(const.ResourceState.failed)` in process_events().
         """
-
         fields = ("key", "value", "purged")
 
     @resource("test::BadPost", agent="agent", id_attribute="key")
