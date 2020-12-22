72e82a67b3a8a77af496badc0a2c8e5dccf62221
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-12-22 13:28:35 +0100

    Bump version: 2020.4.4 → 2020.4.5

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    version="2020.4.4",
+    version="2020.4.5",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="inmanta",
