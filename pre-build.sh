1a1558abea515fa5505f3086b6aa4167b40423d8
Sander <sander.vanbalen@inmanta.com>
2021-01-25 14:53:21 +0100

    made pyproject.toml compatible with new irt build pipeline (#2648)
    * made pyproject.toml compatible with new irt build pipeline
    
    * removed redundant PYTHONPATH assignment
    
    * pre build script config update
    
    * pyproject.toml config update
    
    * fixed toml table syntax
    
    * include parser in MANIFEST.in

---
+++ pre-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# start inmanta app to generate parser/lexer in place
+env=$(mktemp -d)
+python3 -m venv ${env}
+${env}/bin/python3 -m pip install -U pip
+${env}/bin/python3 -m pip install -e .
+${env}/bin/python3 -m inmanta.app -h
+rm -r ${env}
