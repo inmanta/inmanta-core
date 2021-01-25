936b8fe2b7dd6d7bbd1ca979a65fe3a4b24acc6b
Sander Van Balen <git@sandervanbalen.be>
2021-01-25 15:07:55 +0100

    new versioning scheme for pytest-inmanta-extensions

--- tests_common/setup.py
+++ tests_common/setup.py
@@ -35,7 +35,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    version="2020.4.5",
+    version="3.0.0",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="pytest-inmanta-extensions",
