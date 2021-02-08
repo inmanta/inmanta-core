908ee06d2d0860ecd085ed4df1ad0cbfbce8f645
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2021-02-08 20:27:03 +0100

    Fix python path inheritance for virtual envs (#2651)
    Improve handling of python path for processes that are forked by the server/compiler/agent. This should fix issues where the compiler or agents needs to install python packages that are already present in the server python path / venv.

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2016 Inmanta
+    Copyright 2021 Inmanta
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -73,13 +73,10 @@ def test_install_fails(tmpdir, caplog):
     venv.use_virtual_env()
     caplog.clear()
     package_name = "non-existing-pkg-inmanta"
-    exception_raised = False
-    try:
-        venv.install_from_list([package_name])
-    except Exception:
-        exception_raised = True
 
-    assert exception_raised
+    with pytest.raises(Exception):
+        venv.install_from_list([package_name])
+
     log_sequence = LogSequence(caplog)
     log_sequence.contains("inmanta.env", logging.ERROR, f"requirements: {package_name}")
 
@@ -138,45 +135,20 @@ def test_req_parser(tmpdir):
 
 def test_gen_req_file(tmpdir):
     e = env.VirtualEnv(tmpdir)
-    req = ["lorem == 0.1.1", "lorem > 0.1", "dummy-yummy", "iplib@git+https://github.com/bartv/python3-iplib", "lorem"]
+    req = [
+        "lorem == 0.1.1",
+        "lorem > 0.1",
+        "dummy-yummy",
+        "iplib@git+https://github.com/bartv/python3-iplib",
+        "lorem",
+        # verify support for environment markers as described in PEP 508
+        "lorem;python_version<'3.7'",
+        "lorem;platform_machine == 'x86_64' and platform_system == 'Linux'",
+    ]
 
     req_lines = [x for x in e._gen_requirements_file(req).split("\n") if len(x) > 0]
     assert len(req_lines) == 3
-
-
-def test_remove_requirements_present_in_parent_env(tmpdir):
-    v = env.VirtualEnv(tmpdir)
-    v.use_virtual_env()
-
-    # Verify parsing works
-    reqs = [
-        "lorem == 0.1.1",
-        "lorem > 0.1",
-        "lorem >= 1.1,<1.5",
-        "dummy-yummy",
-        "iplib@git+https://github.com/bartv/python3-iplib",
-    ]
-    assert v._remove_requirements_present_in_parent_env(reqs) == reqs
-
-    installed_packages = {
-        k: v for k, v in env.VirtualEnv._get_installed_packages(v._parent_python).items() if k != "pip" and k != "setuptools"
-    }
-    package = next(iter(installed_packages.keys()))
-    package_version = installed_packages[package]
-    splitted_package_version = package_version.split(".", maxsplit=1)
-    newer_package_version = f"{int(splitted_package_version[0]) + 1}.{splitted_package_version[1]}"
-
-    # Package is present in parent venv and constraint is met
-    reqs = [f"{package}=={package_version}", "non_existing_package==1.1.1"]
-    reqs_after_removal = ["non_existing_package==1.1.1"]
-    assert v._remove_requirements_present_in_parent_env(reqs) == reqs_after_removal
-
-    reqs = [f"{package}>={package_version},<{newer_package_version}", "non_existing_package==1.1.1"]
-    assert v._remove_requirements_present_in_parent_env(reqs) == reqs_after_removal
-
-    # Package is present in parent venv, but constraint isn't met
-    reqs = [f"{package}=={newer_package_version}", "non_existing_package==1.1.1"]
-    assert v._remove_requirements_present_in_parent_env(reqs) == reqs
-
-    reqs = [f"{package}>{package_version}", "non_existing_package==1.1.1"]
-    assert v._remove_requirements_present_in_parent_env(reqs) == reqs
+    assert (
+        'lorem == 0.1.1, > 0.1 ; python_version < "3.7" and platform_machine == "x86_64" and platform_system == "Linux"'
+        in req_lines
+    )
