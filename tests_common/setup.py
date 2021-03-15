71e2c7a984aa4dcccc998dc6c357b9c64fa15871
arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>
2021-03-15 15:09:40 +0100

    Pin internal dependencies (#2784)

--- tests_common/setup.py
+++ tests_common/setup.py
@@ -22,7 +22,7 @@ from setuptools import find_packages, setup
 requires = [
     "asyncpg",
     "click",
-    "inmanta~=2020.4.4.dev",
+    "inmanta==2020.4.5",
     "pyformance",
     "pytest-asyncio",
     "pytest-env",
