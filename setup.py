aa00fd14b7789cb2e369857ae16080121bb4d5e7
inmantaci <ci@inmanta.com>
2021-04-29 13:40:53 +0200

    Version bump based on change entries

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-version = "4.1.1"
+version = "4.2.0"
 
 setup(
     version=version,
