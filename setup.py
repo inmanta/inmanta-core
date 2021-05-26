c6a82226911b2748755b6090e9f3c9d29e40e90a
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2021-05-26 20:48:51 +0200

    Limit minor as well

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = [
     "colorlog",
     "cookiecutter",
     "cryptography",
-    "docstring-parser<0.8.1",
+    "docstring-parser<0.8.0",
     "email-validator",
     "execnet",
     "importlib_metadata",
