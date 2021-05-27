3e23d41e83021e29f4887f4b2efa78c24909741b
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2021-05-27 13:56:26 +0200

    Bump version to 2020.4.8

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-version = "2020.4.7"
+version = "2020.4.8"
 
 setup(
     version=version,
