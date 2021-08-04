1e0d3a18e08e8f038eb9b58cfd32fc8d24737f21
Wouter De Borger <wouter.deborger@inmanta.com>
2021-08-04 10:17:53 +0200

    reviewer comments

--- tests/moduletool/test_convert_v1_v2.py
+++ tests/moduletool/test_convert_v1_v2.py
@@ -1,8 +1,25 @@
+"""
+    Copyright 2021 Inmanta
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
 import configparser
 import os
 import sys
 
-from inmanta.module import DummyProject, ModuleV1, ModuleV2Metadata, Project
+from inmanta.module import DummyProject, ModuleV1, ModuleV2Metadata
 from inmanta.moduletool import ModuleConverter
 
 
@@ -17,6 +34,7 @@ def test_module_conversion(tmpdir):
     ModuleConverter(module_in).convert(tmpdir)
 
     assert os.path.exists(os.path.join(tmpdir, "setup.cfg"))
+    assert os.path.exists(os.path.join(tmpdir, "pyproject.toml"))
     assert os.path.exists(os.path.join(tmpdir, "model", "_init.cf"))
     assert os.path.exists(os.path.join(tmpdir, "files", "test.txt"))
     assert os.path.exists(os.path.join(tmpdir, "templates", "template.txt.j2"))
