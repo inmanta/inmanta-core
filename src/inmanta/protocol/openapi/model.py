f679eec673ee1df397ef6e631103ed33da07ee5b
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2021-09-01 09:34:28 +0200

    Added resolving methods to schemabase

--- src/inmanta/protocol/openapi/model.py
+++ src/inmanta/protocol/openapi/model.py
@@ -21,9 +21,8 @@ https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md
 Inspired by FastAPI:
 https://github.com/tiangolo/fastapi
 """
-
 from enum import Enum
-from typing import Any, Dict, List, Optional, Sequence, Union
+from typing import Any, Dict, List, Optional, Sequence, TypeVar, Union
 
 from pydantic import AnyUrl, Field
 
@@ -51,6 +50,9 @@ class Reference(BaseModel):
     ref: str = Field(..., alias="$ref")
 
 
+S = TypeVar("S", bound="SchemaBase")
+
+
 class SchemaBase(BaseModel):
     ref: Optional[str] = Field(None, alias="$ref")
     title: Optional[str] = None
@@ -68,6 +70,61 @@ class SchemaBase(BaseModel):
     anyOf: Optional[Sequence["SchemaBase"]] = None
     enum: Optional[List[str]] = None
 
+    def from_dict(self: S, schema: Dict[str, Any]) -> S:
+        """
+        Build an object with the same type as this object
+
+        :param schema: The schema of the object to build
+        """
+        return SchemaBase(**schema)
+
+    def resolve(self: S, ref_prefix: str, known_schemas: Dict[str, Any]) -> S:
+        """
+        Returns this object or the one this object is refering to.
+
+        :param ref_prefix: The prefix this object ref should have if it is only a reference
+        :param known_schemas: A dict of known schemas, the keys are the reference diminished from their prefix
+
+        :raises ValueError: If the schema has a badly formed ref
+        """
+        if not self.ref:
+            return self
+
+        if not self.ref.startswith(ref_prefix):
+            raise ValueError(f"Schema reference (={self.ref}) doesn't start with the expected prefix: '{ref_prefix}'")
+
+        reference = self.ref[len(ref_prefix) :]
+        return self.from_dict(known_schemas[reference])
+
+    def recursive_resolve(
+        self: S, ref_prefix: str, known_schemas: Dict[str, Any], update: Dict[str, Any], deep: bool = True
+    ) -> S:
+        """
+        Returns this object of the one this object is refering to, and resolve all the nested schema it contains.
+
+        :param ref_prefix: The prefix this object ref should have if it is only a reference
+        :param known_schemas: A dict of known schemas, the keys are the reference diminished from their prefix
+        :param update: A dict of attributes of this object to update
+        :param deep: Whether to perform a deepcopy of the object
+
+        :raises ValueError: If the schema has a badly formed ref
+        """
+
+        # Get this schema if it is not a ref, or a new schema pointed to by the ref
+        schema: SchemaBase = self.resolve(ref_prefix, known_schemas)
+
+        # We only do a deepcopy if the parameter says so AND this object has not been newly built
+        deep = deep and not schema == self
+
+        # Duplicate the schema, and update some of its values
+        duplicate: SchemaBase = schema.copy(update, deep=deep)
+
+        # We copy and resolve the list of schema
+        if duplicate.anyOf is not None:
+            duplicate.anyOf = [s.recursive_resolve(ref_prefix, known_schemas, update, deep=False) for s in duplicate.anyOf]
+
+        return duplicate
+
 
 SchemaBase.update_forward_refs()
 
@@ -76,6 +133,29 @@ class Schema(SchemaBase):
     items: Optional[Union["Schema", SchemaBase]] = None
     properties: Optional[Dict[str, Union["Schema", SchemaBase]]] = None
     additionalProperties: Optional[Union["Schema", SchemaBase, bool]] = None
+
+    def from_dict(self: S, schema: Dict[str, Any]) -> S:
+        return Schema(**schema)
+
+    def recursive_resolve(
+        self: S, ref_prefix: str, known_schemas: Dict[str, Any], update: Dict[str, Any], deep: bool = True
+    ) -> S:
+        duplicate: Schema = super().recursive_resolve(ref_prefix, known_schemas, update, deep=deep)
+
+        if duplicate.items is not None:
+            duplicate.items = duplicate.items.recursive_resolve(ref_prefix, known_schemas, update, deep=False)
+
+        if duplicate.properties is not None:
+            duplicate.properties = {
+                k: s.recursive_resolve(ref_prefix, known_schemas, update, deep=False) for k, s in duplicate.properties.items()
+            }
+
+        if duplicate.additionalProperties is not None and not isinstance(duplicate.additionalProperties, bool):
+            duplicate.additionalProperties = duplicate.additionalProperties.recursive_resolve(
+                ref_prefix, known_schemas, update, deep=False
+            )
+
+        return duplicate
 
 
 Schema.update_forward_refs()
