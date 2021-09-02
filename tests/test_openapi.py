inmanta/inmanta-core

commit 3bd93bc25fdd017b6e63da43534a244d4b14babc

Fixed documentation rendering (OpenAPI) bug where AnyUrl types are shown as object instead of string. (Issue #3192, PR #3193)
# Description

Fixing formating of AnyUrl types in the documentation (openapi and swagger).

closes #3192 and inmanta/inmanta-lsm#451

# Self Check:

Strike through any lines that are not applicable (`~~line~~`) then check the box

- [x] Attached issue to pull request
- [x] Changelog entry
- [x] Type annotations are present
- [x] Code is clear and sufficiently documented
- [x] No (preventable) type errors (check using make mypy or make mypy-diff)
- [x] Sufficient test cases (reproduces the bug/tests the requested feature)
- [x] Correct, in line with design
- [x] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )

# Reviewer Checklist:

- [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
- [ ] Code is clear and sufficiently documented
- [ ] Correct, in line with design

src/inmanta/protocol/openapi/converter.py
@@ -17,18 +17,14 @@
 """
 import inspect
 import json
-import typing
-import uuid
-from datetime import datetime
-from enum import Enum
+import re
 from typing import Callable, Dict, List, Optional, Type, Union
 
-import typing_inspect  # type: ignore
-from pydantic.main import BaseModel
 from pydantic.networks import AnyUrl
 from pydantic.schema import model_schema
 
-from inmanta import types, util
+from inmanta import util
+from inmanta.data.model import BaseModel, patch_pydantic_field_type_schema
 from inmanta.protocol.common import ArgOption, MethodProperties, UrlMethod
 from inmanta.protocol.openapi.model import (
     Components,
@@ -155,83 +151,54 @@ class OpenApiTypeConverter:
     Lookup for OpenAPI types corresponding to python types
     """
 
-    components = Components(schemas={})
+    def __init__(self):
+        self.components = Components(schemas={})
+        self.ref_prefix = "#/components/schemas/"
+        self.ref_regex = re.compile(self.ref_prefix + r"(.*)")
 
-    python_to_openapi_types = {
-        bool: Schema(type="boolean"),
-        int: Schema(type="integer"),
-        str: Schema(type="string"),
-        tuple: Schema(type="array", items=Schema()),
-        list: Schema(type="array", items=Schema()),
-        dict: Schema(type="object"),
-        float: Schema(type="number", format="float"),
-        bytes: Schema(type="string", format="binary"),
-        datetime: Schema(type="string", format="date-time"),
-        uuid.UUID: Schema(type="string", format="uuid"),
-        typing.Any: Schema(),
-        types.StrictNonIntBool: Schema(type="boolean"),
-    }
+        # Applying some monkey patching to pydantic
+        patch_pydantic_field_type_schema()
 
     def get_openapi_type_of_parameter(self, parameter_type: inspect.Parameter) -> Schema:
         type_annotation = parameter_type.annotation
         return self.get_openapi_type(type_annotation)
 
-    def _is_none_type(self, type_annotation: Type) -> bool:
-        return inspect.isclass(type_annotation) and issubclass(type_annotation, type(None))
-
-    def _handle_union_type(self, type_annotation: Type) -> Schema:
-        # An Optional is always a Union
-        type_args = typing_inspect.get_args(type_annotation, evaluate=True)
-        openapi_types = [self.get_openapi_type(type_arg) for type_arg in type_args if not self._is_none_type(type_arg)]
-        none_type_in_type_args = len(openapi_types) < len(type_args)
-        if none_type_in_type_args:
-            if len(openapi_types) == 1:
-                openapi_type = openapi_types[0].copy(deep=True)
-                # An Optional in the OpenAPI Schema is nullable
-                openapi_type.nullable = True
-                return openapi_type
-            return Schema(anyOf=openapi_types, nullable=True)
-        # A Union type can be expressed as a schema that matches any of the type arguments
-        return Schema(anyOf=openapi_types)
-
-    def _handle_dictionary(self, type_annotation: Type) -> Schema:
-        type_args = typing_inspect.get_args(type_annotation, evaluate=True)
-        return Schema(type="object", additionalProperties=self.get_openapi_type(type_args[1]))
-
-    def _handle_pydantic_model(self, type_annotation: Type) -> Schema:
+    def _handle_pydantic_model(self, type_annotation: Type, by_alias: bool = True) -> Schema:
         # JsonSchema stores the model (and sub-model) definitions at #/definitions,
         # but OpenAPI requires them to be placed at "#/components/schemas/"
         # The ref_prefix changes the references, but the actual schemas are still at #/definitions
-        schema = model_schema(type_annotation, by_alias=True, ref_prefix="#/components/schemas/")
+        schema = model_schema(type_annotation, by_alias=by_alias, ref_prefix=self.ref_prefix)
         if "definitions" in schema.keys():
             definitions = schema.pop("definitions")
             if self.components.schemas is not None:
                 self.components.schemas.update(definitions)
         return Schema(**schema)
 
-    def _handle_enums(self, type_annotation: Type) -> Schema:
-        enum_keys = [name for name in type_annotation.__members__.keys()]
-        return Schema(type="string", enum=enum_keys)
-
-    def _handle_list(self, type_annotation: Type) -> Schema:
-        # Type argument is always present, see protocol.common.MethodProperties._validate_type_arg()
-        list_member_type = typing_inspect.get_args(type_annotation, evaluate=True)
-        return Schema(type="array", items=self.get_openapi_type(list_member_type[0]))
-
     def get_openapi_type(self, type_annotation: Type) -> Schema:
-        type_origin = typing_inspect.get_origin(type_annotation)
-        if typing_inspect.is_union_type(type_annotation):
-            return self._handle_union_type(type_annotation)
-        elif inspect.isclass(type_annotation) and issubclass(type_annotation, BaseModel):
-            return self._handle_pydantic_model(type_annotation)
-        elif inspect.isclass(type_annotation) and issubclass(type_annotation, Enum):
-            return self._handle_enums(type_annotation)
-        elif inspect.isclass(type_origin) and issubclass(type_origin, typing.Mapping):
-            return self._handle_dictionary(type_annotation)
-        elif inspect.isclass(type_origin) and issubclass(type_origin, typing.Sequence):
-            return self._handle_list(type_annotation)
-        # Fallback to primitive types
-        return self.python_to_openapi_types.get(type_annotation, Schema(type="object"))
+        class Sub(BaseModel):
+            the_field: type_annotation
+
+            class Config:
+                arbitrary_types_allowed = True
+
+        pydantic_result = self._handle_pydantic_model(Sub).properties["the_field"]
+        pydantic_result.title = None
+        return pydantic_result
+
+    def resolve_reference(self, reference: str) -> Optional[Schema]:
+        """
+        Get a schema from its reference, if this schema exists.  If it doesn't exist, None is
+        returned instead.
+
+        :param reference: The reference to the schema
+        """
+        ref_match = self.ref_regex.match(reference)
+        if not ref_match:
+            return None
+
+        ref_key = ref_match.group(1)
+        schema = self.components.schemas[ref_key]
+        return Schema(**schema)
 
 
 class ArgOptionHandler:

src/inmanta/protocol/openapi/model.py
@@ -73,9 +73,12 @@ SchemaBase.update_forward_refs()
 
 
 class Schema(SchemaBase):
-    items: Optional[SchemaBase] = None
-    properties: Optional[Dict[str, SchemaBase]] = None
-    additionalProperties: Optional[Union[SchemaBase, bool]] = None
+    items: Optional[Union["Schema", SchemaBase]] = None
+    properties: Optional[Dict[str, Union["Schema", SchemaBase]]] = None
+    additionalProperties: Optional[Union["Schema", SchemaBase, bool]] = None
+
+
+Schema.update_forward_refs()
 
 
 class Example(BaseModel):

commit 139221818b4c46c326b988958fa93424f2f0854d

Improve openapi schema classes (PR #3217)
# Description

Added some helper methods to `SchemaBase` and `Schema` classes to make it easier resolve references.
This is a first step towards resolution of failures introduced to lsm by https://github.com/inmanta/inmanta-core/commit/3bd93bc25fdd017b6e63da43534a244d4b14babc

# Self Check:

Strike through any lines that are not applicable (`~~line~~`) then check the box

- [x] ~~Attached issue to pull request~~
- [x] Changelog entry
- [x] Type annotations are present
- [x] Code is clear and sufficiently documented
- [x] No (preventable) type errors (check using make mypy or make mypy-diff)
- [x] Sufficient test cases (reproduces the bug/tests the requested feature)
- [x] Correct, in line with design
- [x] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )

# Reviewer Checklist:

- [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
- [ ] Code is clear and sufficiently documented
- [ ] Correct, in line with design

src/inmanta/protocol/openapi/converter.py
@@ -40,7 +40,6 @@ from inmanta.protocol.openapi.model import (
     RequestBody,
     Response,
     Schema,
-    SchemaBase,
     Server,
 )
 from inmanta.server import config
@@ -169,9 +168,10 @@ class OpenApiTypeConverter:
         # The ref_prefix changes the references, but the actual schemas are still at #/definitions
         schema = model_schema(type_annotation, by_alias=by_alias, ref_prefix=self.ref_prefix)
         if "definitions" in schema.keys():
-            definitions = schema.pop("definitions")
+            definitions: Dict[str, Dict[str, object]] = schema.pop("definitions")
             if self.components.schemas is not None:
-                self.components.schemas.update(definitions)
+                for key, definition in definitions.items():
+                    self.components.schemas[key] = Schema(**definition)
         return Schema(**schema)
 
     def get_openapi_type(self, type_annotation: Type) -> Schema:
@@ -197,8 +197,7 @@ class OpenApiTypeConverter:
             return None
 
         ref_key = ref_match.group(1)
-        schema = self.components.schemas[ref_key]
-        return Schema(**schema)
+        return self.components.schemas[ref_key]
 
 
 class ArgOptionHandler:
@@ -412,7 +411,7 @@ class OperationHandler:
     def _build_return_value_wrapper(self, url_method_properties: MethodProperties) -> Optional[Dict[str, MediaType]]:
         return_type = inspect.signature(url_method_properties.function).return_annotation
         if return_type is not None and return_type != inspect.Signature.empty:
-            return_properties: Optional[Dict[str, SchemaBase]] = None
+            return_properties: Optional[Dict[str, Schema]] = None
             openapi_return_type = self.type_converter.get_openapi_type(return_type)
             if url_method_properties.envelope:
                 return_properties = {url_method_properties.envelope_key: openapi_return_type}

src/inmanta/protocol/openapi/model.py
@@ -21,7 +21,6 @@ https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md
 Inspired by FastAPI:
 https://github.com/tiangolo/fastapi
 """
-
 from enum import Enum
 from typing import Any, Dict, List, Optional, Sequence, Union
 
@@ -51,13 +50,14 @@ class Reference(BaseModel):
     ref: str = Field(..., alias="$ref")
 
 
-class SchemaBase(BaseModel):
+class Schema(BaseModel):
     ref: Optional[str] = Field(None, alias="$ref")
     title: Optional[str] = None
     required: Optional[List[str]] = None
     type: Optional[str] = None
-    items: Optional[Any] = None
-    properties: Optional[Dict[str, Any]] = None
+    items: Optional["Schema"] = None
+    properties: Optional[Dict[str, "Schema"]] = None
+    additionalProperties: Optional[Union["Schema", bool]] = None
     description: Optional[str] = None
     format: Optional[str] = None
     default: Optional[Any] = None
@@ -65,17 +65,74 @@ class SchemaBase(BaseModel):
     readOnly: Optional[bool] = None
     example: Optional[Any] = None
     deprecated: Optional[bool] = None
-    anyOf: Optional[Sequence["SchemaBase"]] = None
+    anyOf: Optional[Sequence["Schema"]] = None
     enum: Optional[List[str]] = None
 
+    def resolve(self, ref_prefix: str, known_schemas: Dict[str, "Schema"]) -> "Schema":
+        """
+        Returns this object or the one this object is refering to.
 
-SchemaBase.update_forward_refs()
+        :param ref_prefix: The prefix this object ref should have if it is only a reference
+        :param known_schemas: A dict of known schemas, the keys are the reference diminished from their prefix
 
+        :raises ValueError: If the schema has a badly formed ref
+        """
+        if not self.ref:
+            return self
 
-class Schema(SchemaBase):
-    items: Optional[Union["Schema", SchemaBase]] = None
-    properties: Optional[Dict[str, Union["Schema", SchemaBase]]] = None
-    additionalProperties: Optional[Union["Schema", SchemaBase, bool]] = None
+        if not self.ref.startswith(ref_prefix):
+            raise ValueError(f"Schema reference (={self.ref}) doesn't start with the expected prefix: '{ref_prefix}'")
+
+        reference = self.ref[len(ref_prefix) :]
+        return known_schemas[reference]
+
+    def recursive_resolve(
+        self, ref_prefix: str, known_schemas: Dict[str, "Schema"], update: Dict[str, Any], deep: bool = True
+    ) -> "Schema":
+        """
+        Returns this object of the one this object is refering to, and resolve all the nested schema it contains.
+
+        :param ref_prefix: The prefix this object ref should have if it is only a reference
+        :param known_schemas: A dict of known schemas, the keys are the reference diminished from their prefix
+        :param update: values to change/add in the new model.
+            Note: the data is not validated before creating the new model: you should trust this data
+            Note: this update is applied to every schema we resolve
+            Note: this is passed directly to BaseModel.copy method
+        :param deep: Whether to perform a deepcopy of the object
+
+        :raises ValueError: If the schema has a badly formed ref
+        """
+
+        # Get this schema if it is not a ref, or a new schema pointed to by the ref
+        schema = self.resolve(ref_prefix, known_schemas)
+
+        # We only do a deepcopy if the parameter says so AND this object has not been newly built
+        deep = deep and schema is not self
+
+        # Duplicate the schema, and update some of its values
+        duplicate = schema.copy(update=update, deep=deep)
+
+        # We copy and resolve the list of schema
+        if duplicate.anyOf is not None:
+            duplicate.anyOf = [s.recursive_resolve(ref_prefix, known_schemas, update, deep=False) for s in duplicate.anyOf]
+
+        # We copy and resolve the items if we have any
+        if duplicate.items is not None:
+            duplicate.items = duplicate.items.recursive_resolve(ref_prefix, known_schemas, update, deep=False)
+
+        # We copy and resolve the properties if we have any
+        if duplicate.properties is not None:
+            duplicate.properties = {
+                k: s.recursive_resolve(ref_prefix, known_schemas, update, deep=False) for k, s in duplicate.properties.items()
+            }
+
+        # We copy and resolve the additionalProperties if we have any
+        if duplicate.additionalProperties is not None and not isinstance(duplicate.additionalProperties, bool):
+            duplicate.additionalProperties = duplicate.additionalProperties.recursive_resolve(
+                ref_prefix, known_schemas, update, deep=False
+            )
+
+        return duplicate
 
 
 Schema.update_forward_refs()
@@ -170,7 +227,7 @@ class PathItem(BaseModel):
 
 
 class Components(BaseModel):
-    schemas: Optional[Dict[str, Union[Schema, Reference]]] = None
+    schemas: Optional[Dict[str, Schema]] = None
     responses: Optional[Dict[str, Union[Response, Reference]]] = None
     parameters: Optional[Dict[str, Union[Parameter, Reference]]] = None
     examples: Optional[Dict[str, Union[Example, Reference]]] = None

tests/test_openapi.py
@@ -38,7 +38,7 @@ from inmanta.protocol.openapi.converter import (
     OpenApiTypeConverter,
     OperationHandler,
 )
-from inmanta.protocol.openapi.model import MediaType, Schema, SchemaBase
+from inmanta.protocol.openapi.model import MediaType, Schema
 from inmanta.server import SLICE_SERVER
 from inmanta.server.extensions import FeatureManager
 from inmanta.server.protocol import Server
@@ -263,7 +263,7 @@ def test_openapi_types_base_model():
 def test_openapi_types_union():
     type_converter = OpenApiTypeConverter()
     openapi_type = type_converter.get_openapi_type(Union[str, bytes])
-    assert openapi_type == Schema(anyOf=[SchemaBase(type="string"), SchemaBase(type="string", format="binary")])
+    assert openapi_type == Schema(anyOf=[Schema(type="string"), Schema(type="string", format="binary")])
 
 
 def test_openapi_types_optional():
@@ -276,7 +276,7 @@ def test_openapi_types_list():
     type_converter = OpenApiTypeConverter()
     openapi_type = type_converter.get_openapi_type(List[Union[int, UUID]])
     assert openapi_type == Schema(
-        type="array", items=Schema(anyOf=[SchemaBase(type="integer"), SchemaBase(type="string", format="uuid")])
+        type="array", items=Schema(anyOf=[Schema(type="integer"), Schema(type="string", format="uuid")])
     )
 
 
@@ -286,7 +286,7 @@ def test_openapi_types_enum():
     assert openapi_type.type == "array"
     assert openapi_type.items.ref == type_converter.ref_prefix + "ResourceAction"
 
-    resource_action_type = Schema(**type_converter.components.schemas["ResourceAction"])
+    resource_action_type = type_converter.components.schemas["ResourceAction"]
     assert resource_action_type.type == "string"
     assert resource_action_type.enum == ["store", "push", "pull", "deploy", "dryrun", "getfact", "other"]
 
@@ -694,3 +694,68 @@ async def test_tags(server, feature_manager):
         for operation in path.values():
             assert len(operation["tags"]) > 0
     assert "core.project" in openapi_parsed["paths"]["/api/v1/project"]["get"]["tags"]
+
+
+def test_openapi_schema() -> None:
+    ref_prefix = "#/"
+    schemas = {
+        "person": Schema(
+            **{
+                "title": "Person",
+                "properties": {
+                    "address": {
+                        "$ref": ref_prefix + "address",
+                    },
+                    "age": {
+                        "title": "Age",
+                        "type": "integer",
+                    },
+                },
+            }
+        ),
+        "address": Schema(
+            **{
+                "title": "Address",
+                "properties": {
+                    "street": {
+                        "title": "Street",
+                        "type": "string",
+                    },
+                    "number": {
+                        "title": "Number",
+                        "type": "integer",
+                    },
+                    "city": {
+                        "title": "City",
+                        "type": "string",
+                    },
+                },
+            }
+        ),
+    }
+
+    assert schemas["person"] == Schema(
+        title="Person",
+        properties={
+            "address": Schema(**{"$ref": ref_prefix + "address"}),
+            "age": Schema(title="Age", type="integer"),
+        },
+    )
+
+    assert schemas["address"] == Schema(
+        title="Address",
+        properties={
+            "street": Schema(title="Street", type="string"),
+            "number": Schema(title="Number", type="integer"),
+            "city": Schema(title="City", type="string"),
+        },
+    )
+
+    assert Schema(**{"$ref": ref_prefix + "person"}).resolve(ref_prefix, schemas) == schemas["person"]
+    assert Schema(**{"$ref": ref_prefix + "address"}).resolve(ref_prefix, schemas) == schemas["address"]
+
+    person_schema = schemas["person"].copy(deep=True)
+
+    assert not Schema(**{"$ref": ref_prefix + "person"}).recursive_resolve(ref_prefix, schemas, update={}) == person_schema
+    person_schema.properties["address"] = schemas["address"]
+    assert Schema(**{"$ref": ref_prefix + "person"}).recursive_resolve(ref_prefix, schemas, update={}) == person_schema