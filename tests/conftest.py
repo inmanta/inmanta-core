356b52eb8af8c7f55e4ddcf6f7dc2e1eae7d6609
Sander Van Balen <git@sandervanbalen.be>
2021-11-10 16:02:57 +0100

    review request changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -411,13 +411,15 @@ def deactive_venv():
     sys.path = old_path
     pkg_resources.working_set = old_working_set
     # Restore PYTHONPATH
-    if "PYTHONPATH" in os.environ:
-        if old_pythonpath is not None:
-            os.environ["PYTHONPATH"] = old_pythonpath
-        else:
-            del os.environ["PYTHONPATH"]
+    if old_pythonpath is not None:
+        os.environ["PYTHONPATH"] = old_pythonpath
+    elif "PYTHONPATH" in os.environ:
+        del os.environ["PYTHONPATH"]
+    # Restore VIRTUAL_ENV
     if old_os_venv is not None:
         os.environ["VIRTUAL_ENV"] = old_os_venv
+    elif "VIRTUAL_ENV" in os.environ:
+        del os.environ["VIRTUAL_ENV"]
     env.mock_process_env(python_path=old_process_env)
     loader.PluginModuleFinder.reset()
 
