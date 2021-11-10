069bb54460ffbc58ef95f4622ee4cae291287b81
Sander Van Balen <git@sandervanbalen.be>
2021-11-10 10:22:33 +0100

    added import test

--- tests/test_import_entry_points.py
+++ tests/test_import_entry_points.py
@@ -106,3 +106,7 @@ def test_import_util(import_entry_point: Callable[[str], Optional[int]]) -> None
 
 def test_import_ast(import_entry_point: Callable[[str], Optional[int]]) -> None:
     assert import_entry_point("inmanta.ast.constraint.expression") == 0
+
+
+def test_import_env(import_entry_point: Callable[[str], Optional[int]]) -> None:
+    assert import_entry_point("inmanta.env") == 0
