fa0c6d17cc5ed27003a4204801346ef5b6fac1b5
Sander Van Balen <git@sandervanbalen.be>
2021-11-15 12:09:28 +0100

    fix

--- src/inmanta/module.py
+++ src/inmanta/module.py
@@ -992,7 +992,8 @@ class ModuleV1Metadata(ModuleMetadata, MetadataFieldRequires):
 
     def to_v2(self) -> "ModuleV2Metadata":
         values = self.dict()
-        values["description"] = values["description"].replace("\n", " ")
+        if values["description"] is not None:
+            values["description"] = values["description"].replace("\n", " ")
         del values["compiler_version"]
         install_requires = [ModuleV2Source.get_package_name_for(r) for r in values["requires"]]
         del values["requires"]
