eb0bc547fb5775bf66a8e75711992f47a5072c8e
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2021-11-18 15:39:24 +0100

    Use ReportReturn type

--- src/inmanta/data/model.py
+++ src/inmanta/data/model.py
@@ -19,7 +19,7 @@ import datetime
 import uuid
 from enum import Enum
 from itertools import chain
-from typing import Any, ClassVar, Dict, List, NewType, Optional, Union
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, List, NewType, Optional, Union
 
 import pydantic
 import pydantic.schema
@@ -29,6 +29,9 @@ import inmanta.ast.export as ast_export
 from inmanta import const
 from inmanta.stable_api import stable_api
 from inmanta.types import ArgumentTypes, JsonType, SimpleTypes, StrictNonIntBool
+
+if TYPE_CHECKING:
+    from inmanta.agent.reporting import ReportReturn
 
 # This reference to the actual pydantic field_type_schema method is only loaded once
 old_field_type_schema = pydantic.schema.field_type_schema
@@ -487,4 +490,4 @@ class AgentProcess(BaseModel):
 
 
 class AgentProcessDetails(AgentProcess):
-    state: Optional[Union[Dict[str, List[str]], Dict[str, str], Dict[str, float], str]]
+    state: Optional["ReportReturn"]
