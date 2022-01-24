5c4d31b5889d4c10506928d86583dd2d351227b8
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-01-24 12:51:09 +0100

    Added endpoint to list facts (Issue #3663, PR #3700)
    # Description
    
    closes #3663
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/inmanta/protocol/methods_v2.py
+++ src/inmanta/protocol/methods_v2.py
@@ -993,3 +993,45 @@ def get_parameters(
     :raise NotFound: This exception is raised when the referenced environment is not found
     :raise BadRequest: When the parameters used for filtering, sorting or paging are not valid
     """
+
+
+@typedmethod(
+    path="/facts",
+    operation="GET",
+    arg_options=methods.ENV_OPTS,
+    client_types=[ClientType.api],
+    api_version=2,
+)
+def get_all_facts(
+    tid: uuid.UUID,
+    limit: Optional[int] = None,
+    first_id: Optional[uuid.UUID] = None,
+    last_id: Optional[uuid.UUID] = None,
+    start: Optional[str] = None,
+    end: Optional[str] = None,
+    filter: Optional[Dict[str, List[str]]] = None,
+    sort: str = "name.asc",
+) -> List[model.Fact]:
+    """
+    List the facts in an environment
+    :param tid: The id of the environment
+    :param limit: Limit the number of facts that are returned
+    :param first_id: The fact id to use as a continuation token for paging, in combination with the 'start' value,
+            because the order by column might contain non-unique values
+    :param last_id: The fact id to use as a continuation token for paging, in combination with the 'end' value,
+            because the order by column might contain non-unique values
+    :param start: The lower limit for the order by column (exclusive).
+                Only one of 'start' and 'end' should be specified at the same time.
+    :param end: The upper limit for the order by column (exclusive).
+                Only one of 'start' and 'end' should be specified at the same time.
+    :param filter: Filter the list of returned facts.
+                The following options are available:
+                name: filter by the name of the fact
+                resource_id: filter by the resource_id of the fact
+    :param sort: Return the results sorted according to the parameter value.
+                The following sorting attributes are supported: 'name', 'resource_id'.
+                The following orders are supported: 'asc', 'desc'
+    :return: A list of all matching facts
+    :raise NotFound: This exception is raised when the referenced environment is not found
+    :raise BadRequest: When the parameters used for filtering, sorting or paging are not valid
+    """
--- tests/server/test_facts_api.py
+++ tests/server/test_facts_api.py
@@ -15,21 +15,22 @@
 
     Contact: code@inmanta.com
 """
+import json
 import uuid
 from datetime import datetime
-from typing import Dict, Optional
+from operator import itemgetter
+from typing import Dict, List, Optional, Tuple
 
 import pytest
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 
 from inmanta import data
 from inmanta.data.model import ResourceVersionIdStr
+from inmanta.server.config import get_bind_port
 
 
-@pytest.mark.asyncio
-async def test_get_facts(client, environment):
-    """
-    Test retrieving facts via the API
-    """
+@pytest.fixture
+async def env_with_facts(environment, client) -> Tuple[str, List[str], List[str]]:
     env_id = uuid.UUID(environment)
     version = 1
     await data.ConfigurationModel(
@@ -56,8 +57,20 @@ async def test_get_facts(client, environment):
     ).insert()
     resource_id_3 = "std::File[agent1,path=/etc/file3]"
 
+    path = "/tmp/filex"
+    resource_id_4 = f"std::File[agent1,path={path}]"
+    await data.Resource.new(
+        environment=env_id,
+        resource_version_id=ResourceVersionIdStr(f"{resource_id_4},v={version}"),
+        attributes={"path": path},
+    ).insert()
+
     async def insert_param(
-        name: str, resource_id: str, updated: Optional[datetime] = None, metadata: Optional[Dict[str, str]] = None
+        name: str,
+        resource_id: Optional[str] = None,
+        source: str = "fact",
+        updated: Optional[datetime] = None,
+        metadata: Optional[Dict[str, str]] = None,
     ) -> uuid.UUID:
         param_id = uuid.uuid4()
         await data.Parameter(
@@ -65,7 +78,7 @@ async def test_get_facts(client, environment):
             name=name,
             value="42",
             environment=env_id,
-            source="fact",
+            source=source,
             resource_id=resource_id,
             updated=updated,
             metadata=metadata,
@@ -77,31 +90,198 @@ async def test_get_facts(client, environment):
     param_id_3 = await insert_param(
         "param_for_other_resource", resource_id_2, updated=datetime.now(), metadata={"very_important_metadata": "123"}
     )
+    for i in range(5):
+        await insert_param(
+            f"param_for_new_resource{i}",
+            resource_id_4,
+            updated=datetime.now() if i % 2 else None,
+            metadata={"new_metadata": "42"} if i % 3 else None,
+        )
+    await insert_param("param_not_related_to_resource", source="plugin")
+    yield environment, [param_id_1, param_id_3], [resource_id, resource_id_2, resource_id_3, resource_id_4]
+
+
+@pytest.mark.asyncio
+async def test_get_facts(client, env_with_facts):
+    """
+    Test retrieving facts via the API
+    """
+    environment, param_ids, resource_ids = env_with_facts
 
     # Query fact list
-    result = await client.get_facts(environment, resource_id)
+    result = await client.get_facts(environment, resource_ids[0])
     assert result.code == 200
     assert len(result.result["data"]) == 2
 
-    result = await client.get_facts(environment, resource_id_2)
+    result = await client.get_facts(environment, resource_ids[1])
     assert result.code == 200
     assert len(result.result["data"]) == 1
 
     # Query facts for a resource that doesn't exist
-    result = await client.get_facts(environment, resource_id_3)
+    result = await client.get_facts(environment, resource_ids[2])
     assert result.code == 200
     assert len(result.result["data"]) == 0
 
     # Query a single fact
-    result = await client.get_fact(environment, resource_id, param_id_1)
+    result = await client.get_fact(environment, resource_ids[0], param_ids[0])
     assert result.code == 200
     assert result.result["data"]["name"] == "param"
     assert result.result["data"]["value"] == "42"
 
     # Query a single fact with mismatching resource id
-    result = await client.get_fact(environment, resource_id, param_id_3)
+    result = await client.get_fact(environment, resource_ids[0], param_ids[1])
     assert result.code == 404
 
     # Query a single not existing fact
-    result = await client.get_fact(environment, resource_id, uuid.uuid4())
+    result = await client.get_fact(environment, resource_ids[0], uuid.uuid4())
     assert result.code == 404
+
+
+@pytest.mark.asyncio
+async def test_fact_list_filters(client, env_with_facts: Tuple[str, List[str], List[str]]):
+    environment, param_ids, resource_ids = env_with_facts
+    result = await client.get_all_facts(
+        environment,
+    )
+    assert result.code == 200
+    assert len(result.result["data"]) == 8
+    # Only facts are returned by this endpoint
+    assert all(fact["source"] == "fact" for fact in result.result["data"])
+
+    result = await client.get_all_facts(environment, filter={"name": "other"})
+    assert result.code == 200
+    assert len(result.result["data"]) == 1
+    assert result.result["data"][0]["name"] == "param_for_other_resource"
+    result = await client.get_all_facts(environment, filter={"resource_id": resource_ids[3]})
+    assert result.code == 200
+    assert len(result.result["data"]) == 5
+    assert all(fact["resource_id"] == resource_ids[3] for fact in result.result["data"])
+
+    result = await client.get_all_facts(environment, filter={"updated": f"gt:{datetime.now()}"})
+    assert result.code == 400
+
+    result = await client.get_all_facts(environment, filter={"resource_id": resource_ids[2]})
+    assert result.code == 200
+    assert len(result.result["data"]) == 0
+
+    result = await client.get_all_facts(
+        uuid.uuid4(),
+    )
+    assert result.code == 404
+
+
+def fact_ids(fact_objects):
+    return [fact["id"] for fact in fact_objects]
+
+
+@pytest.mark.parametrize("order_by_column", ["name", "resource_id"])
+@pytest.mark.parametrize("order", ["DESC", "ASC"])
+@pytest.mark.asyncio
+async def test_facts_paging(server, client, order_by_column, order, env_with_facts):
+    """ Test querying facts with paging, using different sorting parameters."""
+    env, _, _ = env_with_facts
+    result = await client.get_all_facts(
+        env,
+        filter={"name": "res"},
+    )
+    assert result.code == 200
+    assert len(result.result["data"]) == 6
+    all_facts_in_expected_order = sorted(result.result["data"], key=itemgetter(order_by_column, "id"), reverse=order == "DESC")
+    all_fact_ids_in_expected_order = fact_ids(all_facts_in_expected_order)
+
+    result = await client.get_all_facts(env, limit=2, sort=f"{order_by_column}.{order}", filter={"name": "res"})
+    assert result.code == 200
+    assert len(result.result["data"]) == 2
+    assert fact_ids(result.result["data"]) == all_fact_ids_in_expected_order[:2]
+
+    assert result.result["metadata"] == {"total": 6, "before": 0, "after": 4, "page_size": 2}
+    assert result.result["links"].get("next") is not None
+    assert result.result["links"].get("prev") is None
+
+    port = get_bind_port()
+    base_url = "http://localhost:%s" % (port,)
+    http_client = AsyncHTTPClient()
+
+    # Test link for next page
+    url = f"""{base_url}{result.result["links"]["next"]}"""
+    assert "limit=2" in url
+    assert "filter.name=res" in url
+    request = HTTPRequest(
+        url=url,
+        headers={"X-Inmanta-tid": env},
+    )
+    response = await http_client.fetch(request, raise_error=False)
+    assert response.code == 200
+    response = json.loads(response.body.decode("utf-8"))
+    assert fact_ids(response["data"]) == all_fact_ids_in_expected_order[2:4]
+    assert response["links"].get("prev") is not None
+    assert response["links"].get("next") is not None
+    assert response["metadata"] == {"total": 6, "before": 2, "after": 2, "page_size": 2}
+
+    # Test link for next page
+    url = f"""{base_url}{response["links"]["next"]}"""
+    # The filters should be present for the links as well
+    assert "limit=2" in url
+    assert "filter.name=res" in url
+    request = HTTPRequest(
+        url=url,
+        headers={"X-Inmanta-tid": env},
+    )
+    response = await http_client.fetch(request, raise_error=False)
+    assert response.code == 200
+    response = json.loads(response.body.decode("utf-8"))
+    next_page_ids = fact_ids(response["data"])
+    assert next_page_ids == all_fact_ids_in_expected_order[4:]
+    assert response["links"].get("prev") is not None
+    assert response["links"].get("next") is None
+    assert response["metadata"] == {"total": 6, "before": 4, "after": 0, "page_size": 2}
+
+    # Test link for previous page
+    url = f"""{base_url}{response["links"]["prev"]}"""
+    assert "limit=2" in url
+    assert "filter.name=res" in url
+    request = HTTPRequest(
+        url=url,
+        headers={"X-Inmanta-tid": env},
+    )
+    response = await http_client.fetch(request, raise_error=False)
+    assert response.code == 200
+    response = json.loads(response.body.decode("utf-8"))
+    prev_page_ids = fact_ids(response["data"])
+    assert prev_page_ids == all_fact_ids_in_expected_order[2:4]
+    assert response["links"].get("prev") is not None
+    assert response["links"].get("next") is not None
+    assert response["metadata"] == {"total": 6, "before": 2, "after": 2, "page_size": 2}
+
+
+@pytest.mark.asyncio
+async def test_sorting_validation(server, client, env_with_facts):
+    env, _, _ = env_with_facts
+    sort_status_map = {
+        "agents.Desc": 400,
+        "name.asc": 200,
+        "version.desc": 400,
+        "name": 400,
+        "resourceid.asc": 400,
+        "res_id.asc": 400,
+        "resource_id.asc": 200,
+    }
+    for sort, expected_status in sort_status_map.items():
+        result = await client.get_all_facts(env, sort=sort)
+        assert result.code == expected_status
+
+
+@pytest.mark.asyncio
+async def test_filter_validation(server, client, env_with_facts):
+    env, _, _ = env_with_facts
+    filter_status_map = [
+        ("name.desc", 400),
+        ({"name": ["file1", "res2"]}, 200),
+        ({"resource_id": [1, 2]}, 200),
+        ({"updated": "le:42"}, 400),
+        ({"source": "internal"}, 400),
+        ({"updated": True}, 400),
+    ]
+    for filter, expected_status in filter_status_map:
+        result = await client.get_all_facts(env, filter=filter)
+        assert result.code == expected_status
