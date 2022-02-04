74a5c2176051a44912506e9076ea76c11ace70fd
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-02-04 15:01:05 +0100

    Fix

--- src/inmanta/protocol/methods_v2.py
+++ src/inmanta/protocol/methods_v2.py
@@ -1100,19 +1100,18 @@ def list_dryruns(tid: uuid.UUID, version: int) -> List[model.DryRun]:
 
 
 @typedmethod(
-    path="/dryruns/<version>/<report_id>",
+    path="/dryrun/<report_id>",
     operation="GET",
     arg_options=methods.ENV_OPTS,
     client_types=[ClientType.api],
     api_version=2,
 )
-def get_dryrun_diff(tid: uuid.UUID, version: int, report_id: uuid.UUID) -> model.DryRunReport:
+def get_dryrun_diff(tid: uuid.UUID, report_id: uuid.UUID) -> model.DryRunReport:
     """
     Get the report of a dryrun, describing the changes a deployment would make,
     with the difference between the current and target states provided in a form similar to the desired state diff endpoint.
 
     :param tid: The id of the environment
-    :param version: The version of the configuration model the dryrun belongs to
     :param report_id: The dryrun id to calculate the diff for
     :raise NotFound: This exception is raised when the referenced environment or version is not found
     :return: The dryrun report, with a summary and the list of differences.
