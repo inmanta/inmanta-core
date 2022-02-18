e9dba155f5d4578f2c1cd2be650b924169826f4e
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-02-18 13:39:50 +0100

    [FIX] disallow newline in STRING

--- src/inmanta/parser/plyInmantaLex.py
+++ src/inmanta/parser/plyInmantaLex.py
@@ -184,7 +184,7 @@ def t_INT(t: lex.LexToken) -> lex.LexToken:  # noqa: N802
 
 
 def t_STRING(t: lex.LexToken) -> lex.LexToken:  # noqa: N802
-    r"(\"([^\\\"]|\\.)*\")|(\'([^\\\']|\\.)*\')"
+    r"(\"([^\\\"\r\n]|\\.)*\")|(\'([^\\\'\r\n]|\\.)*\')"
     t.value = bytes(t.value[1:-1], "utf-8").decode("unicode_escape")
     lexer = t.lexer
 
