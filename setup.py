8458961fe2f1aab7de9cbd664c579644af36d8e5
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-02-28 16:28:11 +0100

    Update python versions in the setup.py classifiers (Issue #3851, PR #3917)
    # Description
    
    Update python versions in the setup.py classifiers
    
    closes #3851
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    ~~- [ ] Type annotations are present~~
    ~~- [ ] Code is clear and sufficiently documented~~
    ~~- [ ] No (preventable) type errors (check using make mypy or make mypy-diff)~~
    ~~- [ ] Sufficient test cases (reproduces the bug/tests the requested feature)~~
    ~~- [ ] Correct, in line with design~~
    ~~- [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- setup.py
+++ setup.py
@@ -62,9 +62,10 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Topic :: System :: Systems Administration",
         "Topic :: Utilities",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="orchestrator orchestration configurationmanagement",
     project_urls={
