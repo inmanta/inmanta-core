94796e8d87cf7a3abaa5ae777fb48d6583f56d20
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-02-28 08:48:13 +0100

    [FEAT] update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -65,6 +65,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="orchestrator orchestration configurationmanagement",
     project_urls={
