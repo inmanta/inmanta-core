9388edd8d1cba2f3e2e863abf22169421e44a72d
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-02-28 10:21:46 +0100

    [FEAT] remove python 3.5 from the classifiers

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Topic :: System :: Systems Administration",
         "Topic :: Utilities",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
