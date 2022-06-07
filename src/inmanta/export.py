ebeec5fe8463bbcb89b69fe59a043bf4029df9d4
Lejoly Florent <florent.lejoly@inmanta.com>
2022-06-07 11:31:47 +0200

    update

--- src/inmanta/export.py
+++ src/inmanta/export.py
@@ -107,6 +107,7 @@ class Exporter(object):
     # instance vars
     types: Optional[Dict[str, Entity]]
     scopes: Optional[Namespace]
+    resource_mapping: Optional[Dict[str, "Resource"]]
     failed: bool  # did the compile fail?
 
     # class vars
@@ -187,7 +188,7 @@ class Exporter(object):
                             entity,
                             instance.location,
                         )
-
+        self.resource_mapping = resource_mapping
         Resource.convert_requires(resource_mapping, ignored_set)
 
     def _run_export_plugins_specified_in_config_file(self) -> None:
@@ -452,9 +453,7 @@ class Exporter(object):
             name: str = resource_set_instance.get_attribute("name").value
             resources_in_set: ResultVariable = resource_set_instance.get_attribute("resources")
             for resource_in_set in resources_in_set.value:
-                resource_id: str = Resource.create_from_model(
-                    self, str(resource_in_set.type), DynamicProxy.return_value(resource_in_set)
-                ).id.resource_str()
+                resource_id: str = self.resource_mapping[resource_in_set].id.resource_str()
                 if resource_id in resource_sets and resource_sets[resource_id] != name:
                     raise CompilerException("resource '%s' can not be part of multiple ResourceSets" % resource_id)
                 resource_sets[resource_id] = name
