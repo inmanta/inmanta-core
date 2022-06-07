db10e1f5245b0559693eb792312492996f6f008a
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-06-07 10:38:13 +0200

    Make sure that the `inmanta project install` command doesn't protect the inmanta-dev-dependencies package (Issue #4249, PR #4314)
    # Description
    
    Make sure that the `inmanta project install` command doesn't protect the `inmanta-dev-dependencies` package.
    
    closes #4249
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation~~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- tests/test_module_loader.py
+++ tests/test_module_loader.py
@@ -26,7 +26,7 @@ import pytest
 from pkg_resources import Requirement
 
 from inmanta import plugins
-from inmanta.env import LocalPackagePath
+from inmanta.env import LocalPackagePath, process_env
 from inmanta.module import (
     DummyProject,
     InmantaModuleRequirement,
@@ -545,6 +545,14 @@ def test_project_requirements_dont_overwrite_core_requirements_source(
     but with another version. The requirements of core should not be
     overwritten. The module gets installed from source
     """
+    if "inmanta-core" in process_env.get_installed_packages(only_editable=True):
+        pytest.skip(
+            "This test would fail if it runs against an inmanta-core installed in editable mode, because the build tag "
+            "on the development branch is set to .dev0. The inmanta package protection feature would make pip "
+            "install a non-editable version of the same package. But no version with build tag .dev0 exists on the python "
+            "package repository."
+        )
+
     # Create the module
     module_name: str = "minimalv2module"
     module_path: str = str(tmpdir.join(module_name))
@@ -576,6 +584,13 @@ def test_project_requirements_dont_overwrite_core_requirements_index(
     but with another version. The requirements of core should not be
     overwritten. The module gets installed from index.
     """
+    if "inmanta-core" in process_env.get_installed_packages(only_editable=True):
+        pytest.skip(
+            "This test would fail if it runs against an inmanta-core installed in editable mode, because the build tag "
+            "on the development branch is set to .dev0. The inmanta package protection feature would make pip "
+            "install a non-editable version of the same package. But no version with build tag .dev0 exists on the python "
+            "package repository."
+        )
     # Create the module
     module_name: str = "minimalv2module"
     module_path: str = str(tmpdir.join(module_name))
