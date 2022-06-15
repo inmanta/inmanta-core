9c286850619e9590bf41cce02129e5d388c02a0f
Lejoly Florent <florent.lejoly@inmanta.com>
2022-06-15 17:01:43 +0200

    update

--- tests/agent_server/test_resource_sets.py
+++ tests/agent_server/test_resource_sets.py
@@ -520,3 +520,149 @@ async def test_put_partial_dependency_graph(server, client, environment, clienth
         "Invalid request: The model should have a dependency graph that is closed and no dangling dependencies: "
         "{'test::Resource[agent1,key=key2]'}"
     )
+
+
+async def test_put_partial_mixed_scenario(server, client, environment, clienthelper):
+    """
+    A test that starts with: resources in several different resource sets and resources in the shared set.
+    The partial update does: An update of a subset of the resources sets an addition of shared resources
+    """
+    version = await clienthelper.get_version()
+    resources = [
+        {
+            "key": "key1",
+            "value": "1",
+            "id": "test::Resource[agent1,key=key1],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+        {
+            "key": "key2",
+            "value": "2",
+            "id": "test::Resource[agent1,key=key2],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": ["test::Resource[agent1,key=key1]"],
+        },
+        {
+            "key": "key3",
+            "value": "3",
+            "id": "test::Resource[agent1,key=key3],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+        {
+            "key": "key4",
+            "value": "4",
+            "id": "test::Resource[agent1,key=key4],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": ["test::Resource[agent1,key=key3]"],
+        },
+        {
+            "key": "key5",
+            "value": "5",
+            "id": "test::Resource[agent1,key=key5],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+        {
+            "key": "key6",
+            "value": "6",
+            "id": "test::Resource[agent1,key=key6],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+    ]
+    resource_sets = {
+        "test::Resource[agent1,key=key1]": "set-a",
+        "test::Resource[agent1,key=key2]": "set-a",
+        "test::Resource[agent1,key=key3]": "set-b",
+        "test::Resource[agent1,key=key4]": "set-b",
+    }
+
+    result = await client.put_version(
+        tid=environment,
+        version=version,
+        resources=resources,
+        resource_state={},
+        unknowns=[],
+        version_info={},
+        compiler_version=get_compiler_version(),
+        resource_sets=resource_sets,
+    )
+    assert result.code == 200
+    version = await clienthelper.get_version()
+    resources_partial = [
+        {
+            "key": "key1",
+            "value": "100",
+            "id": "test::Resource[agent1,key=key1],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+        {
+            "key": "key2",
+            "value": "200",
+            "id": "test::Resource[agent1,key=key2],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+        {
+            "key": "key7",
+            "value": "700",
+            "id": "test::Resource[agent1,key=key7],v=%d" % version,
+            "send_event": False,
+            "purged": False,
+            "requires": [],
+        },
+    ]
+
+    result = await client.put_partial(
+        tid=environment,
+        version=version,
+        resources=resources_partial,
+        resource_state={},
+        unknowns=[],
+        version_info=None,
+        compiler_version=get_compiler_version(),
+        resource_sets={
+            "test::Resource[agent1,key=key1]": "set-a",
+            "test::Resource[agent1,key=key2]": "set-a",
+            "test::Resource[agent1,key=key3]": "set-b",
+            "test::Resource[agent1,key=key4]": "set-b",
+        },
+    )
+
+    assert result.code == 200
+    resource_list = await data.Resource.get_resources_in_latest_version(uuid.UUID(environment))
+    resource_sets_from_db = {resource.resource_id: resource.resource_set for resource in resource_list}
+    assert len(resource_list) == 7
+    assert resource_list[0].attributes == {"key": "key1", "value": "100", "purged": False, "requires": [], "send_event": False}
+    assert resource_list[1].attributes == {"key": "key2", "value": "200", "purged": False, "requires": [], "send_event": False}
+    assert resource_list[2].attributes == {"key": "key3", "value": "3", "purged": False, "requires": [], "send_event": False}
+    assert resource_list[3].attributes == {
+        "key": "key4",
+        "value": "4",
+        "purged": False,
+        "requires": ["test::Resource[agent1,key=key3]"],
+        "send_event": False,
+    }
+    assert resource_list[4].attributes == {"key": "key5", "value": "5", "purged": False, "requires": [], "send_event": False}
+    assert resource_list[5].attributes == {"key": "key6", "value": "6", "purged": False, "requires": [], "send_event": False}
+    assert resource_list[6].attributes == {"key": "key7", "value": "700", "purged": False, "requires": [], "send_event": False}
+    assert resource_sets_from_db == {
+        "test::Resource[agent1,key=key1]": "set-a",
+        "test::Resource[agent1,key=key2]": "set-a",
+        "test::Resource[agent1,key=key3]": "set-b",
+        "test::Resource[agent1,key=key4]": "set-b",
+        "test::Resource[agent1,key=key5]": None,
+        "test::Resource[agent1,key=key6]": None,
+        "test::Resource[agent1,key=key7]": None,
+    }
