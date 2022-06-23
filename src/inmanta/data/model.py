c8f5cde4297b840c12d36ae2c828ef0dc41bddac
Lejoly Florent <florent.lejoly@inmanta.com>
2022-06-23 16:15:36 +0200

    update

--- src/inmanta/data/model.py
+++ src/inmanta/data/model.py
@@ -29,7 +29,6 @@ from pydantic.fields import ModelField
 import inmanta
 import inmanta.ast.export as ast_export
 from inmanta import const, protocol
-from inmanta.resources import Id
 from inmanta.stable_api import stable_api
 from inmanta.types import ArgumentTypes, JsonType, SimpleTypes, StrictNonIntBool
 
@@ -312,6 +311,8 @@ class ResourceMinimal(BaseModel):
     @classmethod
     @validator("id")
     def name_must_contain_space(cls, v):
+        from inmanta.resources import Id  # prevent circular import
+
         if Id.is_resource_version_id(v):
             return v
         raise ValueError(f"id {v} is not of type ResourceVersionIdStr")
@@ -324,6 +325,8 @@ class ResourceMinimal(BaseModel):
         takes a resource and return the same resource with it version incremented
         (the input resource is modified)
         """
+        from inmanta.resources import Id  # prevent circular import
+
         old_res = Id.parse_id(self.id)
         new_res = Id(old_res.entity_type, old_res.agent_name, old_res.attribute, old_res.attribute_value, old_res.version + 1)
         self.id = new_res.resource_version_str()
