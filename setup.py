879dd7b4e0ec2cfe73108d832272b13ce4068b62
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-06-27 11:30:38 +0200

    [FIX] fix chardet dependency issue

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ requires = [
     "tornado~=6.0",
     "typing_inspect~=0.7",
     "build~=0.7",
+    # This extra can be removed when this issue is resolved: https://github.com/psf/requests/issues/6177
+    "requests[use_chardet_on_py3]",
     "ruamel.yaml~=0.17",
     "toml~=0.10 ",
 ]
