be59dc2464e4cff9c84590917d6e940eaa81e6d1
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-06-28 09:17:45 +0200

    Fix race condition in test_environment_settings. (PR #4466)
    # Description
    
    fix race condition in test environment settings
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    ~~- [ ] Attached issue to pull request~~
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    ~~- [ ] Sufficient test cases (reproduces the bug/tests the requested feature)~~
    - [x] Correct, in line with design
    ~~- [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- tests/server/test_env_settings.py
+++ tests/server/test_env_settings.py
@@ -15,10 +15,25 @@
 
     Contact: code@inmanta.com
 """
+from typing import Dict
+
 import pytest
 
 from inmanta import data
 from inmanta.util import get_compiler_version
+
+
+def get_environment_setting_default(setting: str) -> object:
+    return data.Environment._settings[setting].default
+
+
+def check_only_contains_default_setting(settings_dict: Dict[str, object]) -> None:
+    """
+    Depending on when the background cleanup processes are run, it is possible that environment settings are set, independently
+    of the tests below. This method ensures these settings are properly set with their default values.
+    """
+    for setting_name, setting_value in settings_dict.items():
+        assert setting_value == get_environment_setting_default(setting_name)
 
 
 @pytest.mark.asyncio
@@ -31,7 +46,8 @@ async def test_environment_settings(client, server, environment_default):
     assert "settings" in result.result
     assert "metadata" in result.result
     assert "auto_deploy" in result.result["metadata"]
-    assert len(result.result["settings"]) == 0
+
+    check_only_contains_default_setting(result.result["settings"])
 
     # set invalid value
     result = await client.set_setting(tid=environment_default, id="auto_deploy", value="test")
@@ -46,7 +62,12 @@ async def test_environment_settings(client, server, environment_default):
 
     result = await client.list_settings(tid=environment_default)
     assert result.code == 200
-    assert len(result.result["settings"]) == 1
+
+    for setting_name, setting_value in result.result["settings"].items():
+        if setting_name == "auto_deploy":
+            assert setting_value is False
+        else:
+            assert setting_value == get_environment_setting_default(setting_name)
 
     result = await client.get_setting(tid=environment_default, id="auto_deploy")
     assert result.code == 200
@@ -71,7 +92,8 @@ async def test_environment_settings(client, server, environment_default):
     result = await client.list_settings(tid=environment_default)
     assert result.code == 200
     assert "settings" in result.result
-    assert len(result.result["settings"]) == 1
+
+    check_only_contains_default_setting(result.result["settings"])
 
     result = await client.set_setting(tid=environment_default, id=data.AUTOSTART_AGENT_DEPLOY_SPLAY_TIME, value=20)
     assert result.code == 200
@@ -122,7 +144,7 @@ async def test_environment_settings_v2(client_v2, server, environment_default):
     assert "settings" in response.result["data"]
     assert "definition" in response.result["data"]
     assert "auto_deploy" in response.result["data"]["definition"]
-    assert len(response.result["data"]["settings"]) == 0
+    check_only_contains_default_setting(response.result["data"]["settings"])
 
     response = await client_v2.environment_settings_set(tid=environment_default, id="auto_deploy", value=False)
     assert response.code == 200
