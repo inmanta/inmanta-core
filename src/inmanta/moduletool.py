5eb1419d2e142c8f9bb8ba2a28721801c94711dd
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-06-29 14:20:11 +0200

    Added support to create development builds of V2 modules (Issue inmanta/irt#1184, PR #4468)
    # Description
    
    This PR adds support to create development builds of V2 modules.
    
    Part of inmanta/irt#1184
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/inmanta/moduletool.py
+++ src/inmanta/moduletool.py
@@ -17,6 +17,7 @@
 """
 import argparse
 import configparser
+import datetime
 import inspect
 import logging
 import os
@@ -507,6 +508,14 @@ mode.
             default=None,
             dest="output_dir",
         )
+        build.add_argument(
+            "--dev",
+            dest="dev_build",
+            help="Perform a development build of the module. This adds the build tag `.dev<timestamp>` to the "
+            "package name. The timestamp has the form %%Y%%m%%d%%H%%M%%S.",
+            default=False,
+            action="store_true",
+        )
 
         subparser.add_parser("v1tov2", help="Convert a V1 module to a V2 module in place")
 
@@ -557,7 +566,7 @@ mode.
             raise ModuleVersionException(f"Expected a v1 module, but found v{module.GENERATION.value} module")
         ModuleConverter(module).convert_in_place()
 
-    def build(self, path: Optional[str] = None, output_dir: Optional[str] = None) -> str:
+    def build(self, path: Optional[str] = None, output_dir: Optional[str] = None, dev_build: bool = False) -> str:
         """
         Build a v2 module and return the path to the build artifact.
         """
@@ -574,9 +583,9 @@ mode.
         if isinstance(module, ModuleV1):
             with tempfile.TemporaryDirectory() as tmpdir:
                 ModuleConverter(module).convert(tmpdir)
-                return V2ModuleBuilder(tmpdir).build(output_dir)
+                return V2ModuleBuilder(tmpdir).build(output_dir, dev_build=dev_build)
         else:
-            return V2ModuleBuilder(path).build(output_dir)
+            return V2ModuleBuilder(path).build(output_dir, dev_build=dev_build)
 
     def get_project_for_module(self, module: str) -> Project:
         try:
@@ -901,7 +910,7 @@ class V2ModuleBuilder:
         """
         self._module = ModuleV2(project=None, path=os.path.abspath(module_path))
 
-    def build(self, output_directory: str) -> str:
+    def build(self, output_directory: str, dev_build: bool = False) -> str:
         """
         Build the module and return the path to the build artifact.
         """
@@ -912,11 +921,31 @@ class V2ModuleBuilder:
             # Copy module to temporary directory to perform the build
             build_path = os.path.join(tmpdir, "module")
             shutil.copytree(self._module.path, build_path)
+            if dev_build:
+                self._add_dev_build_tag_to_setup_cfg(build_path)
             self._ensure_plugins(build_path)
             self._move_data_files_into_namespace_package_dir(build_path)
             path_to_wheel = self._build_v2_module(build_path, output_directory)
             self._verify_wheel(build_path, path_to_wheel)
             return path_to_wheel
+
+    def _add_dev_build_tag_to_setup_cfg(self, build_path: str) -> None:
+        """
+        Add a build_tag of the format `.dev<timestamp>` to the setup.cfg file. The timestamp has the form %Y%m%d%H%M%S.
+        """
+        path_setup_cfg = os.path.join(build_path, "setup.cfg")
+        # Read setup.cfg file
+        config_in = ConfigParser()
+        config_in.read(path_setup_cfg)
+        # Set build_tag
+        if not config_in.has_section("egg_info"):
+            config_in.add_section("egg_info")
+        timestamp_uct = datetime.datetime.now(datetime.timezone.utc)
+        timestamp_uct_str = timestamp_uct.strftime("%Y%m%d%H%M%S")
+        config_in.set("egg_info", "tag_build", f".dev{timestamp_uct_str}")
+        # Write file back
+        with open(path_setup_cfg, "w") as fh:
+            config_in.write(fh)
 
     def _verify_wheel(self, build_path: str, path_to_wheel: str) -> None:
         """
