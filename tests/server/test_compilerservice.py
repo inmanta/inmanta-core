6373694b8ef6d22bc093fad78d47f4acf522fbe0
FloLey <95339363+FloLey@users.noreply.github.com>
2022-07-07 10:08:45 +0200

    Update tests/server/test_compilerservice.py
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- tests/server/test_compilerservice.py
+++ tests/server/test_compilerservice.py
@@ -576,7 +576,7 @@ async def test_server_partial_compile(server, client, environment, monkeypatch):
 
     # Do a partial compile with removed resource_sets
     compile_id, _ = await compilerslice.request_recompile(
-        env, force_update=True, do_export=False, remote_id=remote_id1, partial=True, removed_resource_sets=["a", "b", "c"]
+        env, force_update=False, do_export=False, remote_id=remote_id1, partial=True, removed_resource_sets=["a", "b", "c"]
     )
 
     async def wait_for_report():
