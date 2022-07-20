7e74376126a7456d1cf8eb150edec674ed68e0f8
Sander Van Balen <git@sandervanbalen.be>
2022-07-20 16:44:11 +0200

    Fixed incorrect exception chaining for conflicting dependencies exception (PR #4577)
    # Description
    
    See https://docs.python.org/3/tutorial/errors.html#exception-chaining
    
    closes #4573
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [ ] Attached issue to pull request
    - [ ] Changelog entry
    - [ ] Type annotations are present
    - [ ] Code is clear and sufficiently documented
    - [ ] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/inmanta/moduletool.py
+++ src/inmanta/moduletool.py
@@ -813,7 +813,7 @@ version: 0.0.1dev0"""
             try:
                 env.process_env.install_from_source([env.LocalPackagePath(path=install_path, editable=editable)])
             except env.ConflictingRequirements as e:
-                raise InvalidModuleException(e)
+                raise InvalidModuleException("Module installation failed due to conflicting dependencies") from e
 
         module_path: str = os.path.abspath(path) if path is not None else os.getcwd()
         module: Module = self.construct_module(None, module_path)
