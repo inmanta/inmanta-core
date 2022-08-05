1824688aacb8c78809d28a77282ce7078b68f570
inmantaci <ci@inmanta.com>
2022-08-05 14:23:34 +0200

    Increment dev version after RC release

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-version = "7.0.0"
+version = "7.0.1"
 
 setup(
     version=version,
