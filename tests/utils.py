5a2ea0fbe0a2a8555d4a926601bbe1aa6db96ff9
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-08-19 16:06:48 +0200

    Fix issue that causes an agent restart storm for all agents on an agent process when an agent on that process is paused. (Issue inmanta/inmanta-core#4398, PR #4696)
    Pull request opened by the merge tool on behalf of #4696

--- tests/utils.py
+++ tests/utils.py
@@ -18,12 +18,10 @@
 import asyncio
 import configparser
 import datetime
-import inspect
 import json
 import logging
 import os
 import shutil
-import time
 import uuid
 from collections import abc
 from dataclasses import dataclass
@@ -37,7 +35,7 @@ from pydantic.tools import lru_cache
 import build
 import build.env
 from _pytest.mark import MarkDecorator
-from inmanta import const, data, env, module
+from inmanta import const, data, env, module, util
 from inmanta.moduletool import ModuleTool
 from inmanta.protocol import Client
 from inmanta.server.bootloader import InmantaBootloader
@@ -47,17 +45,16 @@ from libpip2pi.commands import dir2pi
 from packaging import version
 
 
-async def retry_limited(fun, timeout, *args, **kwargs):
-    async def fun_wrapper():
-        if inspect.iscoroutinefunction(fun):
-            return await fun(*args, **kwargs)
-        else:
-            return fun(*args, **kwargs)
-
-    start = time.time()
-    while time.time() - start < timeout and not (await fun_wrapper()):
-        await asyncio.sleep(0.1)
-    if not (await fun_wrapper()):
+async def retry_limited(
+    fun: Union[abc.Callable[..., bool], abc.Callable[..., abc.Awaitable[bool]]],
+    timeout: float,
+    interval: float = 0.1,
+    *args: object,
+    **kwargs: object,
+) -> None:
+    try:
+        await util.retry_limited(fun, timeout, interval, *args, **kwargs)
+    except asyncio.TimeoutError:
         raise AssertionError("Bounded wait failed")
 
 
