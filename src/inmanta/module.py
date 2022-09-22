8421d433858b453cfd4a3151303e14390aa9e343
Lejoly Florent <florent.lejoly@inmanta.com>
2022-09-22 10:23:43 +0200

    update

--- src/inmanta/module.py
+++ src/inmanta/module.py
@@ -509,9 +509,9 @@ class ModuleSource(Generic[TModule]):
     def _log_snapshot_difference(
         self, version_snapshot: Dict[str, "Version"], previous_snapshot: Dict[str, "Version"], header: Optional[str]
     ) -> None:
-        set_pre_install = set(previous_snapshot.items())
-        set_post_install = set(version_snapshot.items())
-        updates_and_additions = set_post_install - set_pre_install
+        set_pre_install: Set[Tuple[str, "Version"]] = set(previous_snapshot.items())
+        set_post_install: Set[Tuple[str, "Version"]] = set(version_snapshot.items())
+        updates_and_additions: Set[Tuple[str, "Version"]] = set_post_install - set_pre_install
 
         if version_snapshot:
             out = [header] if header is not None else []
