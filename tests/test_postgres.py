b2d04ce5d9efe8d0a344844b054ff8d284413d3e
Sander Van Balen <git@sandervanbalen.be>
2022-09-26 12:25:51 +0200

    fixed second deadlock?

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -22,11 +22,13 @@ especially those that are not explicitly documented anywhere. These tests act bo
 future PostgreSQL versions and as a knowledge collector to refer back to when we need it.
 """
 import asyncio
+from collections import abc
 
 import asyncpg
 import pytest
 
 
+@pytest.mark.slowtest
 async def test_postgres_cascade_locking_order(postgresql_pool) -> None:
     """
     Verifies that Postgres' cascade deletion acquires locks top-down. This is important because in order to avoid deadlocks
@@ -87,5 +89,81 @@ async def test_postgres_cascade_locking_order(postgresql_pool) -> None:
     with pytest.raises(asyncpg.DeadlockDetectedError):
         await asyncio.gather(
             lock_bottom_up(),
+            delete(),
+        )
+
+
+@pytest.mark.slowtest
+@pytest.mark.parametrize("definition_order_one_two", [True, False])
+async def test_postgres_cascade_locking_order_siblings(postgresql_pool, definition_order_one_two: bool) -> None:
+    """
+    Verifies locking order for siblings in the cascade tree. Locking order seems to be based on definition order of the
+    referencing columns. Locking order may shift in case of updates to the definition of these columns.
+    """
+    leaf_definitions: tuple[str] = tuple(
+        f"CREATE TABLE IF NOT EXISTS {name} (name varchar PRIMARY KEY, myroot varchar REFERENCES root(name) ON DELETE CASCADE);"
+        for name in ("leafone", "leaftwo")
+    )
+    async with postgresql_pool.acquire() as connection:
+        await connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS root (name varchar PRIMARY KEY);
+            %s
+            %s
+            """
+            % (leaf_definitions if definition_order_one_two else tuple(reversed(leaf_definitions)))
+        )
+
+    async def insert():
+        async with postgresql_pool.acquire() as connection:
+            await connection.execute(
+                """
+                INSERT INTO root VALUES
+                    ('root1');
+
+                INSERT INTO leafone VALUES
+                    ('leafone1', 'root1'),
+                    ('leafone2', 'root1');
+
+                INSERT INTO leaftwo VALUES
+                    ('leaftwo1', 'root1'),
+                    ('leaftwo2', 'root1');
+                """
+            )
+
+    async def lock_one_two():
+        async with postgresql_pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.execute("LOCK TABLE leafone IN SHARE MODE")
+                await asyncio.sleep(0.1)
+                await connection.execute("LOCK TABLE leaftwo IN SHARE MODE")
+
+    async def lock_two_one():
+        async with postgresql_pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.execute("LOCK TABLE leaftwo IN SHARE MODE")
+                await asyncio.sleep(0.1)
+                await connection.execute("LOCK TABLE leafone IN SHARE MODE")
+
+    async def delete():
+        async with postgresql_pool.acquire() as connection:
+            async with connection.transaction():
+                await asyncio.sleep(0.05)
+                await connection.execute("DELETE FROM root")
+
+    success: abc.Awaitable[None] = lock_one_two() if definition_order_one_two else lock_two_one()
+    deadlock: abc.Awaitable[None] = lock_two_one() if definition_order_one_two else lock_one_two()
+
+    # always do successful first so we don't need to deal with cleanup after deadlock
+    await insert()
+    await asyncio.gather(
+        success,
+        delete(),
+    )
+
+    await insert()
+    with pytest.raises(asyncpg.DeadlockDetectedError):
+        await asyncio.gather(
+            deadlock,
             delete(),
         )
