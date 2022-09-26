0d9169a1170b3a454f5d17bb47e6b2741150ee67
Sander Van Balen <git@sandervanbalen.be>
2022-09-26 09:54:50 +0200

    docstrings and restructure

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -1,10 +1,37 @@
+"""
+    Copyright 2022 Inmanta
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
+
+"""
+This module contains tests related to PostgreSQL and how we interact with it. Its purpose is to verify PostgreSQL internals,
+especially those that are not explicitly documented anywhere. These tests act both as a confidence check for compatibility with
+future PostgreSQL versions and as a knowledge collector to refer back to when we need it.
+"""
 import asyncio
 import asyncpg
 
 import pytest
 
 
-async def test_postgres_cascade(postgresql_pool) -> None:
+async def test_postgres_cascade_locking_order(postgresql_pool) -> None:
+    """
+    Verifies that Postgres' cascade deletion acquires locks top-down. This is important because in order to avoid deadlocks
+    we define a corresponding locking order for all transactions. See `inmanta.data.ConfigurationModel` docstring.
+    """
     async with postgresql_pool.acquire() as connection:
         await connection.execute(
             """
@@ -61,6 +88,3 @@ async def test_postgres_cascade(postgresql_pool) -> None:
             lock_bottom_up(),
             delete(),
         )
-
-
-# TODO: add tests for known deadlocks
