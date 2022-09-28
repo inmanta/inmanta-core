9d276098dfbe598757f170416a46680fe62b34b1
inmantaci <ci@inmanta.com>
2022-09-28 15:29:38 +0200

    Increment dev version after RC release

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-version = "7.1.0"
+version = "7.1.1"
 
 setup(
     version=version,
--- tests_common/setup.py
+++ tests_common/setup.py
@@ -19,7 +19,7 @@ from os import path
 
 from setuptools import find_namespace_packages, setup
 
-version = "7.1.0"
+version = "7.1.1"
 
 requires = [
     "asyncpg",
