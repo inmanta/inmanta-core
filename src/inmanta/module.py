9377e152baeff702a257983d219ba3ebfa30ccb4
Lejoly Florent <florent.lejoly@inmanta.com>
2022-10-03 10:35:07 +0200

    Improve the logging of the pip commands by using a separated logger for those. Also add the content of the requirements and constraints files to the logging. (Issue #4651, PR #4869)
    # Description
    
    The following information will be logged (in a separate logger inmanta.pip):
    
    1. the pip command itself
    2. the content of the requirements.txt file
    
    closes #4651
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/inmanta/module.py
+++ src/inmanta/module.py
@@ -356,7 +356,7 @@ class CLIGitProvider(GitProvider):
         process_env["GIT_ASKPASS"] = "true"
         cmd = ["git", "clone", src, dest]
 
-        return_code, _ = env.PythonEnvironment.run_command_and_stream_output(cmd, env_vars=process_env)
+        return_code, _ = env.CommandRunner(LOGGER).run_command_and_stream_output(cmd, env_vars=process_env)
 
         if return_code != 0:
             raise Exception(f"An unexpected error occurred while cloning into {dest} from {src}.")
