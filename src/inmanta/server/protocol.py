6ae30ccce960f0a7028fa3915101458fad7af40d
Lejoly Florent <florent.lejoly@inmanta.com>
2022-10-10 11:42:43 +0200

    update

--- src/inmanta/server/protocol.py
+++ src/inmanta/server/protocol.py
@@ -319,7 +319,7 @@ class ServerSlice(inmanta.protocol.endpoints.CallTarget, TaskHandler):
     def schedule_cron(self, call: TaskMethod, cron: str, cancel_on_stop: bool = True) -> None:
         """
         Schedule a task according to a cron specifier. Tasks with the same call and the same schedule are considered the same.
-        Clients that wish to be able to delete tasks should make sure to use a unique `call` function."
+        Clients that wish to be able to delete tasks should make sure to use a unique `call` function.
 
         :param cron: The cron specifier to schedule the task by.
         """
