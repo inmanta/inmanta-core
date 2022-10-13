3e65d8ae856452d31b62861d9308e78ba09d1c39
Hugo Thiollière <hugo.thiolliere@inmanta.com>
2022-10-13 15:32:30 +0200

    [REFACTOR]

--- predictive_test_selection/collect_test_failure_data.py
+++ predictive_test_selection/collect_test_failure_data.py
@@ -8,10 +8,9 @@ from datetime import datetime
 from enum import IntFlag, auto
 from typing import Any, List, Mapping
 import requests
+import logging
 
-
-
-
+LOGGER = logging.getLogger(__name__)
 
 
 
@@ -28,20 +27,20 @@ class CommonFileExtension(IntFlag):
 
 class DataParser():
     """
-        # x date text NOT NULL, => use influxdb timestamp
-          test_fqn text NOT NULL, ==> GO for test case not file
-        x commit_hash text NOT NULL,
-        x n_changes_3 int NOT NULL,
-        x n_changes_14 int NOT NULL,
-        x n_changes_56 int NOT NULL,
-        x file_cardinality int NOT NULL,
-        x file_extension text NOT NULL,
-          target_cardinality int NOT NULL,  => always 1 since at test case level and not at test file level
-          failure_rate_7 real NOT NULL,
-          failure_rate_14 real NOT NULL,
-          failure_rate_28 real NOT NULL,
-          failure_rate_56 real NOT NULL,
-          test_failed int NOT NULL
+        This class fetches all information regarding a specific code change and the tests that are run on this specific commit
+        and sends it all to the influxdb database.
+
+        Information regarding the code change:
+            x commit_hash
+            x n_changes_3  \
+            x n_changes_14  -> Changes made to the files involved in this change in the last 3, 14 and 56 days
+            x n_changes_56 /
+            x file_cardinality -> Number of files involved in this change
+            x file_extension -> File extensions of the files involved in this change
+
+        Information regarding the tests:
+            x test_fqn -> fully qualified name of the test case: <test_file_name>.<test_case>
+            x test_failed -> integer denoting whether this test failed or not.
     """
     def __init__(self):
         self.code_change_data: Mapping[str, Any] = {}
@@ -66,8 +65,8 @@ class DataParser():
         data["commit_hash"] = subprocess.check_output(["git", "log", "--pretty=%H", "-1"]).strip()
 
         # Get current branch
-        cmd = ["git", "rev-parse", "--abbrev", "-ref", "HEAD"]
-        current_branch = subprocess.check_output(cmd)
+        cmd = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
+        current_branch = subprocess.check_output(cmd).strip().decode()
 
         cmd = ["git", "diff", f"master...{current_branch}", "--name-only"]
         changed_files = [line for line in subprocess.check_output(cmd, env=os.environ.copy()).decode().split("\n") if line]
@@ -116,8 +115,6 @@ class DataParser():
         tree = ET.parse(path)
         root = tree.getroot()
 
-        self.test_result_data = {}
-
         for test_suite in root.findall("testsuite"):
             for test_case in test_suite.findall("testcase"):
                 if test_case.find("skipped") is not None:  # test got skipped -> we don't record anything
@@ -133,33 +130,42 @@ class DataParser():
 
 
     def create_data_payload(self) -> str:
-        # Use influx_db auto-generated timestamp
+        """"
+            Anatomy of each line of the payload:
+            Measurement: test_result
+            tag_set: fqn, commit_hash
+            field_set: failed_as_int, n_changes, file_extension, file_cardinality
+            timestamp: Use influx_db auto-generated timestamp
+        """
         data_points: List[str] = [
             (
-                f"test_result,fqn={test_file},commit_hash={self.code_change_data['commit_hash']}"
+                f"test_result,fqn={test_fqn},commit_hash={self.code_change_data['commit_hash']}"
                 f" failed_as_int={failed},n_changes={self.code_change_data['n_changes']},"
                 f"file_extension={self.code_change_data['file_extension']},"
                 f"file_cardinality={self.code_change_data['file_cardinality']}"
             )
-            for test_file, failed in self.test_result_data.items()
+            for test_fqn, failed in self.test_result_data.items()
         ]
         return "\n".join(data_points)
 
-    def send_influxdb_data(self) -> None:
+    def send_influxdb_data(self, dry_run: bool = True) -> None:
         url = "http://mon.ii.inmanta.com:8086/write?db=predictive_test_selection&precision=s"
-        r = requests.post(url, data=self.create_data_payload())
+        payload = self.create_data_payload()
 
-        r.raise_for_status()
+        if not dry_run:
+            r = requests.post(url, data=payload)
 
+            r.raise_for_status()
+        else:
+            LOGGER.debug(payload)
 
+    def collect_data(self):
+        self.parse_code_change()
+        self.parse_xml_test_results()
+        self.send_influxdb_data()
 
 
 if __name__ == "__main__":
     data_parser = DataParser()
-    data_parser.parse_code_change()
-    data_parser.parse_xml_test_results()
-    data_parser.send_influxdb_data()
+    data_parser.collect_data()
 
-    # code_change_data = parse_code_change()
-    # data_points = parse_xml_test_results(code_change_data)
-    # send_influxdb_data(data_points)
