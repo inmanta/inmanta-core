9225b572a6d62268f688839228323ec17d07b716
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-11-14 07:39:08 +0100

    Removed support to use a dictionary in the requires metadata field of a V1 module or an Inmanta project. (Issue inmanta/inmanta-core#4974, PR #5101)
    # Description
    
    Removed support to use a dictionary in the requires metadata field of a V1 module or an Inmanta project.
    
    closes #4974
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/inmanta/module.py
+++ src/inmanta/module.py
@@ -266,10 +266,6 @@ class InvalidMetadata(CompilerException):
         return msg
 
 
-class MetadataDeprecationWarning(Warning):
-    pass
-
-
 class ModuleDeprecationWarning(Warning):
     pass
 
@@ -1118,20 +1114,6 @@ class MetadataFieldRequires(BaseModel):
     @validator("requires", pre=True)
     @classmethod
     def requires_to_list(cls, v: object) -> object:
-        if isinstance(v, dict):
-            # transform legacy format for backwards compatibility
-            warnings.warn(
-                MetadataDeprecationWarning(
-                    "The yaml dictionary syntax for specifying module requirements has been deprecated. Please use the"
-                    " documented list syntax instead."
-                )
-            )
-            result: List[str] = []
-            for key, value in v.items():
-                if not (isinstance(key, str) and isinstance(value, str) and value.startswith(key)):
-                    raise ValueError("Invalid legacy requires format, expected `mod: mod [constraint]`.")
-                result.append(value)
-            return result
         return cls.to_list(v)
 
 
