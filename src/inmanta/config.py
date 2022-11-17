85a9aabe95c8c2239f1ce8f56b9635c19c2210a7
Lejoly Florent <florent.lejoly@inmanta.com>
2022-11-17 12:08:36 +0100

    Revert "update"
    This reverts commit 7a031a26bac0313d9637952e4df23b79693e64e8.

--- src/inmanta/config.py
+++ src/inmanta/config.py
@@ -332,7 +332,7 @@ class Option(Generic[T]):
             return defa
 
     def set(self, value: str) -> None:
-        """Only for tests"""
+        """ Only for tests"""
         Config.set(self.section, self.name, value)
 
 
