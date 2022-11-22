070019a461c58e6e5b70877678c861b2a704562d
Sander Van Balen <git@sandervanbalen.be>
2022-11-22 17:20:24 +0100

    Added the inmanta-workon command (Issue #4376, PR #5137)
    # Description
    
    Added the `inmanta-workon` command. I will create separate pull requests on `inmanta` and `inmanta-service-orchestrator` for the RPM installation.
    
    @wouterdb I'm adding you as a reviewer only for the documentation and change entry part. Is the `model_developers` section the best place for it?
    
    part of #4376
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,6 +79,7 @@ import time
 import traceback
 import uuid
 import venv
+from collections import abc
 from configparser import ConfigParser
 from typing import AsyncIterator, Awaitable, Callable, Dict, Iterator, List, Optional, Tuple, Union
 
@@ -115,7 +116,7 @@ from inmanta.parser.plyInmantaParser import cache_manager
 from inmanta.protocol import VersionMatch
 from inmanta.server import SLICE_AGENT_MANAGER, SLICE_COMPILER
 from inmanta.server.bootloader import InmantaBootloader
-from inmanta.server.protocol import SliceStartupException
+from inmanta.server.protocol import Server, SliceStartupException
 from inmanta.server.services.compilerservice import CompilerService, CompileRun
 from inmanta.types import JsonType
 from inmanta.warnings import WarningsManager
@@ -739,7 +740,7 @@ async def server_config(event_loop, inmanta_config, postgres_db, database_name, 
 
 
 @pytest.fixture(scope="function")
-async def server(server_pre_start):
+async def server(server_pre_start) -> abc.AsyncIterator[Server]:
     """
     :param event_loop: explicitly include event_loop to make sure event loop started before and closed after this fixture.
     May not be required
