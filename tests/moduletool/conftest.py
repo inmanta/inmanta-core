318b11dee889aad69a8d2885230631fed836ac62
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-11-30 09:09:49 +0100

    Fix commit message mod12

--- tests/moduletool/conftest.py
+++ tests/moduletool/conftest.py
@@ -144,8 +144,8 @@ def modules_repo(git_modules_dir) -> str:
     add_file(mod11, "file", "test", "release version 4.2.0", version="4.2.0")
 
     mod12 = make_module_simple(reporoot, "mod12", version="3.2.1")
-    add_file(mod12, "file", "test", "release version 4.0.0", version="4.0.0.dev0")
-    add_file(mod12, "file", "test", "release version 4.1.0", version="4.0.0")
+    add_file(mod12, "file", "test", "release version 4.0.0.dev0", version="4.0.0.dev0")
+    add_file(mod12, "file", "test", "release version 4.0.0", version="4.0.0")
 
     proj = makemodule(
         reporoot, "testproject", [("mod1", None), ("mod2", ">2016"), ("mod5", None)], True, ["mod1", "mod2", "mod6", "mod7"]
