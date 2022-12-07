38d63eb9a3cd788960443f2ee94291d3e05d5102
Sander Van Balen <git@sandervanbalen.be>
2022-12-07 16:50:08 +0100

    fixed resource action consistency check

--- tests/db/migration_tests/test_v202206290_to_v202208190.py
+++ tests/db/migration_tests/test_v202206290_to_v202208190.py
@@ -31,29 +31,30 @@ async def test_added_resource_join_table(
 
     """
     all_ras = await postgresql_client.fetch(
-        """SELECT ra.action_id, r.environment, r.resource_version_id FROM public.resourceaction as ra
+        """SELECT ra.action_id, r.environment, r.resource_id, r.model FROM public.resourceaction as ra
                 INNER JOIN public.resource as r
                 ON r.resource_version_id = ANY(ra.resource_version_ids)
                 AND r.environment = ra.environment
         """
     )
-    all_ra_set = {(r[0], r[1], r[2]) for r in all_ras}
+    all_ra_set = {(r[0], r[1], r[2], r[3]) for r in all_ras}
     assert len(all_ra_set) != 0
 
     # Migrate DB schema
     await migrate_db_from()
 
     post_ra_one = await postgresql_client.fetch(
-        """SELECT ra.action_id, r.environment, r.resource_version_id FROM public.resourceaction as ra
+        # resource_version_id was dropped in a later migration => construct it on the fly
+        """SELECT ra.action_id, r.environment, r.resource_id, r.model FROM public.resourceaction as ra
                 INNER JOIN public.resource as r
-                ON r.resource_version_id = ANY(ra.resource_version_ids)
+                ON r.resource_id || ',v=' || r.model = ANY(ra.resource_version_ids)
                 AND r.environment = ra.environment
         """
     )
-    assert all_ra_set == {(r[0], r[1], r[2]) for r in post_ra_one}
+    assert all_ra_set == {(r[0], r[1], r[2], r[3]) for r in post_ra_one}
 
     post_ra_two = await postgresql_client.fetch(
-        """SELECT ra.action_id, r.environment, r.resource_version_id FROM public.resource as r
+        """SELECT ra.action_id, r.environment, r.resource_id, r.model FROM public.resource as r
                 INNER JOIN public.resourceaction_resource as jt
                     ON r.environment = jt.environment
                     AND r.resource_id = jt.resource_id
@@ -62,4 +63,4 @@ async def test_added_resource_join_table(
                     ON ra.action_id = jt.resource_action_id
         """
     )
-    assert all_ra_set == {(r[0], r[1], r[2]) for r in post_ra_two}
+    assert all_ra_set == {(r[0], r[1], r[2], r[3]) for r in post_ra_two}
