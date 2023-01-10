fa6cc1b1f38a238234bc9cc148d23cc25028fb08
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-01-10 11:09:00 +0100

    Simplify code

--- tests/db/migration_tests/test_v202206290_to_v202208190.py
+++ tests/db/migration_tests/test_v202206290_to_v202208190.py
@@ -20,6 +20,8 @@ from collections import abc
 
 import pytest
 
+from utils import resource_action_consistency_check
+
 
 @pytest.mark.db_restore_dump(os.path.join(os.path.dirname(__file__), "dumps/v202206290.sql"))
 async def test_added_resource_join_table(
@@ -43,24 +45,4 @@ async def test_added_resource_join_table(
     # Migrate DB schema
     await migrate_db_from()
 
-    post_ra_one = await postgresql_client.fetch(
-        # resource_version_id was dropped in a later migration => construct it on the fly
-        """SELECT ra.action_id, r.environment, r.resource_id, r.model FROM public.resourceaction as ra
-                INNER JOIN public.resource as r
-                ON r.resource_id || ',v=' || r.model = ANY(ra.resource_version_ids)
-                AND r.environment = ra.environment
-        """
-    )
-    assert all_ra_set == {(r[0], r[1], r[2], r[3]) for r in post_ra_one}
-
-    post_ra_two = await postgresql_client.fetch(
-        """SELECT ra.action_id, r.environment, r.resource_id, r.model FROM public.resource as r
-                INNER JOIN public.resourceaction_resource as jt
-                    ON r.environment = jt.environment
-                    AND r.resource_id = jt.resource_id
-                    AND r.model = jt.resource_version
-                INNER JOIN public.resourceaction as ra
-                    ON ra.action_id = jt.resource_action_id
-        """
-    )
-    assert all_ra_set == {(r[0], r[1], r[2], r[3]) for r in post_ra_two}
+    await resource_action_consistency_check()
