3ff40bde818370512ba35ac12b75b2d064991f0b
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-01-11 13:58:06 +0100

    Remove second redundant line

--- src/inmanta/server/services/environment_metrics_service.py
+++ src/inmanta/server/services/environment_metrics_service.py
@@ -139,7 +139,6 @@ class EnvironmentMetricsService(protocol.ServerSlice):
         self.register_metric_collector(ResourceCountMetricsCollector())
         self.register_metric_collector(AgentCountMetricsCollector())
         self.register_metric_collector(CompileTimeMetricsCollector())
-        self.previous_timestamp = datetime.now()
         self.schedule(
             self.flush_metrics, COLLECTION_INTERVAL_IN_SEC, initial_delay=COLLECTION_INTERVAL_IN_SEC, cancel_on_stop=True
         )
