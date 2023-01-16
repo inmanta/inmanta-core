7fbddfa4e4c4c00a44e22ea26c24c380fd1b0461
Lejoly Florent <florent.lejoly@inmanta.com>
2023-01-16 11:18:47 +0100

    update

--- src/inmanta/server/services/environment_metrics_service.py
+++ src/inmanta/server/services/environment_metrics_service.py
@@ -181,9 +181,11 @@ class EnvironmentMetricsService(protocol.ServerSlice):
         self.register_metric_collector(CompileWaitingTimeMetricsCollector())
         self.register_metric_collector(AgentCountMetricsCollector())
         self.register_metric_collector(CompileTimeMetricsCollector())
-        self.schedule(
-            self.flush_metrics, COLLECTION_INTERVAL_IN_SEC, initial_delay=COLLECTION_INTERVAL_IN_SEC, cancel_on_stop=True
-        )
+        if COLLECTION_INTERVAL_IN_SEC != 0:
+            print("OLA")
+            self.schedule(
+                self.flush_metrics, COLLECTION_INTERVAL_IN_SEC, initial_delay=COLLECTION_INTERVAL_IN_SEC, cancel_on_stop=True
+            )
 
     def register_metric_collector(self, metrics_collector: MetricsCollector) -> None:
         """
