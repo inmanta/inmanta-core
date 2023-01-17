e6e29c1d456386b9d52ac170f5bad799eb799200
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-01-17 08:56:03 +0100

    Fix pep8

--- src/inmanta/server/services/environment_metrics_service.py
+++ src/inmanta/server/services/environment_metrics_service.py
@@ -20,7 +20,7 @@ import logging
 import math
 import textwrap
 import uuid
-from collections.abc import Sequence, Mapping
+from collections.abc import Sequence
 from datetime import datetime, timedelta
 from enum import Enum
 from typing import Dict, List, Optional, Union
