fc9748b65f915070d44204872620234b10751cf3
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-02-10 13:46:12 +0100

    Fix bug in the serialization of the ConfigurationModel dao. (PR #5539)
    Pull request opened by the merge tool on behalf of #5539

--- src/inmanta/data/__init__.py
+++ src/inmanta/data/__init__.py
@@ -4562,7 +4562,7 @@ class ConfigurationModel(BaseDocument):
     def to_dict(self) -> JsonType:
         dct = BaseDocument.to_dict(self)
         dct["status"] = dict(self._status)
-        dct["done"] = self._done
+        dct["done"] = self.done
         return dct
 
     @classmethod
