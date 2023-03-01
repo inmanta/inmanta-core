e6b982c29cb6e6788670ec19873d99bb674dca80
FloLey <95339363+FloLey@users.noreply.github.com>
2023-03-01 11:31:45 +0100

    Update tests/test_usersetup.py
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- tests/test_usersetup.py
+++ tests/test_usersetup.py
@@ -82,8 +82,7 @@ def setup_config(tmpdir, postgres_db, database_name):
 
 async def test_user_setup(tmpdir, server_pre_start, postgres_db, database_name, hard_clean_db, hard_clean_db_post):
     ibl = InmantaBootloader()
-    # we need the server to start so that all the migrations scripts are applied, but the server needs to be shut down
-    # so that the connection is free and that user_setup can use it.
+    # we need the server to start so that all the migrations scripts are applied, but the server needs to be shut down afterwards, otherwise the call to get_connection_pool() will result in an exception saying that the connection pool is already set in the database layer.
     try:
         await ibl.start()
         await ibl.stop(timeout=15)
