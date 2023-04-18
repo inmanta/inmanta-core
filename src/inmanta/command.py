47b7af6f3663f81680f60b63549bbffd38aaabfb
hugo <hugo.thiolliere@inmanta.com>
2023-04-18 11:42:25 +0200

    [FIX] fix sequence typing

--- src/inmanta/command.py
+++ src/inmanta/command.py
@@ -18,10 +18,11 @@
 
 
 import argparse
-from typing import Callable, Dict, List, Optional, Sequence
+from collections import abc
+from typing import Callable, Dict, List, Optional
 
 FunctionType = Callable[[argparse.Namespace], None]
-ParserConfigType = Callable[[argparse.ArgumentParser, Sequence[argparse.ArgumentParser]], None]
+ParserConfigType = Callable[[argparse.ArgumentParser, abc.Sequence[argparse.ArgumentParser]], None]
 
 
 class CLIException(Exception):
