97a227268fa4e4edfa6d4d8ba7c522f7333d2d43
Sander Van Balen <git@sandervanbalen.be>
2023-04-18 13:38:17 +0200

    Added assertion for better visibility on flaky test (PR #5839)
    This test failed tonight but I can't seem to reproduce it. Since there is not sufficient data to really investigate I added an initial assertion that should cover the same case and fail with a better message.

--- tests/test_2way_protocol.py
+++ tests/test_2way_protocol.py
@@ -147,7 +147,9 @@ async def test_2way_protocol(unused_tcp_port, no_tid_check, postgres_db, databas
 async def check_sessions(sessions):
     for s in sessions:
         a = await s.client.get_agent_status_x("X")
-        assert a.get_result()["status"] == "ok"
+        assert a.code == 200, a.result
+        result = a.get_result()
+        assert result["status"] == "ok", result
 
 
 @pytest.mark.slowtest
