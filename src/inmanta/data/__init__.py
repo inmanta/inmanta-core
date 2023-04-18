ec5cbf41ab997e70680fe45e3359cd6c754dc4d7
Lejoly Florent <florent.lejoly@inmanta.com>
2023-04-18 11:40:11 +0200

    Implement storage backend for the unmanaged resources (Issue #5690, PR #5791)
    # Description
    
    Implement a first storage backend for the unmanaged resources
    
    add following API endpoint URLs:
    POST api/v2/unmanaged/<unmanaged_resource_id> (and GET)
    POST api/v2/unmanaged/ (batch) (and GET)
    
    closes #5690
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

--- src/inmanta/data/__init__.py
+++ src/inmanta/data/__init__.py
@@ -905,6 +905,17 @@ class NotificationOrder(AbstractDatabaseOrderV2):
         return (ColumnNameStr("id"), UUIDColumn)
 
 
+class UnmanagedResourceOrder(SingleDatabaseOrder):
+    """Represents the ordering by which unmanaged resources should be sorted"""
+
+    @classmethod
+    def get_valid_sort_columns(cls) -> Dict[ColumnNameStr, ColumnType]:
+        """Describes the names and types of the columns that are valid for this DatabaseOrder"""
+        return {
+            ColumnNameStr("unmanaged_resource_id"): StringColumn,
+        }
+
+
 class BaseQueryBuilder(ABC):
     """Provides a way to build up a sql query from its parts.
     Each method returns a new query builder instance, with the additional parameters processed"""
@@ -5782,6 +5793,26 @@ class User(BaseDocument):
         return m.User(username=self.username, auth_method=self.auth_method)
 
 
+class UnmanagedResource(BaseDocument):
+    """
+    :param environment: the environment of the resource
+    :param unmanaged_resource_id: The id of the resource
+    :param values: The values associated with the unmanaged_resource
+    """
+
+    environment: uuid.UUID
+    unmanaged_resource_id: m.ResourceIdStr
+    values: dict[str, str]
+
+    __primary_key__ = ("environment", "unmanaged_resource_id")
+
+    def to_dto(self) -> m.UnmanagedResource:
+        return m.UnmanagedResource(
+            unmanaged_resource_id=self.unmanaged_resource_id,
+            values=self.values,
+        )
+
+
 _classes = [
     Project,
     Environment,
@@ -5801,6 +5832,7 @@ _classes = [
     EnvironmentMetricsGauge,
     EnvironmentMetricsTimer,
     User,
+    UnmanagedResource,
 ]
 
 
