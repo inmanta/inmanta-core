80ca5e377eb3262d038344877090f4716f3be404
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-04-21 13:42:49 +0200

    Fix

--- src/inmanta/__init__.py
+++ src/inmanta/__init__.py
@@ -16,7 +16,7 @@
     Contact: code@inmanta.com
 """
 
-COMPILER_VERSION = "2023.3"
+COMPILER_VERSION = "2024.0"
 RUNNING_TESTS = False
 """
     This is enabled/disabled by the test suite when tests are run.
