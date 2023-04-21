176ea66e6cdc6888f0394a0e217f12d524d29c11
Lejoly Florent <florent.lejoly@inmanta.com>
2023-04-21 15:05:09 +0200

    update

--- src/inmanta/server/services/compilerservice.py
+++ src/inmanta/server/services/compilerservice.py
@@ -563,7 +563,7 @@ class CompilerService(ServerSlice, environmentservice.EnvironmentListener):
                 "message": "Full recompile triggered by AUTO_FULL_COMPILE cron schedule",
             }
 
-            def recompile() -> TaskMethod:
+            def create_task() -> TaskMethod:
                 if env.halted:
 
                     async def do_nothing() -> None:
@@ -580,6 +580,7 @@ class CompilerService(ServerSlice, environmentservice.EnvironmentListener):
                         metadata=metadata,
                     )
 
+            recompile: TaskMethod = create_task()
             self.schedule_cron(recompile, schedule_cron, cancel_on_stop=False)
             self._scheduled_full_compiles[env.id] = (recompile, schedule_cron)
 
