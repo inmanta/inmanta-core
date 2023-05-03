a020d899cf4ae77319e008090bbecb51a00ccb04
Wouter De Borger <wouter.deborger@inmanta.com>
2023-05-03 09:15:08 +0200

    Make sure resource.version == resource.id.version (Issue #5931, PR #5934)
    # Description
    
    Ensure  resource.version == resource.id.version
    
    closes #5931
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~~
    - [ ] ~~If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)~~

--- src/inmanta/data/__init__.py
+++ src/inmanta/data/__init__.py
@@ -4362,8 +4362,7 @@ class Resource(BaseDocument):
         # Due to a bug, the version field has always been present in the attributes dictionary.
         # This bug has been fixed in the database. For backwards compatibility reason we here make sure that the
         # version field is present in the attributes dictionary served out via the API.
-        if "version" not in record["attributes"]:
-            record["attributes"]["version"] = version
+        record["attributes"]["version"] = version
         record["provides"] = [resources.Id.set_version_in_id(id, version) for id in record["provides"]]
 
     @classmethod
@@ -5015,8 +5014,7 @@ class Resource(BaseDocument):
         # Due to a bug, the version field has always been present in the attributes dictionary.
         # This bug has been fixed in the database. For backwards compatibility reason we here make sure that the
         # version field is present in the attributes dictionary served out via the API.
-        if "version" not in self.attributes:
-            attributes["version"] = self.model
+        attributes["version"] = self.model
 
         return m.Resource(
             environment=self.environment,
