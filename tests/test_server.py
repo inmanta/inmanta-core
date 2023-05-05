48ff4215fcf4842dd10eddd0ac9e72c17e8ba4e9
FloLey <florent.lejoly@inmanta.com>
2023-05-05 10:21:33 +0200

    update

--- tests/test_server.py
+++ tests/test_server.py
@@ -1446,9 +1446,10 @@ async def test_cleanup_old_agents(server, client, env1_halted, env2_halted):
     await env1.set(data.AUTOSTART_AGENT_MAP, {"agent3": "", "internal": ""})
     await env2.set(data.AUTOSTART_AGENT_MAP, {"agent1": "", "internal": ""})
 
+    # these checks are here to investigate the fact that the test case is flaky and that we have a suspicion
+    # that it is an issue with the agent map
     agentmap_env1 = await client.get_setting(tid=env1.id, id=data.AUTOSTART_AGENT_MAP)
     agentmap_env2 = await client.get_setting(tid=env2.id, id=data.AUTOSTART_AGENT_MAP)
-
     assert agentmap_env1.result["value"] == {"agent3": "", "internal": ""}
     assert agentmap_env2.result["value"] == {"agent1": "", "internal": ""}
 
@@ -1524,17 +1525,19 @@ async def test_cleanup_old_agents(server, client, env1_halted, env2_halted):
     agents_before_purge = await data.Agent.get_list()
     assert len(agents_before_purge) == 6
 
+    # these checks are here to investigate the fact that the test case is flaky and that we have a suspicion
+    # that it is an issue with the agent map
     agentmap_env1 = await client.get_setting(tid=env1.id, id=data.AUTOSTART_AGENT_MAP)
     agentmap_env2 = await client.get_setting(tid=env2.id, id=data.AUTOSTART_AGENT_MAP)
-
     assert agentmap_env1.result["value"] == {"agent3": "", "internal": ""}
     assert agentmap_env2.result["value"] == {"agent1": "", "internal": ""}
 
     await server.get_slice(SLICE_ORCHESTRATION)._purge_versions()
 
+    # these checks are here to investigate the fact that the test case is flaky and that we have a suspicion
+    # that it is an issue with the agent map
     agentmap_env1 = await client.get_setting(tid=env1.id, id=data.AUTOSTART_AGENT_MAP)
     agentmap_env2 = await client.get_setting(tid=env2.id, id=data.AUTOSTART_AGENT_MAP)
-
     assert agentmap_env1.result["value"] == {"agent3": "", "internal": ""}
     assert agentmap_env2.result["value"] == {"agent1": "", "internal": ""}
 
