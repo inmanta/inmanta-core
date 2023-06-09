d2a2510cfece35a813e5aab6ed8331ebdee65e39
hugo <hugo.thiolliere@inmanta.com>
2023-06-09 10:23:41 +0200

    Improve error reporting when attempting to move a resource to another resource set in a partial compile. (Issue #5884, PR #6095)
    Pull request opened by the merge tool on behalf of #6095

--- src/inmanta/data/__init__.py
+++ src/inmanta/data/__init__.py
@@ -4944,7 +4944,7 @@ class Resource(BaseDocument):
         deleted_resource_sets: abc.Set[str],
         *,
         connection: Optional[asyncpg.connection.Connection] = None,
-    ) -> abc.Set[m.ResourceIdStr]:
+    ) -> dict[m.ResourceIdStr, str]:
         """
         Copy the resources that belong to an unchanged resource set of a partial compile,
         from source_version to destination_version. This method doesn't copy shared resources.
@@ -4983,7 +4983,7 @@ class Resource(BaseDocument):
                 FROM {cls.table_name()} AS r
                 WHERE r.environment=$1 AND r.model=$2 AND r.resource_set IS NOT NULL AND NOT r.resource_set=ANY($4)
             )
-            RETURNING resource_id
+            RETURNING resource_id, resource_set
         """
         async with cls.get_connection(connection) as con:
             result = await con.fetch(
@@ -4993,7 +4993,7 @@ class Resource(BaseDocument):
                 destination_version,
                 updated_resource_sets | deleted_resource_sets,
             )
-            return {record["resource_id"] for record in result}
+            return {str(record["resource_id"]): str(record["resource_set"]) for record in result}
 
     @classmethod
     async def get_resources_in_resource_sets(
