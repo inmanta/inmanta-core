eda977470b408de9bdb85424260ac7d5fdc4e2c9
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-06-12 12:17:44 +0200

    Fix dashes

--- src/inmanta/moduletool.py
+++ src/inmanta/moduletool.py
@@ -744,11 +744,11 @@ When a stable release is done, this command:
 * Does a commit that changes the current version to a development version that is one patch increment ahead of the released
   version.
 
-When a development release is done using the --dev option, this command:
+When a development release is done using the \-\-dev option, this command:
 
 * Does a commit that updates the current version of the module to a development version that is a patch, minor or major version
-  ahead of the previous stable release. The size of the increment is determined by the --revision, --patch, --minor or
-  --major argument (--patch is the default). When a CHANGELOG.md file is present in the root of the module
+  ahead of the previous stable release. The size of the increment is determined by the \-\-revision, \-\-patch, \-\-minor or
+  \-\-major argument (\-\-patch is the default). When a CHANGELOG.md file is present in the root of the module
   directory then the version number in the changelog is also updated accordingly. The changelog file is always populated with
   the associated stable version and not a development version.
             """.strip(),
