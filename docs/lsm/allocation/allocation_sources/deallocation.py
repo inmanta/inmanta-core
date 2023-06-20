808b4e99443d7c5009d7b9489649cbd452444ac3
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-06-20 11:47:55 +0200

    The CRUDHandler has been made generic with respect to the specific PurgeableResource it acts on. (Issue inmanta/inmanta-core#5555, PR #6129)
    # Description
    
    Make CRUDHandler generic
    
    closes #5555
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation
    - [ ] ~~If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s)~~

--- docs/lsm/allocation/allocation_sources/deallocation.py
+++ docs/lsm/allocation/allocation_sources/deallocation.py
@@ -101,7 +101,7 @@ class PGAllocationResource(PurgeableResource):
 
 
 @provider("vlan_assignment::PGAllocation", name="pgallocation")
-class PGAllocation(CRUDHandler):
+class PGAllocation(CRUDHandler[PGAllocationResource]):
     def __init__(self, *args, **kwargs):
         super(PGAllocation, self).__init__(*args, **kwargs)
         self._allocator = PGServiceIdAllocator(attribute="vlan_id")
