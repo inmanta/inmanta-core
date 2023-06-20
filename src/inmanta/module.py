775683282c5661f9690abfba17d092a3577659a2
hugo <hugo.thiolliere@inmanta.com>
2023-06-20 08:29:26 +0200

    [WIP]

--- src/inmanta/module.py
+++ src/inmanta/module.py
@@ -17,7 +17,6 @@
 """
 
 import configparser
-import enum
 import glob
 import importlib
 import logging
@@ -35,6 +34,7 @@ from abc import ABC, abstractmethod
 from collections import abc, defaultdict
 from configparser import ConfigParser
 from dataclasses import dataclass
+from enum import EnumMeta, Enum
 from functools import reduce
 from importlib.abc import Loader
 from io import BytesIO, TextIOBase
@@ -1484,11 +1484,52 @@ class ModuleV2Metadata(ModuleMetadata):
 
         return out
 
+class OnAccess(EnumMeta):
+    """
+    runs a user-specified function whenever member is accessed
+    """
+    #
+    def __getattribute__(cls, name):
+        obj = super().__getattribute__(name)
+        if isinstance(obj, Enum) and obj._on_access:
+            obj._on_access()
+        return obj
+    #
+    def __getitem__(cls, name):
+        member = super().__getitem__(name)
+        if member._on_access:
+            member._on_access()
+        return member
+    #
+    def __call__(cls, value, names=None, *, module=None, qualname=None, type=None, start=1):
+        obj = super().__call__(value, names, module=module, qualname=qualname, type=type, start=start)
+        if isinstance(obj, Enum) and obj._on_access:
+            obj._on_access()
+        return obj
+
+class DeprecatedEnum(Enum, metaclass=OnAccess):
+    #
+    def __new__(cls, value, *args):
+        member = object.__new__(cls)
+        member._value_ = value
+        member._args = args
+        member._on_access = member.deprecate if args else None
+        return member
+    #
+    def deprecate(self):
+        args = (self.name, ) + self._args
+        import warnings
+        warnings.warn(
+                "member %r is deprecated; %s" % args,
+                DeprecationWarning,
+                stacklevel=3,
+                )
+
 
 @stable_api
-class ModuleRepoType(enum.Enum):
+class ModuleRepoType(DeprecatedEnum):
     git = "git"
-    package = "package"
+    package = "package", "Please set the pip index url through the project.yml `pip -> index_url` option."
 
 
 @stable_api
