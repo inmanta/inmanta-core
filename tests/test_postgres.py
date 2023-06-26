a16a3e1e42ee0f9bd34986d453b1804c9c2144b4
Lejoly Florent <florent.lejoly@inmanta.com>
2023-06-26 11:10:55 +0200

    update

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -27,70 +27,69 @@ from collections import abc
 import asyncpg
 import pytest
 
-
-@pytest.mark.slowtest
-async def test_postgres_cascade_locking_order(postgresql_pool) -> None:
-    """
-    Verifies that Postgres' cascade deletion acquires locks top-down. This is important because in order to avoid deadlocks
-    we define a corresponding locking order for all transactions. See `TableLockMode`, `RowLockMode` and `ConfigurationModel`
-    docstrings in `inmanta.data`.
-    """
-    async with postgresql_pool.acquire() as connection:
-        await connection.execute(
-            """
-            CREATE TABLE IF NOT EXISTS root (name varchar PRIMARY KEY);
-            CREATE TABLE IF NOT EXISTS leaf (
-                name varchar PRIMARY KEY,
-                myroot varchar REFERENCES root(name) ON DELETE CASCADE
-            );
-            """
-        )
-
-    async def insert():
-        async with postgresql_pool.acquire() as connection:
-            await connection.execute(
-                """
-                INSERT INTO root VALUES
-                    ('root1');
-
-                INSERT INTO leaf VALUES
-                    ('leaf1', 'root1'),
-                    ('leaf2', 'root1');
-                """
-            )
-
-    async def lock_top_down():
-        async with postgresql_pool.acquire() as connection:
-            async with connection.transaction():
-                await connection.execute("LOCK TABLE root IN SHARE MODE")
-                await asyncio.sleep(0.1)
-                await connection.execute("LOCK TABLE leaf IN SHARE MODE")
-
-    async def lock_bottom_up():
-        async with postgresql_pool.acquire() as connection:
-            async with connection.transaction():
-                await connection.execute("LOCK TABLE leaf IN SHARE MODE")
-                await asyncio.sleep(0.1)
-                await connection.execute("LOCK TABLE root IN SHARE MODE")
-
-    async def delete():
-        async with postgresql_pool.acquire() as connection:
-            async with connection.transaction():
-                await asyncio.sleep(0.05)
-                await connection.execute("DELETE FROM root")
-
-    await insert()
-    await asyncio.gather(
-        lock_top_down(),
-        delete(),
-    )
-
-    await insert()
-    with pytest.raises(asyncpg.DeadlockDetectedError):
-        await asyncio.gather(
-            lock_bottom_up(),
-            delete(),
-        )
+# @pytest.mark.slowtest
+# async def test_postgres_cascade_locking_order(postgresql_pool) -> None:
+#     """
+#     Verifies that Postgres' cascade deletion acquires locks top-down. This is important because in order to avoid deadlocks
+#     we define a corresponding locking order for all transactions. See `TableLockMode`, `RowLockMode` and `ConfigurationModel`
+#     docstrings in `inmanta.data`.
+#     """
+#     async with postgresql_pool.acquire() as connection:
+#         await connection.execute(
+#             """
+#             CREATE TABLE IF NOT EXISTS root (name varchar PRIMARY KEY);
+#             CREATE TABLE IF NOT EXISTS leaf (
+#                 name varchar PRIMARY KEY,
+#                 myroot varchar REFERENCES root(name) ON DELETE CASCADE
+#             );
+#             """
+#         )
+#
+#     async def insert():
+#         async with postgresql_pool.acquire() as connection:
+#             await connection.execute(
+#                 """
+#                 INSERT INTO root VALUES
+#                     ('root1');
+#
+#                 INSERT INTO leaf VALUES
+#                     ('leaf1', 'root1'),
+#                     ('leaf2', 'root1');
+#                 """
+#             )
+#
+#     async def lock_top_down():
+#         async with postgresql_pool.acquire() as connection:
+#             async with connection.transaction():
+#                 await connection.execute("LOCK TABLE root IN SHARE MODE")
+#                 await asyncio.sleep(0.1)
+#                 await connection.execute("LOCK TABLE leaf IN SHARE MODE")
+#
+#     async def lock_bottom_up():
+#         async with postgresql_pool.acquire() as connection:
+#             async with connection.transaction():
+#                 await connection.execute("LOCK TABLE leaf IN SHARE MODE")
+#                 await asyncio.sleep(0.1)
+#                 await connection.execute("LOCK TABLE root IN SHARE MODE")
+#
+#     async def delete():
+#         async with postgresql_pool.acquire() as connection:
+#             async with connection.transaction():
+#                 await asyncio.sleep(0.05)
+#                 await connection.execute("DELETE FROM root")
+#
+#     await insert()
+#     await asyncio.gather(
+#         lock_top_down(),
+#         delete(),
+#     )
+#
+#     await insert()
+#     with pytest.raises(asyncpg.DeadlockDetectedError):
+#         await asyncio.gather(
+#             lock_bottom_up(),
+#             delete(),
+#         )
 
 
 @pytest.mark.slowtest
