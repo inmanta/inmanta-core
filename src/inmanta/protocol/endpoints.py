aec210f0beb16baf850a77986f2b09f449f115cb
Lejoly Florent <florent.lejoly@inmanta.com>
2023-07-04 11:42:09 +0200

    update

--- src/inmanta/protocol/endpoints.py
+++ src/inmanta/protocol/endpoints.py
@@ -193,7 +193,7 @@ class SessionEndpoint(Endpoint, CallTarget):
         assert self._env_id is not None
         LOGGER.log(3, "Starting agent for %s", str(self.sessionid))
         self._client = SessionClient(self.name, self.sessionid, timeout=self.server_timeout)
-        self._heartbeat_client = SessionClient(self.name, self.sessionid, timeout=self.server_timeout, force_instance=True)
+        self._heartbeat_client = SessionClient(self.name, self.sessionid, timeout=self.server_timeout)
         await self.start_connected()
         self.add_background_task(self.perform_heartbeat())
 
@@ -334,13 +334,12 @@ class Client(Endpoint):
         version_match: VersionMatch = VersionMatch.lowest,
         exact_version: int = 0,
         with_rest_client: bool = True,
-        force_instance: bool = False,
     ) -> None:
         super().__init__(name)
         assert isinstance(timeout, int), "Timeout needs to be an integer value."
         LOGGER.debug("Start transport for client %s", self.name)
         if with_rest_client:
-            self._transport_instance = client.RESTClient(self, connection_timout=timeout, force_instance=force_instance)
+            self._transport_instance = client.RESTClient(self, connection_timout=timeout)
         else:
             self._transport_instance = None
         self._version_match = version_match
@@ -448,8 +447,8 @@ class SessionClient(Client):
     A client that communicates with server endpoints over a session.
     """
 
-    def __init__(self, name: str, sid: uuid.UUID, timeout: int = 120, force_instance: bool = False) -> None:
-        super().__init__(name, timeout, force_instance=force_instance)
+    def __init__(self, name: str, sid: uuid.UUID, timeout: int = 120) -> None:
+        super().__init__(name, timeout)
         self._sid = sid
 
     async def _call(
