e4a975746585ea096d92c7d4db06774fc29550c2
Lejoly Florent <florent.lejoly@inmanta.com>
2023-07-10 16:36:55 +0200

    update

--- tests/compiler/test_not.py
+++ tests/compiler/test_not.py
@@ -20,54 +20,21 @@ import textwrap
 from inmanta import compiler
 
 
-def test_list_not(snippetcompiler, capsys) -> None:
-    """
-    Verify the basic workings of the list comprehension expression.
-    """
-    snippetcompiler.setup_for_snippet(
-        textwrap.dedent(
-            """
-            test_list = [1, 2, 3, 4, 5]
-
-            if not (6 in test_list):
-                std::print("true")
-            else:
-                std::print("false")
-            end
-
-
-            """.strip(
-                "\n"
-            )
-        )
-    )
-    compiler.do_compile()
-    out, err = capsys.readouterr()
-    assert out == "true\n"
-    assert err == ""
-
-
 def test_list_not_in(snippetcompiler, capsys) -> None:
     """
-    Verify the basic workings of the list comprehension expression.
+    Verify the basic workings of the 'not ( value in expression)' and the 'value not in expression' syntax
     """
     snippetcompiler.setup_for_snippet(
         textwrap.dedent(
             """
             test_list = [1, 2, 3, 4, 5]
-
-            if 6 not in test_list:
-                std::print("true")
-            else:
-                std::print("false")
-            end
-
+            assert = not (6 in test_list)
+            assert = 6 not in test_list
+            assert = 5 in test_list
+            assert = not (5 not in test_list)
             """.strip(
                 "\n"
             )
         )
     )
     compiler.do_compile()
-    out, err = capsys.readouterr()
-    assert out == "true\n"
-    assert err == ""
