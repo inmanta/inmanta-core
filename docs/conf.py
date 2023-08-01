7eeedcfd35d64402dacf547b3f6c16bcec7c25c8
Sander Van Balen <git@sandervanbalen.be>
2023-08-01 11:21:59 +0200

    dropped newline

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,6 @@ import sys, os, pkg_resources, datetime
 from importlib.metadata import PackageNotFoundError
 from sphinx.errors import ConfigError
 
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
