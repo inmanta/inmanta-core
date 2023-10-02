ecddfcbb2a38a9f4fdff7d9323d29538417f10ce
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2023-10-02 11:46:41 +0200

    Make sure to set running to True

--- src/inmanta/agent/agent.py
+++ src/inmanta/agent/agent.py
@@ -212,10 +212,23 @@ class ResourceAction(ResourceActionBase):
             results: List[ResourceActionResult] = cast(List[ResourceActionResult], await asyncio.gather(*waiters))
 
             if self.undeployable:
-                self.status = self.undeployable
-                self.change = const.Change.nochange
-                self.changes = {}
-                self.future.set_result(ResourceActionResult(cancel=False))
+                self.running = True
+                try:
+                    if self.is_done():
+                        # Action is cancelled
+                        self.logger.log(const.LogLevel.TRACE.to_int, "%s %s is no longer active" % (self.gid, self.resource))
+                        return
+                    result = sum(results, ResourceActionResult(cancel=False))
+                    if result.cancel:
+                        # self.running will be set to false when self.cancel is called
+                        # Only happens when global cancel has not cancelled us but our predecessors have already been cancelled
+                        return
+                    self.status = self.undeployable
+                    self.change = const.Change.nochange
+                    self.changes = {}
+                    self.future.set_result(ResourceActionResult(cancel=False))
+                finally:
+                    self.running = False
                 return
 
             async with self.scheduler.ratelimiter:
