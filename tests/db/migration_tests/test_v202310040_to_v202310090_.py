abcc8839177577ae16469ee1fb8f680cc701eb95
hugo <hugo.thiolliere@inmanta.com>
2023-10-09 10:37:14 +0200

    [WIP]

---
+++ tests/db/migration_tests/test_v202310040_to_v202310090_.py
@@ -0,0 +1,31 @@
+"""
+    Copyright 2023 Inmanta
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
+import os
+from collections import abc
+from inmanta.data import Environment
+
+import pytest
+
+@pytest.mark.db_restore_dump(os.path.join(os.path.dirname(__file__), "dumps/v202310040.sql"))
+async def test_type_change(migrate_db_from: abc.Callable[[], abc.Awaitable[None]],  get_tables_in_db: abc.Callable[[], abc.Awaitable[list[str]]]) -> None:
+    await migrate_db_from()
+
+    env = await Environment.get_one(name="dev-1")
+    assert isinstance(env.settings["autostart_agent_deploy_interval"], str)
+    assert isinstance(env.settings["autostart_agent_repair_interval"], str)
+
