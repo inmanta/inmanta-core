26161a175e1af9c278d9a932600366f029e1d50a
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2023-10-20 15:09:47 +0200

    Update docstring

--- src/inmanta/agent/handler.py
+++ src/inmanta/agent/handler.py
@@ -979,10 +979,7 @@ class DiscoveryHandler(HandlerAPI[TDiscovery], Generic[TDiscovery, TDiscovered])
           conventional resource type expected to be deployed on a network, but rather a way to express
           the intent to discover resources of the second type TDiscovered already present on the network.
         - TDiscovered denotes the handler's Unmanaged Resource type. This is the type of the resources that have been
-          discovered and reported to the server. Objects of this type must be either:
-             1. A pydantic object
-             2. An object that has a `to_dict()` method that returns a serializable dictionary
-             3. A serializable dictionary
+          discovered and reported to the server. Objects of this type must an pydantic object.
     """
 
     def check_facts(self, ctx: HandlerContext, resource: TDiscovery) -> Dict[str, object]:
