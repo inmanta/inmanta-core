7ab56f999f6a4a103f19a6a9f07f8a9edf03c5fb
Florent Lejoly <florent.lejoly@inmanta.com>
2023-11-08 16:53:47 +0100

    update

--- src/inmanta/ast/type.py
+++ src/inmanta/ast/type.py
@@ -591,9 +591,7 @@ class Literal(Union):
     """
 
     def __init__(self) -> None:
-        Union.__init__(
-            self, [NullableType(Integer()), Float(), Bool(), String(), TypedList(self), TypedDict(self)]
-        )
+        Union.__init__(self, [NullableType(Integer()), Float(), Bool(), String(), TypedList(self), TypedDict(self)])
 
     def type_string_internal(self) -> str:
         return "Literal"
