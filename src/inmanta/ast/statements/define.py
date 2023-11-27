42c15e93cd0993121b3c261f23398f031473e0bc
Florent Lejoly <florent.lejoly@inmanta.com>
2023-11-27 16:14:52 +0100

    update

--- src/inmanta/ast/statements/define.py
+++ src/inmanta/ast/statements/define.py
@@ -294,7 +294,7 @@ class DefineImplementation(TypeDefinitionStatement):
                 )
             self.type.set_type(cls)
             self.copy_location(self.type)
-            self.block.normalize()
+            # self.block.normalize() #todo
             self.anchors.extend(self.block.get_anchors())
         except TypeNotFoundException as e:
             e.set_statement(self)
