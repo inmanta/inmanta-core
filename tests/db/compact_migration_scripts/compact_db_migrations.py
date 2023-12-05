8ac9a1286a1d65a081f83516cd163e25dadacb93
Lejoly Florent <florent.lejoly@inmanta.com>
2023-12-05 11:45:31 +0100

    update

--- tests/db/compact_migration_scripts/compact_db_migrations.py
+++ tests/db/compact_migration_scripts/compact_db_migrations.py
@@ -20,6 +20,8 @@
 """
 import asyncio
 import importlib
+import os
+import re
 
 import asyncpg
 import pytest
@@ -29,10 +31,51 @@ from inmanta.data.schema import CORE_SCHEMA_NAME, DBSchema
 PACKAGE_NAME = "versions_to_compact"
 
 
+def extract_version(filename):
+    match = re.match(r"^v(\d+)", filename)
+    return int(match.group(1)) if match else None
+
+
+def get_sorted_versions(directory):
+    versions = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".py"):
+            version = extract_version(filename)
+            if version is not None:
+                versions.append(version)
+    return sorted(versions)
+
+
+def check_versions(compact_dir, original_dir):
+    compact_versions = get_sorted_versions(compact_dir)
+    original_versions = get_sorted_versions(original_dir)
+
+    if not compact_versions or not original_versions:
+        raise ValueError("One or both directories are empty")
+
+    if compact_versions[0] != 1:
+        raise ValueError("The v1 migration script is missing in the scripts to be compacted.")
+
+    highest_compact = compact_versions[-1]
+    lowest_original = original_versions[0]
+
+    if highest_compact > lowest_original:
+        raise ValueError(
+            f"Overlap detected: a version number in the scripts to compact ({highest_compact}) has a higher "
+            f"version than on of the remaining migration scripts({lowest_original})"
+        )
+
+
 async def test_compact_and_dump(postgres_db, database_name):
     """
     Compact, apply database migrations using DBSchema, and dump the database schema with modifications.
     """
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    compact_dir = os.path.join(script_dir, "versions_to_compact")
+    original_dir = os.path.abspath(os.path.join(script_dir, "../../../src/inmanta/db/versions"))
+
+    check_versions(compact_dir, original_dir)
+
     outfile = f"{os.path.dirname(__file__)}/compacted_dump.sql"
     connection = await asyncpg.connect(
         host=postgres_db.host,
