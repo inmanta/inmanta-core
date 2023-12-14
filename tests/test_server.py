eb46945577803934f721cde02c4ccaeba8414770
FloLey <95339363+FloLey@users.noreply.github.com>
2023-12-14 15:42:33 +0100

    Update tests/test_server.py
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- tests/test_server.py
+++ tests/test_server.py
@@ -844,7 +844,7 @@ async def test_get_resource_actions(postgresql_client, client, clienthelper, ser
     #  adding a resource action with its change field set to "created" to test the get_resource_actions
     #  filtering on resources with changes
 
-    rvid_r1_v1 = "std::File[agent1,path=/etc/file1],v=1"
+    rvid_r1_v1 = f"std::File[agent1,path=/etc/file1],v={version}"
     resources.append(
         {
             "group": "root",
