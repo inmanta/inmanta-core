de687c594a05cefec37fa04fad9644fd8b7070fe
FloLey <95339363+FloLey@users.noreply.github.com>
2023-12-19 15:56:54 +0100

    Update src/inmanta/protocol/rest/__init__.py
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- src/inmanta/protocol/rest/__init__.py
+++ src/inmanta/protocol/rest/__init__.py
@@ -202,7 +202,7 @@ class CallArguments:
 
         is_generic_list = arg_type and typing_inspect.is_generic_type(arg_type) and typing_inspect.get_origin(arg_type) is list
         is_single_type_list = len(typing_inspect.get_args(arg_type, evaluate=True)) == 1
-        is_not_list = not isinstance(message_arg, list)
+        arg_value_is_not_list = not isinstance(message_arg, list)
 
         if is_generic_list and is_not_list and is_single_element_list:
             return [message_arg]
