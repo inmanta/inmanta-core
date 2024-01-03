bb4083c92c619719f1ef1e488646c535f7e743e4
FloLey <95339363+FloLey@users.noreply.github.com>
2024-01-03 14:29:07 +0100

    Update src/inmanta/app.py
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- src/inmanta/app.py
+++ src/inmanta/app.py
@@ -83,7 +83,7 @@ LOGGER = logging.getLogger("inmanta")
 
 @command("server", help_msg="Start the inmanta server")
 def start_server(options: argparse.Namespace) -> None:
-    LOGGER.info(f"Starting inmanta-server version {COMPILER_VERSION}")
+    LOGGER.info(f"Starting inmanta-server version %s", COMPILER_VERSION)
     if options.config_file and not os.path.exists(options.config_file):
         LOGGER.warning("Config file %s doesn't exist", options.config_file)
 
