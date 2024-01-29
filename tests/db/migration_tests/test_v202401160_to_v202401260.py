ccc97e4677d26a9e4ee8a6c9748b1b555720bf8a
hugo <hugo.thiolliere@inmanta.com>
2024-01-29 15:40:51 +0100

    [WIP]

--- tests/db/migration_tests/test_v202401160_to_v202401260.py
+++ tests/db/migration_tests/test_v202401160_to_v202401260.py
@@ -36,32 +36,37 @@ async def test_add_non_null_constraint(
     r"""
     This migration script adds the non-null constraint to the ``expires`` column of the parameter table.
 
-    The v202401160.sql dump is initialized with 2 parameters and 2 facts:
+    The v202401160.sql dump is initialized with 3 parameters and 3 facts:
 
     COPY public.parameter (id, name, value, environment, resource_id, source, updated, metadata, expires) FROM stdin;
-    dc7b5b59-33f4-417b-9afc-3c91b24b4008	fact1	value1	630a163d-d010-4f70-b158-3434f7477aa9	std::File[localhost,path=/tmp/test1]	fact	\N	\N	f       # NOQA E501
-    379e582a-007e-46eb-ac76-479c23da3d14	fact2	value2	630a163d-d010-4f70-b158-3434f7477aa9	std::File[localhost,path=/tmp/test2]	fact	\N	\N	\N      # NOQA E501
-    7428c4f7-d03d-47f9-8cf0-bce6688b61b3	parameter1	value1	630a163d-d010-4f70-b158-3434f7477aa9		fact	\N	\N	f
-    bb53a43c-c74f-43a9-999d-dc04c54324a1	parameter2	value2	630a163d-d010-4f70-b158-3434f7477aa9		fact	\N	\N	\N
+    da7cdfcd-b88d-41f6-86d2-49251adabea1	fact1	value1	b1ed3482-7826-4c31-a185-0d6993192e38	std::File[localhost,path=/tmp/test1]	fact	\N	\N	f  # NOQA E501
+    adef7ac5-c24b-474a-8cdc-edfa27362960	fact2	value2	b1ed3482-7826-4c31-a185-0d6993192e38	std::File[localhost,path=/tmp/test2]	fact	\N	\N	\N  # NOQA E501
+    f817f3b4-9079-4320-be99-172b00823b87	fact3	value3	b1ed3482-7826-4c31-a185-0d6993192e38	std::File[localhost,path=/tmp/test3]	fact	\N	\N	t  # NOQA E501
+    dea1161f-9561-485a-9541-dded930d69c6	parameter1	value1	b1ed3482-7826-4c31-a185-0d6993192e38		fact	\N	\N	f
+    757dec65-3367-437a-82d3-6b8f53739dad	parameter2	value2	b1ed3482-7826-4c31-a185-0d6993192e38		fact	\N	\N	\N
+    0c3f2e4a-2aaf-4c6f-bdbb-075679b16f31	parameter3	value3	b1ed3482-7826-4c31-a185-0d6993192e38		fact	\N	\N	t
     \.
 
     This test checks that a sensible default value is set for parameters/facts with a null value for the 'expires' column
     i.e. parameters never expire and facts always expire by default
+    And that parameters never expire
     """
 
     await migrate_db_from()
 
     expected_expire_values: dict[str, bool] = {
         # Facts:                                  Value before migration:
-        "dc7b5b59-33f4-417b-9afc-3c91b24b4008": False,  # False
-        "379e582a-007e-46eb-ac76-479c23da3d14": True,  # None
+        "da7cdfcd-b88d-41f6-86d2-49251adabea1": False,  # False
+        "adef7ac5-c24b-474a-8cdc-edfa27362960": True,  # None
+        "f817f3b4-9079-4320-be99-172b00823b87": True,  # True
         # Parameters:
-        "7428c4f7-d03d-47f9-8cf0-bce6688b61b3": False,  # False
-        "bb53a43c-c74f-43a9-999d-dc04c54324a1": False,  # None
+        "dea1161f-9561-485a-9541-dded930d69c6": False,  # False
+        "757dec65-3367-437a-82d3-6b8f53739dad": False,  # None
+        "0c3f2e4a-2aaf-4c6f-bdbb-075679b16f31": False,  # True
     }
 
     client = Client("client")
-    result = await client.list_params(tid="630a163d-d010-4f70-b158-3434f7477aa9")
+    result = await client.list_params(tid="b1ed3482-7826-4c31-a185-0d6993192e38")
     assert result.code == 200
     assert len(result.result["parameters"]) == len(expected_expire_values)
     for param in result.result["parameters"]:
