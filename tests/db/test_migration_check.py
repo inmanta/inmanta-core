d24dac579cd4a76af0f3132eb630623cb45c6644
Wouter De Borger <wouter.deborger@inmanta.com>
2024-02-06 16:48:12 +0100

    add missing fixture

--- tests/db/test_migration_check.py
+++ tests/db/test_migration_check.py
@@ -18,7 +18,7 @@
 from pathlib import Path
 
 
-def test_migration_check():
+def test_migration_check(clean_reset):
     """
     Make sure there is a database dump for the latest version of the db and
     that a migration test exists for this dump.
