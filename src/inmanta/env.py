525d4b2d63bb79e9f3ef80b406946613015d2a29
FloLey <95339363+FloLey@users.noreply.github.com>
2024-02-09 09:01:43 +0100

    Update src/inmanta/env.py
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- src/inmanta/env.py
+++ src/inmanta/env.py
@@ -511,7 +511,7 @@ class Pip(PipCommandBuilder):
             not_found: list[str] = []
             conflicts: list[str] = []
             indexes: str = ""
-            no_index: bool = "--no-index" in cmd or strtobool(env.get("PIP_NO_INDEX", "false").lower())
+            no_index: bool = "--no-index" in cmd or strtobool(env.get("PIP_NO_INDEX", "false"))
             for line in full_output:
                 m = re.search(r"No matching distribution found for ([\S]+)", line)
                 if m:
