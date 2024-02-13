91a9c84fa449f56571840d61c803f9ca0fef1791
hugo <hugo.thiolliere@inmanta.com>
2024-02-13 10:19:48 +0100

    [WIP]

--- tests/db/migration_tests/test_v202402080_to_v202402130.py
+++ tests/db/migration_tests/test_v202402080_to_v202402130.py
@@ -40,44 +40,22 @@ async def test_add_non_null_constraint(
       - the non-null constraint to the ``undeployable`` and ``skipped_for_undeployable``
         columns of the configurationmodel table.
       - default empty arrays instead of NULL for these columns
-
-
     """
 
-    result = await postgresql_client.fetch(
-        """
-            SELECT * FROM public.configurationmodel;
-        """
-    )
-    # data = json.loads(result[0])
-    assert result[0]
-    # assert isinstance(settings[data.AUTOSTART_AGENT_DEPLOY_INTERVAL], int)
-    # assert isinstance(settings[data.AUTOSTART_AGENT_REPAIR_INTERVAL], int)
+    query = """
+        SELECT undeployable, skipped_for_undeployable FROM public.configurationmodel
+        WHERE environment ='138898f0-adaa-4018-ba0a-733a23847318'
+        AND version=6;
+    """
+    result = await postgresql_client.fetch(query)
+
+    assert result[0]["undeployable"] is None
+    assert result[0]["skipped_for_undeployable"] is None
 
     await migrate_db_from()
 
-    result = await postgresql_client.fetch(
-        """
-            SELECT * FROM public.configurationmodel;
-        """
-    )
-    assert result[0]
-    assert  True
-    #
-    # expected_expire_values: dict[str, bool] = {
-    #     # Facts:                                  Value before migration:
-    #     "da7cdfcd-b88d-41f6-86d2-49251adabea1": False,  # False
-    #     "adef7ac5-c24b-474a-8cdc-edfa27362960": True,  # None
-    #     "f817f3b4-9079-4320-be99-172b00823b87": True,  # True
-    #     # Parameters:
-    #     "dea1161f-9561-485a-9541-dded930d69c6": False,  # False
-    #     "757dec65-3367-437a-82d3-6b8f53739dad": False,  # None
-    #     "0c3f2e4a-2aaf-4c6f-bdbb-075679b16f31": False,  # True
-    # }
-    #
-    # client = Client("client")
-    # result = await client.list_params(tid="b1ed3482-7826-4c31-a185-0d6993192e38")
-    # assert result.code == 200
-    # assert len(result.result["parameters"]) == len(expected_expire_values)
-    # for param in result.result["parameters"]:
-    #     assert expected_expire_values[param["id"]] == param["expires"], param["name"]
+    result = await postgresql_client.fetch(query)
+
+    assert isinstance(result[0]["undeployable"], list)
+    assert isinstance(result[0]["skipped_for_undeployable"], list)
+
