6089aca905b22be6d8040271e782e1b52f1fd25c
Lejoly Florent <florent.lejoly@inmanta.com>
2024-02-20 17:15:38 +0100

    update

--- tests/test_server.py
+++ tests/test_server.py
@@ -880,7 +880,7 @@ async def test_bootloader_db_wait(monkeypatch, tmpdir, caplog, wait_up):
 @pytest.mark.parametrize("wait_up", ["True", "False"])
 async def test_bootlader_connect_running_db(server_config, postgres_db, caplog, wait_up):
     """
-    Tests that the bootloader can connect to an existing database and can start.
+    Tests that the bootloader can connect to a database and can start for both wait_up values
     """
     config.Config.set("database", "wait_up", wait_up)
     caplog.set_level(logging.INFO)
