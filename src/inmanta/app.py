4de1526f90bb9b8796ec70101d8eb0d994720683
Florent Lejoly <florent.lejoly@inmanta.com>
2024-02-22 11:52:07 +0100

    Introduced a new configuration option `database.wait_time` for the Inmanta server, enabling it to wait for the database to become available before starting. Users can specify the maximum time (in seconds) the server should wait for the database to be up. A value of 0 means the server will not wait, while a negative value indicates the server will wait indefinitely. (Issue #6994, PR #7217)
    # Description
    
    In order to make the server start in a container, it is convenient if it can wait for the DB to be up.
    This PR adds an option that makes the server poll for the DB and only start when the DB is there.
    
    closes #6994
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

--- src/inmanta/app.py
+++ src/inmanta/app.py
@@ -71,13 +71,26 @@ from inmanta.warnings import WarningsManager
 LOGGER = logging.getLogger("inmanta")
 
 
-@command("server", help_msg="Start the inmanta server")
+def server_parser_config(parser: argparse.ArgumentParser, parent_parsers: abc.Sequence[argparse.ArgumentParser]) -> None:
+    parser.add_argument(
+        "--db-wait-time",
+        type=int,
+        dest="db_wait_time",
+        help="Maximum time in seconds the server will wait for the database to be up before starting. "
+        "A value of 0 means the server will not wait. If set to a negative value, the server will wait indefinitely.",
+    )
+
+
+@command("server", help_msg="Start the inmanta server", parser_config=server_parser_config)
 def start_server(options: argparse.Namespace) -> None:
     if options.config_file and not os.path.exists(options.config_file):
         LOGGER.warning("Config file %s doesn't exist", options.config_file)
 
     if options.config_dir and not os.path.isdir(options.config_dir):
         LOGGER.warning("Config directory %s doesn't exist", options.config_dir)
+
+    if options.db_wait_time is not None:
+        Config.set("database", "wait_time", str(options.db_wait_time))
 
     util.ensure_event_loop()
 
