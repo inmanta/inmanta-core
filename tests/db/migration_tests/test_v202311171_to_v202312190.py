20fb0684c77819d87e3c0775c44e9d2a472ef4ae
Florent Lejoly <florent.lejoly@inmanta.com>
2024-03-07 14:30:03 +0100

    update

--- tests/db/migration_tests/test_v202311171_to_v202312190.py
+++ tests/db/migration_tests/test_v202311171_to_v202312190.py
@@ -52,7 +52,7 @@ async def test_resource_state_table(postgres_db, database_name, migrate_db_from:
     assert rps.last_success == last_success.started
     assert rps.last_produced_events == last_produced_events
 
-    # # Verify that orphaned resource is included in resource_persistent_state
-    # orphaned_rps = await ResourcePersistentState.get_one(environment=env.id, resource_id="std::File[localhost,path=/tmp/test2]")
-    # assert orphaned_rps
-    # assert orphaned_rps.last_deployed_version == 2  # Last version where the resource existed
+    # Verify that orphaned resource is included in resource_persistent_state
+    orphaned_rps = await ResourcePersistentState.get_one(environment=env.id, resource_id="std::File[localhost,path=/tmp/test2]")
+    assert orphaned_rps
+    assert orphaned_rps.last_deployed_version == 2  # Last version where the resource existed
