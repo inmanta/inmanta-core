1fd22321d3881ec2b1e2f6b630e34c4ae4d863ca
hugo <hugo.thiolliere@inmanta.com>
2024-03-18 09:21:30 +0100

    [WIP]

--- tests/db/migration_tests/test_v202402130_to_v202403120.py
+++ tests/db/migration_tests/test_v202402130_to_v202403120.py
@@ -38,7 +38,7 @@ async def test_drop_not_null_constraint(
     await migrate_db_from()
 
     client = Client("client")
-    result = await client.get_reports(tid="4d6d694b-0915-495a-909c-582832c504fe")
+    result = await client.get_reports(tid="f015501d-e9e9-4016-914d-c7e214ae28bc")
     assert result.result["reports"]
     assert result.code == 200
     for report in result.result["reports"]:
