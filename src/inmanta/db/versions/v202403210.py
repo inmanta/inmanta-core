22f5f7b44cf261e4e872d1fc4d875c725cb0433f
Florent Lejoly <florent.lejoly@inmanta.com>
2024-03-21 16:48:20 +0100

    Change index on resource table to speed up query to get deployment progress in inmanta-lsm (Issue inmanta/inmanta-lsm#1630, PR #7410)
    # Description
    
    Change index on resource table to speed query to get deployment progress in inmanta-lsm
    
    part of https://github.com/inmanta/inmanta-lsm/pull/1655
    
    closes https://github.com/inmanta/inmanta-lsm/issues/1630
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [ ] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [ ] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

---
+++ src/inmanta/db/versions/v202403210.py
@@ -0,0 +1,32 @@
+"""
+    Copyright 2024 Inmanta
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
+
+from asyncpg import Connection
+
+
+async def update(connection: Connection) -> None:
+    """Change index on resource table to speed up query to get deployment progress in inmanta-lsm"""
+    schema = """
+-- Drop the old index
+DROP INDEX IF EXISTS resource_environment_model_resource_type_idx;
+
+-- Create the new index with the additional column
+CREATE INDEX resource_environment_model_resource_type_idx
+ON public.resource USING btree (environment, model, resource_type, resource_id_value);
+    """
+    await connection.execute(schema)
