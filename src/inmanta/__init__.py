3b085770fc659381576f4a8dbae0dedc591f59cc
inmantaci <ci@inmanta.com>
2024-04-12 09:58:21 +0200

    Merge tool: minor version bump

--- src/inmanta/__init__.py
+++ src/inmanta/__init__.py
@@ -19,7 +19,7 @@
 COMPILER_VERSION = "2024.2"
 # This version is managed by bumpversion. Should you ever update it manually, make sure to consistently update it everywhere
 # (See the bumpversion.cfg file for relevant locations).
-__version__ = "11.1.1"
+__version__ = "11.2.0"
 
 RUNNING_TESTS = False
 """
