c1a08be7e113a177c4fa68afb4ae9e2248615e25
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2024-05-29 15:44:39 +0200

    Update

--- src/inmanta/protocol/__init__.py
+++ src/inmanta/protocol/__init__.py
@@ -57,7 +57,7 @@ from . import methods, methods_v2
 from .auth import decode_token, encode_token
 from .common import Response, Result, gzipped_json, json_encode
 from .decorators import handle, method, typedmethod
-from .endpoints import Client, LocalClient, SessionClient, SessionEndpoint, SyncClient, VersionMatch
+from .endpoints import Client, SessionClient, SessionEndpoint, SyncClient, VersionMatch
 
 __all__ = [
     "Response",
@@ -65,7 +65,6 @@ __all__ = [
     "gzipped_json",
     "json_encode",
     "Client",
-    "LocalClient",
     "SessionClient",
     "SessionEndpoint",
     "SyncClient",
