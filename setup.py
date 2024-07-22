1bf7e9767a931322367327268adcf3ea0d932c65
Sander Van Balen <git@sandervanbalen.be>
2024-07-22 14:07:40 +0200

    added upper bound to setuptools dependency

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requires = [
     "pynacl~=1.5",
     "python-dateutil~=2.0",
     "pyyaml~=6.0",
-    "setuptools",
+    "setuptools<70",
     "texttable~=1.0",
     "tornado~=6.0",
     "typing-extensions>=4.8,<4.10",
