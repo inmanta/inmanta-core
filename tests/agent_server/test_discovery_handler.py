a2f5d5ff627956901012a08e54c2a80f788321de
hugo <hugo.thiolliere@inmanta.com>
2024-08-02 13:45:36 +0200

    [WIP]

--- tests/agent_server/test_discovery_handler.py
+++ tests/agent_server/test_discovery_handler.py
@@ -153,6 +153,7 @@ async def test_discovery_resource_handler_basic_test(
             "discovered_resource_id": f"test::MyUnmanagedResource[discovery_agent,val={val}]",
             "values": {"val": val},
             "managed_resource_uri": None,
+            'discovery_resource_id': resource_id,
             "discovery_resource_uri": f"/api/v2/resource/{parse.quote(resource_id)}",
         }
         for val in all_values
