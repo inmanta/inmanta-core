36cdf7c9800284717b26582da00097c7bd7b3dfe
Hugo Lloreda <hugo.lloreda.sanchez@inmanta.com>
2024-08-06 16:07:20 +0200

    removing old log lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -843,11 +843,7 @@ async def server(server_pre_start) -> abc.AsyncIterator[Server]:
 
     yield ibl.restserver
 
-    logger.info("WILL CLEAN SERVER")
-
     try:
-        logger.info("STOPPING")
-
         await ibl.stop(timeout=15)
     except concurrent.futures.TimeoutError:
         logger.exception("Timeout during stop of the server in teardown")
