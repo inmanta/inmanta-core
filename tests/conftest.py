8d575cdf86f63d05eb16cd6851535ab062f4d68c
Hugo Lloreda <hugo.lloreda.sanchez@inmanta.com>
2024-08-07 11:44:41 +0200

    change config

--- tests/conftest.py
+++ tests/conftest.py
@@ -815,6 +815,8 @@ async def server_config(event_loop, inmanta_config, postgres_db, database_name, 
         config.Config.set("server", "agent-timeout", "2")
         config.Config.set("agent", "agent-repair-interval", "0")
         config.Config.set("agent", "executor-mode", "forking")
+        config.Config.set("agent", "executor-venv-retention-time", "10")
+        config.Config.set("agent", "executor-retention-time", "10")
         yield config
 
 
@@ -888,6 +890,8 @@ async def server_multi(
         config.Config.set("server", "agent-timeout", "2")
         config.Config.set("agent", "agent-repair-interval", "0")
         config.Config.set("agent", "executor-mode", "forking")
+        config.Config.set("agent", "executor-venv-retention-time", "10")
+        config.Config.set("agent", "executor-retention-time", "10")
 
         ibl = InmantaBootloader(configure_logging=True)
 
