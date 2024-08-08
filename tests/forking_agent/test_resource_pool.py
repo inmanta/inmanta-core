40a68bbbb377678632849ce2f1172f3e0aaec605
Wouter De Borger <wouter.deborger@inmanta.com>
2024-08-08 18:06:35 +0200

    added weird cache

---
+++ tests/forking_agent/test_resource_pool.py
@@ -0,0 +1,159 @@
+"""
+    Copyright 2024 Inmanta
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Contact: code@inmanta.com
+"""
+import itertools
+
+from inmanta.agent.resourcepool import PoolMember, PoolManager, TPoolID, TPoolMember
+
+
+async def test_resource_pool():
+
+    counter = itertools.count()
+    class SimplePoolMember(PoolMember[str]):
+
+        def __init__(self, my_id:str) -> None:
+            super().__init__(my_id)
+            self.count = next(counter)
+
+        def __repr__(self):
+            return self.id + str(self.count)
+
+        async def close(self) -> None:
+            await super().close()
+            await self.closed()
+
+    class SimplePoolManager(PoolManager[SimplePoolMember, str]):
+        async def create_member(self, executor_id: str) -> SimplePoolMember:
+            return SimplePoolMember(my_id=executor_id)
+
+
+    manager = SimplePoolManager()
+    await manager.start()
+
+    a1 = await manager.get("a")
+    a2 = await manager.get("a")
+    b = await manager.get("b")
+
+    assert a1.count == a2.count
+    assert a1.id != b.id
+
+    await a2.close()
+
+    a3 = await manager.get("a")
+    assert a1.count != a3.count
+
+    await manager.request_close(a1)
+    assert len(manager.closing_childern) == 0
+
+    await manager.request_close(a3)
+    assert len(manager.closing_childern) == 0
+
+async def test_resource_pool_stacking():
+    counter = itertools.count()
+
+    class SimplePoolMember(PoolMember[str]):
+
+        def __init__(self, my_id: str) -> None:
+            super().__init__(my_id)
+            self.count = next(counter)
+
+        def __repr__(self):
+            return self.id + str(self.count)
+
+        async def close(self) -> None:
+            await super().close()
+            await self.closed()
+
+    dcounter = itertools.count()
+    class DoublePoolManager(PoolManager[SimplePoolMember, str], PoolMember[str]):
+        async def create_member(self, executor_id: str) -> SimplePoolMember:
+            return SimplePoolMember(my_id=executor_id)
+
+        def __init__(self, my_id: str) -> None:
+            PoolMember.__init__(self, my_id)
+            PoolManager.__init__(self)
+            self.count = next(dcounter)
+
+        def __repr__(self):
+            return "M" + self.id + str(self.count)
+
+        async def close(self) -> None:
+            await PoolMember.close(self)
+            await PoolManager.stop(self)
+            await self.closed()
+
+        async def child_closed(self, pool_member: SimplePoolMember) -> bool:
+            await super().child_closed(pool_member)
+            if len(self.pool) == 0:
+                await self.close()
+
+
+    class UpperManager(PoolManager[DoublePoolManager, str]):
+
+        async def create_member(self, executor_id: str) -> SimplePoolMember:
+            dpm = DoublePoolManager(my_id=executor_id)
+            await dpm.start()
+            return dpm
+
+    um = UpperManager()
+
+    await um.start()
+
+    a1 = await um.get("a")
+    a1_2 = await um.get("a")
+    b1 = await um.get("b")
+
+    assert a1.count == a1_2.count
+    assert b1.id != a1.id
+
+    a1_a1 = await a1.get("A")
+    a1_a1_2 = await a1.get("A")
+    assert a1_a1.count == a1_a1_2.count
+
+    b1_a1 = await b1.get("A")
+    assert a1_a1.count != b1_a1.count
+
+    class OverManager(PoolManager[SimplePoolMember, str]):
+
+        def __init__(self):
+            super().__init__()
+            self.sub_manager = UpperManager()
+
+        async def start(self) -> None:
+            await super().start()
+            await self.sub_manager.start()
+
+        async def create_member(self, executor_id: str) -> SimplePoolMember:
+            pre = executor_id.split(".")[0]
+            producer = await self.sub_manager.get(pre)
+            return await producer.get(executor_id)
+
+
+    om = OverManager()
+    await om.start()
+    aa = await om.get("a.a")
+    aa2 = await om.get("a.a")
+    assert aa.count == aa2.count
+
+    await aa.close()
+
+    assert len(om.pool) == 0
+    assert len(om.sub_manager.pool) == 0
+
+
+
+
