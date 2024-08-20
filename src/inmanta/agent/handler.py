19378cdb2527945f341d07c2abefad7330d8786d
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2024-08-20 13:39:38 +0200

    Remove warning entirely

--- src/inmanta/agent/handler.py
+++ src/inmanta/agent/handler.py
@@ -440,11 +440,8 @@ class HandlerAPI(ABC, Generic[TResource]):
     def __init__(self, agent: "inmanta.agent.executor.AgentInstance", io: object = None) -> None:
         """
         :param agent: The agent that is executing this handler.
-        :param io: Parameter for backwards compatibility
+        :param io: Parameter for backwards compatibility. It is not used by the handler.
         """
-        if io:
-            warnings.warn("The io parameter is no longer used and is deprecated.", DeprecationWarning, stacklevel=2)
-
         self._agent = agent
         self._client: Optional[protocol.SessionClient] = None
 
