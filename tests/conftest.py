10433cfb573af407c756365540363c2a3c97fa68
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2024-09-30 11:15:32 +0200

    Implement review comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1891,20 +1891,26 @@ def is_caplog_handler(handler: logging.Handler) -> bool:
 @pytest.fixture(scope="function", autouse=True)
 async def dont_remove_caplog_handlers(request, monkeypatch):
     """
-    Caplog captures log messages by attaching handlers to the root logger. This fixture makes sure the
-    inmanta.logging.FullLoggingConfig.apply_config() method doesn't remove any handlers that were added by the caplog fixture.
+    Caplog captures log messages by attaching handlers to the root logger. Applying a logging config with
+    `inmanta_logging.FullLoggingConfig.apply_config()` removes any existing logging configuration.
+    As such, this fixtures puts a wrapper around the `apply_config()` method to make sure that:
+
+     * The pytest handlers are not removed/closed after the execution of the apply_config() method.
+     * The root log level, configured by pytest, is not altered by the call to apply_config().
     """
     original_apply_config = inmanta_logging.FullLoggingConfig.apply_config
 
-    def patched_apply_config(self) -> None:
+    def apply_config_wrapper(self) -> None:
         # Make sure the root log level is not altered.
         root_log_level: int = logging.root.level
-        # Make sure that the caplog root handlers are not removed/closed.
+        # Save the caplog root handlers so that we can restore them after.
         caplog_root_handler: list[logging.Handler] = [h for h in logging.root.handlers if is_caplog_handler(h)]
         for current_handler in caplog_root_handler:
             logging.root.removeHandler(current_handler)
-        # Make sure that the weak references to the caplog handlers in the logging._handlerList are not removed.
-        # This list is used to tear down the handlers in the reverse order with respect to the setup order.
+        # When the `apply_config()` method is called, the `logging._handlerList` is used to find all the handlers
+        # that should be closed. We remove the handlers from that list to prevent the handlers from being closed.
+        #
+        # This `logging._handlerList` is used to tear down the handlers in the reverse order with respect to the setup order.
         # As such, this method should not alter the order. We assume the caplog handlers are entirely independent
         # from any other handler. Like that the order only matters within the set of caplog handlers.
         re_add_to_handler_list: list[weakref.ReferenceType] = [
@@ -1921,7 +1927,7 @@ async def dont_remove_caplog_handlers(request, monkeypatch):
             logging.root.addHandler(current_handler)
         logging.root.setLevel(root_log_level)
 
-    monkeypatch.setattr(inmanta_logging.FullLoggingConfig, "apply_config", patched_apply_config)
+    monkeypatch.setattr(inmanta_logging.FullLoggingConfig, "apply_config", apply_config_wrapper)
 
 
 @pytest.fixture(scope="session")
