80ffc82c8398d2bed1636d3ce19948be400d0b80
Sander Van Balen <git@sandervanbalen.be>
2024-10-04 10:11:35 +0200

    typo

--- src/inmanta/const.py
+++ src/inmanta/const.py
@@ -364,4 +364,4 @@ AGENT_SCHEDULER_ID = "$__scheduler"
 
 # resource attributes for event propagation
 RESOURCE_ATTRIBUTE_SEND_EVENTS = "send_event"
-RESOURCE_ATTRIBUTE_RECEIVE_EVENTS = "send_events"
+RESOURCE_ATTRIBUTE_RECEIVE_EVENTS = "receive_events"
