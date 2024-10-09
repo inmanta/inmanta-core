23cf5488b0aeedfe2bb6c64de73e7332441be681
Hugo Lloreda <hugo.lloreda.sanchez@inmanta.com>
2024-10-09 15:21:43 +0200

    optional dependency: logfire

--- src/inmanta/tracing.py
+++ src/inmanta/tracing.py
@@ -19,21 +19,79 @@
 import contextlib
 import logging
 import os
-from typing import Any, Callable, ContextManager, LiteralString, Mapping, ParamSpec, Sequence, TypeVar
+from typing import Any, Callable, ContextManager, Literal, LiteralString, Mapping, ParamSpec, Sequence, TypeVar
 
-import logfire
-import logfire._internal.config
-import logfire.integrations
-import logfire.integrations.pydantic
-from logfire import LevelName, LogfireSpan
-from logfire._internal.main import NoopSpan
-from logfire.propagate import ContextCarrier
 from opentelemetry.instrumentation.asyncpg import AsyncPGInstrumentor
 
 LOGGER = logging.getLogger("inmanta")
 
-# Make sure we don't get warnings when it is off
-logfire._internal.config.GLOBAL_CONFIG.ignore_no_config = True
+# We need this early to make @instrument work
+enabled = os.getenv("LOGFIRE_TOKEN", None) is not None
+if enabled:
+    import logfire
+    import logfire._internal.config
+    import logfire.integrations
+    import logfire.integrations.pydantic
+    from logfire import LevelName, LogfireSpan
+    from logfire._internal.main import NoopSpan
+    from logfire.propagate import ContextCarrier
+
+    # Make sure we don't get warnings when it is off
+    logfire._internal.config.GLOBAL_CONFIG.ignore_no_config = True
+else:
+    # Retaken from Logfire 1.0.1 to make sure tests pass even if logfire is not installed
+    LevelName = Literal["trace", "debug", "info", "notice", "warn", "warning", "error", "fatal"]
+    """Level names for records."""
+
+    class NoopSpan:
+        """Implements the same methods as `LogfireSpan` but does nothing.
+
+        Used in place of `LogfireSpan` and `FastLogfireSpan` when an exception occurs during span creation.
+        This way code like:
+
+            with logfire.span(...) as span:
+                span.set_attribute(...)
+
+        doesn't raise an error even if `logfire.span` fails internally.
+        If `logfire.span` just returned `None` then the `with` block and the `span.set_attribute` call would raise an error.
+        """
+
+        def __init__(self, *_args: Any, **__kwargs: Any) -> None:
+            pass
+
+        def __getattr__(self, _name: str) -> Any:
+            # Handle methods of LogfireSpan which return nothing
+            return lambda *_args, **__kwargs: None  # type: ignore
+
+        def __enter__(self) -> "NoopSpan":
+            return self
+
+        def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: Any) -> None:
+            pass
+
+        # Implement methods/properties that return something to get the type right.
+        @property
+        def message_template(self) -> str:  # pragma: no cover
+            return ""
+
+        @property
+        def tags(self) -> Sequence[str]:  # pragma: no cover
+            return []
+
+        @property
+        def message(self) -> str:  # pragma: no cover
+            return ""
+
+        # This is required to make `span.message = ` not raise an error.
+        @message.setter
+        def message(self, message: str):
+            pass
+
+        def is_recording(self) -> bool:
+            return False
+
+    LogfireSpan = NoopSpan
+    ContextCarrier = Mapping[str, Any]
 
 
 def configure_logfire(service: str) -> None:
@@ -50,7 +108,7 @@ def configure_logfire(service: str) -> None:
     - OTEL_DETAILED_REPORTING: When set detailed parameters are logged such as asyncpg parameters and pydantic objects
     """
 
-    if os.getenv("LOGFIRE_TOKEN", None):
+    if enabled:
         LOGGER.info("Setting up telemetry")
         enable()
 
@@ -66,10 +124,6 @@ def configure_logfire(service: str) -> None:
         )
     else:
         LOGGER.info("Not setting up telemetry")
-
-
-# We need this early to make @instrument work
-enabled = os.getenv("LOGFIRE_TOKEN", None) is not None
 
 
 no_span = NoopSpan()
--- tests/conftest.py
+++ tests/conftest.py
@@ -114,7 +114,6 @@ import inmanta.compiler as compiler
 import inmanta.compiler.config
 import inmanta.main
 import inmanta.user_setup
-import logfire
 from inmanta import config, const, data, env, loader, protocol, resources
 from inmanta.agent import config as agent_cfg
 from inmanta.agent import handler
@@ -1983,13 +1982,3 @@ def disable_version_and_agent_cleanup_job():
     orchestrationservice.PERFORM_CLEANUP = False
     yield
     orchestrationservice.PERFORM_CLEANUP = old_perform_cleanup
-
-
-@pytest.fixture(scope="session", autouse=not PYTEST_PLUGIN_MODE)
-def configure_logfire():
-    """Configure logfire to ensure all the instrumentation works correctly and does not provide warnings. This does not
-    setup tracing for tests"""
-    logfire.configure(
-        send_to_logfire=False,
-        console=False,
-    )
