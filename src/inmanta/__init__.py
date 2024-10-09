bbdf9bed9d7e04519279be5db588387db51db3b3
hugo <hugo.thiolliere@inmanta.com>
2024-10-09 15:00:30 +0200

    bump compiler version to 2025.1

--- src/inmanta/__init__.py
+++ src/inmanta/__init__.py
@@ -16,7 +16,7 @@
     Contact: code@inmanta.com
 """
 
-COMPILER_VERSION = "2025.0"
+COMPILER_VERSION = "2025.1"
 # This version is managed by bumpversion. Should you ever update it manually, make sure to consistently update it everywhere
 # (See the bumpversion.cfg file for relevant locations).
 __version__ = "14.0.0"
