c6ad4fddd0556d1f24ce8799729fbd56970da347
hugo <hugo.thiolliere@inmanta.com>
2024-10-09 15:45:41 +0200

    Bump COMPILER_VERSION to 2024.3 (PR #8194)
    # Description
    
    Iso7 part for https://github.com/inmanta/inmanta-core/pull/8188
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [ ] Attached issue to pull request
    - [ ] Changelog entry
    - [ ] Type annotations are present
    - [ ] Code is clear and sufficiently documented
    - [ ] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

--- src/inmanta/__init__.py
+++ src/inmanta/__init__.py
@@ -16,7 +16,7 @@
     Contact: code@inmanta.com
 """
 
-COMPILER_VERSION = "2024.2"
+COMPILER_VERSION = "2024.3"
 # This version is managed by bumpversion. Should you ever update it manually, make sure to consistently update it everywhere
 # (See the bumpversion.cfg file for relevant locations).
 __version__ = "11.4.0"
