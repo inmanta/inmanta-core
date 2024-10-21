3b91d053c9eed2b8190753440b32cbf02b89c957
wouter <wouter.deborger@inmanta.com>
2024-10-21 11:26:05 +0200

    Move to DB

--- src/inmanta/deploy/persistence.py
+++ src/inmanta/deploy/persistence.py
@@ -1,26 +1,37 @@
 """
     Copyright 2024 Inmanta
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
+
         http://www.apache.org/licenses/LICENSE-2.0
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
+
     Contact: code@inmanta.com
 """
 
 import abc
+import datetime
 import logging
+from typing import Any, Sequence
 from uuid import UUID
 
-from inmanta import const
+from asyncpg import UniqueViolationError
+
+from inmanta import const, data
 from inmanta.agent.executor import DeployResult
+from inmanta.const import TERMINAL_STATES, TRANSIENT_STATES, VALID_STATES_ON_STATE_UPDATE, Change, ResourceState
 from inmanta.data.model import AttributeStateChange, ResourceIdStr, ResourceVersionIdStr
 from inmanta.protocol import Client
+from inmanta.protocol.exceptions import BadRequest, Conflict, NotFound
 from inmanta.resources import Id
+from inmanta.server.services.resourceservice import ResourceActionLogLine
 
 LOGGER = logging.getLogger(__name__)
 
@@ -75,3 +86,207 @@ class ToServerUpdateManager(StateUpdateManager):
         )
         if response.code != 200:
             LOGGER.error("Resource status update failed %s for %s ", response.result, result.rvid)
+
+
+class ToDbUpdateManager(StateUpdateManager):
+
+    def __init__(self, environment: UUID) -> None:
+        self.environment = environment
+        # FIXME: We may want to move this to be 100% scheduler side at some point
+        self._resource_action_logger = logging.getLogger(const.NAME_RESOURCE_ACTION_LOGGER)
+
+    async def send_in_progress(
+        self, action_id: UUID, resource_id: ResourceVersionIdStr
+    ) -> dict[ResourceIdStr, const.ResourceState]:
+        # Cleanup for typign
+        resource_id_str = resource_id
+        resource_id = Id.parse_id(resource_id_str)
+
+        async with data.Resource.get_connection() as connection:
+            async with connection.transaction():
+                # FIXME: do we needs this check?
+                resource = await data.Resource.get_one(
+                    connection=connection,
+                    environment=self.environment,
+                    resource_id=resource_id.resource_str(),
+                    model=resource_id.version,
+                    lock=data.RowLockMode.FOR_UPDATE,
+                )
+                assert resource is not None, f"Resource {resource_id} does not exists in the database, this should not happen"
+
+                resource_action = data.ResourceAction(
+                    environment=self.environment,
+                    version=resource_id.version,
+                    resource_version_ids=[resource_id_str],
+                    action_id=action_id,
+                    action=const.ResourceAction.deploy,
+                    started=datetime.datetime.now().astimezone(),
+                    messages=[
+                        data.LogLine.log(
+                            logging.INFO,
+                            "Resource deploy started on agent %(agent)s, setting status to deploying",
+                            agent=resource_id.agent_name,
+                        )
+                    ],
+                    status=const.ResourceState.deploying,
+                )
+                try:
+                    await resource_action.insert(connection=connection)
+                except UniqueViolationError:
+                    # TODO: why do we have this, which exception do we want
+                    raise Conflict(message=f"A resource action with id {action_id} already exists.")
+
+                # FIXME: we may want to have this in the RPS table instead of Resource table, at some point
+                await resource.update_fields(connection=connection, status=const.ResourceState.deploying)
+
+            # FIXME: shortcut to use scheduler state
+            result = await data.Resource.get_last_non_deploying_state_for_dependencies(
+                environment=self.environment, resource_version_id=resource_id, connection=connection
+            )
+
+            return {Id.parse_id(key).resource_str(): const.ResourceState[value] for key, value in result.items()}
+
+    async def send_deploy_done(self, result: DeployResult) -> None:
+        def error_and_log(message: str, **context: Any) -> None:
+            """
+            :param message: message to return both to logger and to remote caller
+            :param context: additional context to attach to log
+            """
+            ctx = ",".join([f"{k}: {v}" for k, v in context.items()])
+            LOGGER.error("%s %s", message, ctx)
+            raise BadRequest(message)
+
+        resource_id_str = result.rvid
+        resource_id = Id.parse_id(resource_id_str)
+
+        action_id = result.action_id
+
+        status = result.status
+        messages = result.messages
+        change = result.change
+
+        finished = datetime.datetime.now().astimezone()
+
+        changes_with_rvid = {
+            resource_id_str: {attr_name: attr_change.model_dump()} for attr_name, attr_change in result.changes.items()
+        }
+
+        if status not in VALID_STATES_ON_STATE_UPDATE:
+            error_and_log(
+                f"Status {status} is not a valid status at the end of a deployment.",
+                resource_ids=[resource_id_str],
+                action=const.ResourceAction.deploy,
+                action_id=action_id,
+            )
+        if status in TRANSIENT_STATES:
+            error_and_log(
+                "No transient state can be used to mark a deployment as done.",
+                status=status,
+                resource_ids=[resource_id_str],
+                action=const.ResourceAction.deploy,
+                action_id=action_id,
+            )
+
+        async with data.Resource.get_connection() as connection:
+            async with connection.transaction():
+                resource = await data.Resource.get_one(
+                    connection=connection,
+                    environment=self.environment,
+                    resource_id=resource_id.resource_str(),
+                    model=resource_id.version,
+                    # acquire lock on Resource before read and before lock on ResourceAction to prevent conflicts with
+                    # cascading deletes
+                    lock=data.RowLockMode.FOR_UPDATE,
+                )
+                if resource is None:
+                    raise NotFound("The resource with the given id does not exist in the given environment.")
+
+                # no escape from terminal
+                if resource.status != status and resource.status in TERMINAL_STATES:
+                    LOGGER.error("Attempting to set undeployable resource to deployable state")
+                    raise AssertionError("Attempting to set undeployable resource to deployable state")
+
+                resource_action = await data.ResourceAction.get(action_id=action_id, connection=connection)
+                if resource_action is None:
+                    raise NotFound(
+                        f"No resource action exists for action_id {action_id}. Ensure "
+                        f"`/resource/<resource_id>/deploy/start` is called first. "
+                    )
+                if resource_action.finished is not None:
+                    raise Conflict(
+                        f"Resource action with id {resource_id_str} was already marked as done at {resource_action.finished}."
+                    )
+
+                for log in messages:
+                    # All other data is stored in the database. The msg was already formatted at the client side.
+                    self.log_resource_action(
+                        self.environment,
+                        [resource_id_str],
+                        log.log_level.to_int,
+                        log._data["timestamp"],
+                        log.msg,
+                    )
+
+                await resource_action.set_and_save(
+                    messages=[
+                        {
+                            **log.to_dict(),
+                            "timestamp": log._data["timestamp"].astimezone().isoformat(timespec="microseconds"),
+                        }
+                        for log in messages
+                    ],
+                    changes=changes_with_rvid,
+                    status=status,
+                    change=change,
+                    finished=finished,
+                    connection=connection,
+                )
+
+                extra_fields = {}
+                if status == ResourceState.deployed:
+                    extra_fields["last_success"] = resource_action.started
+
+                # keep track IF we need to propagate if we are stale
+                # but only do it at the end of the transaction
+                if change != Change.nochange:
+                    # We are producing an event
+                    extra_fields["last_produced_events"] = finished
+
+                await resource.update_fields(
+                    status=status,
+                    connection=connection,
+                )
+                await resource.update_persistent_state(
+                    last_deploy=finished,
+                    last_deployed_version=resource_id.version,
+                    last_deployed_attribute_hash=resource.attribute_hash,
+                    last_non_deploying_status=const.NonDeployingResourceState(status),
+                    **extra_fields,
+                    connection=connection,
+                )
+
+                if "purged" in resource.attributes and resource.attributes["purged"] and status == const.ResourceState.deployed:
+                    await data.Parameter.delete_all(
+                        environment=self.environment, resource_id=resource.resource_id, connection=connection
+                    )
+
+    def log_resource_action(
+        self, env: UUID, resource_ids: Sequence[str], log_level: int, ts: datetime.datetime, message: str
+    ) -> None:
+        """Write the given log to the correct resource action logger"""
+        logger = self.get_resource_action_logger(env)
+        if len(resource_ids) == 0:
+            message = "no resources: " + message
+        elif len(resource_ids) > 1:
+            message = "multiple resources: " + message
+        else:
+            message = resource_ids[0] + ": " + message
+        log_record = ResourceActionLogLine(logger.name, log_level, message, ts)
+        logger.handle(log_record)
+
+    def get_resource_action_logger(self, environment: UUID) -> logging.Logger:
+        """Get the resource action logger for the given environment.
+        :param environment: The environment to get a logger for
+        :return: The logger for the given environment.
+        """
+        return self._resource_action_logger.getChild(str(environment))
--- src/inmanta/deploy/scheduler.py
+++ src/inmanta/deploy/scheduler.py
@@ -32,7 +32,7 @@ from inmanta.agent.executor import DeployResult
 from inmanta.data import ConfigurationModel
 from inmanta.data.model import ResourceIdStr, ResourceType, ResourceVersionIdStr
 from inmanta.deploy import work
-from inmanta.deploy.persistence import StateUpdateManager, ToServerUpdateManager
+from inmanta.deploy.persistence import StateUpdateManager, ToDbUpdateManager, ToServerUpdateManager
 from inmanta.deploy.state import DeploymentResult, ModelState, ResourceDetails, ResourceState, ResourceStatus
 from inmanta.deploy.tasks import Deploy, DryRun, RefreshFact, Task
 from inmanta.deploy.work import PrioritizedTask, TaskPriority
@@ -147,7 +147,7 @@ class ResourceScheduler(TaskManager):
         self.client = client
         self.code_manager = CodeManager(client)
         self.executor_manager = executor_manager
-        self._state_update_delegate = ToServerUpdateManager(client, environment)
+        self._state_update_delegate = ToDbUpdateManager(environment)
 
     def reset(self) -> None:
         """
