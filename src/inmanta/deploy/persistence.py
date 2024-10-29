21c1791a1090415f4c1448ddc6b7ff4f245e475b
jptrindade <joao.pedro.trindade@inmanta.com>
2024-10-29 07:43:43 +0000

    remove db part of the dryrun update

--- src/inmanta/deploy/persistence.py
+++ src/inmanta/deploy/persistence.py
@@ -32,7 +32,6 @@ from inmanta.protocol import Client
 from inmanta.protocol.exceptions import BadRequest, Conflict, NotFound
 from inmanta.resources import Id
 from inmanta.server.services import resourceservice
-from inmanta.util import parse_timestamp
 
 LOGGER = logging.getLogger(__name__)
 
@@ -121,6 +120,20 @@ class ToDbUpdateManager(StateUpdateManager):
         # FIXME: We may want to move the writing of the log to the scheduler side as well,
         #  when all uses of this logger are moved
         self._resource_action_logger = logging.getLogger(const.NAME_RESOURCE_ACTION_LOGGER)
+
+    def get_resource_action_logger(self, environment: UUID) -> logging.Logger:
+        """Get the resource action logger for the given environment.
+        :param environment: The environment to get a logger for
+        :return: The logger for the given environment.
+        """
+        return self._resource_action_logger.getChild(str(environment))
+
+    def log_resource_action(self, env: UUID, resource_id: str, log_level: int, ts: datetime.datetime, message: str) -> None:
+        """Write the given log to the correct resource action logger"""
+        logger = self.get_resource_action_logger(env)
+        message = resource_id + ": " + message
+        log_record = resourceservice.ResourceActionLogLine(logger.name, log_level, message, ts)
+        logger.handle(log_record)
 
     async def send_in_progress(
         self, action_id: UUID, resource_id: ResourceVersionIdStr
@@ -297,78 +310,5 @@ class ToDbUpdateManager(StateUpdateManager):
                         environment=self.environment, resource_id=resource.resource_id, connection=connection
                     )
 
-    def log_resource_action(self, env: UUID, resource_id: str, log_level: int, ts: datetime.datetime, message: str) -> None:
-        """Write the given log to the correct resource action logger"""
-        logger = self.get_resource_action_logger(env)
-        message = resource_id + ": " + message
-        log_record = resourceservice.ResourceActionLogLine(logger.name, log_level, message, ts)
-        logger.handle(log_record)
-
-    def get_resource_action_logger(self, environment: UUID) -> logging.Logger:
-        """Get the resource action logger for the given environment.
-        :param environment: The environment to get a logger for
-        :return: The logger for the given environment.
-        """
-        return self._resource_action_logger.getChild(str(environment))
-
     async def dryrun_update(self, env: UUID, dryrun_result: DryrunResult) -> None:
-        # async with self.dryrun_lock: Should this class have this lock or one of the child classes?
-        payload = {
-            "changes": dryrun_result.changes,
-            "id_fields": Id.parse_id(dryrun_result.rvid).to_dict(),
-            "id": dryrun_result.rvid,
-        }
-        await data.DryRun.update_resource(dryrun_result.dryrun_id, dryrun_result.rvid, payload)
-
-        async with data.Resource.get_connection() as inner_connection:
-            async with inner_connection.transaction():
-                # validate resources
-                resource_id_parsed = Id.parse_id(dryrun_result.rvid)
-                version = resource_id_parsed.version
-                resource = await data.Resource.get_one(
-                    environment=env,
-                    resource_id=resource_id_parsed.resource_str(),
-                    model=version,
-                    # acquire lock on Resource before read and before lock on ResourceAction to prevent conflicts with
-                    # cascading deletes
-                    lock=data.RowLockMode.FOR_NO_KEY_UPDATE,
-                    connection=inner_connection,
-                )
-                assert (
-                    resource is not None
-                ), f"Resource {resource_id_parsed} does not exists in the database, this should not happen"
-
-                # get instance
-                resource_action = await data.ResourceAction.get(action_id=dryrun_result.dryrun_id, connection=inner_connection)
-                if resource_action is None:
-                    resource_action = data.ResourceAction(
-                        environment=env,
-                        version=version,
-                        resource_version_ids=dryrun_result.rvid,
-                        action_id=dryrun_result.dryrun_id,
-                        action=const.ResourceAction.dryrun,
-                        started=dryrun_result.started,
-                        changes=dryrun_result.changes,
-                        status=const.ResourceState.dry,
-                        finished=dryrun_result.finished,
-                        connection=inner_connection,
-                        messages=[
-                            {
-                                **msg,
-                                "timestamp": parse_timestamp(str(msg._data.get("timestamp"))).isoformat(
-                                    timespec="microseconds"
-                                ),
-                            }
-                            for msg in dryrun_result.messages
-                        ],
-                    )
-                    await resource_action.insert(connection=inner_connection)
-                for msg in dryrun_result.messages:
-                    # All other data is stored in the database. The msg was already formatted at the client side.
-                    self.log_resource_action(
-                        env,
-                        dryrun_result.rvid,
-                        const.LogLevel(msg.log_level).to_int,
-                        parse_timestamp(str(msg._data.get("timestamp"))),
-                        msg.msg,
-                    )
+        pass
