7b1b591ba5201e8932abf68246c1d8761058e213
wouter <wouter.deborger@inmanta.com>
2024-11-05 16:46:59 +0100

    Final cleanup

--- src/inmanta/main.py
+++ src/inmanta/main.py
@@ -31,7 +31,7 @@ import texttable
 
 from inmanta import protocol, util
 from inmanta.config import Config, cmdline_rest_transport
-from inmanta.const import AgentAction, AgentTriggerMethod, DesiredStateVersionStatus, ResourceAction
+from inmanta.const import AgentAction, DesiredStateVersionStatus, ResourceAction
 from inmanta.data.model import ResourceVersionIdStr
 from inmanta.resources import Id
 from inmanta.types import JsonType
