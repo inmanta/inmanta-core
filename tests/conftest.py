1a71d99fcf909968b91e8c92edea3a2a807e73dc
wouter <wouter.deborger@inmanta.com>
2024-11-13 14:17:08 +0100

    Remove methods for test fixture compatibility in the DB (Issue #5399, PR #8345)
    # Description
    
    closes #5399
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [ ] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,6 +40,7 @@ from inmanta.agent.handler import (
 from inmanta.agent.write_barier_executor import WriteBarierExecutorManager
 from inmanta.config import log_dir
 from inmanta.data.model import ResourceIdStr
+from inmanta.db.util import PGRestore
 from inmanta.logging import InmantaLoggerConfig
 from inmanta.protocol import auth
 from inmanta.resources import IgnoreResourceException, PurgeableResource, Resource, resource
@@ -160,12 +161,6 @@ if PYTEST_PLUGIN_MODE:
     from inmanta_tests import utils  # noqa: F401
 else:
     import utils
-
-# These elements were moved to inmanta.db.util to allow them to be used from other extensions.
-# This import statement is present to ensure backwards compatibility.
-from inmanta.db.util import MODE_READ_COMMAND, MODE_READ_INPUT, AsyncSingleton, PGRestore  # noqa: F401
-from inmanta.db.util import clear_database as do_clean_hard  # noqa: F401
-from inmanta.db.util import postgres_get_custom_types as postgress_get_custom_types  # noqa: F401
 
 logger = logging.getLogger(__name__)
 
