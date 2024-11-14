18a0932a554deddea5de94e26e851c0d9f0aeb9b
wouter <wouter.deborger@inmanta.com>
2024-11-14 09:06:21 +0100

    Add lost file

--- tests/server/test_compilerservice.py
+++ tests/server/test_compilerservice.py
@@ -441,7 +441,7 @@ async def test_compile_runner(environment_factory: EnvironmentFactory, server, c
     compile, stages = await _compile_and_assert(env=env, export=True, meta={"type": "Test"})
     assert stages["Init"]["returncode"] == 0
     assert stages["Cloning repository"]["returncode"] == 0
-    assert stages["Creating venv"]["returncode"] == 0
+    assert stages["Venv check"]["returncode"] == 0
     assert stages["Installing modules"]["returncode"] == 0
     assert stages["Recompiling configuration model"]["returncode"] == 0
     out = stages["Recompiling configuration model"]["outstream"]
@@ -452,7 +452,7 @@ async def test_compile_runner(environment_factory: EnvironmentFactory, server, c
     # no export
     compile, stages = await _compile_and_assert(env=env, export=False)
     assert stages["Init"]["returncode"] == 0
-    assert stages["Creating venv"]["returncode"] == 0
+    assert stages["Venv check"]["returncode"] == 0
     assert stages["Recompiling configuration model"]["returncode"] == 0
     out = stages["Recompiling configuration model"]["outstream"]
     assert f"{marker_print} {no_marker}" in out
@@ -465,7 +465,7 @@ async def test_compile_runner(environment_factory: EnvironmentFactory, server, c
     assert len(compile.request.requested_environment_variables) == 1
     assert stages["Init"]["returncode"] == 0
     assert f"Using extra environment variables during compile TESTMARKER='{marker}'" in stages["Init"]["outstream"]
-    assert stages["Creating venv"]["returncode"] == 0
+    assert stages["Venv check"]["returncode"] == 0
     assert stages["Recompiling configuration model"]["returncode"] == 0
     out = stages["Recompiling configuration model"]["outstream"]
     assert f"{marker_print} {marker}" in out
@@ -476,7 +476,7 @@ async def test_compile_runner(environment_factory: EnvironmentFactory, server, c
     compile, stages = await _compile_and_assert(env=env2, export=False)
     assert stages["Init"]["returncode"] == 0
     assert stages[f"Switching branch from {env.repo_branch} to {env2.repo_branch}"]["returncode"] == 0
-    assert stages["Creating venv"]["returncode"] == 0
+    assert stages["Venv check"]["returncode"] == 0
     assert stages["Installing modules"]["returncode"] == 0
     assert stages["Recompiling configuration model"]["returncode"] == 0
     out = stages["Recompiling configuration model"]["outstream"]
@@ -487,7 +487,7 @@ async def test_compile_runner(environment_factory: EnvironmentFactory, server, c
     # update with no update
     compile, stages = await _compile_and_assert(env=env2, export=False, update=True)
     assert stages["Init"]["returncode"] == 0
-    assert stages["Creating venv"]["returncode"] == 0
+    assert stages["Venv check"]["returncode"] == 0
     assert stages["Pulling updates"]["returncode"] == 0
     assert stages["Uninstall inmanta packages from the compiler venv"]["returncode"] == 0
     assert stages["Updating modules"]["returncode"] == 0
@@ -500,7 +500,7 @@ async def test_compile_runner(environment_factory: EnvironmentFactory, server, c
     environment_factory.write_main(make_main(marker_print3))
     compile, stages = await _compile_and_assert(env=env2, export=False, update=True)
     assert stages["Init"]["returncode"] == 0
-    assert stages["Creating venv"]["returncode"] == 0
+    assert stages["Venv check"]["returncode"] == 0
     assert stages["Pulling updates"]["returncode"] == 0
     assert stages["Uninstall inmanta packages from the compiler venv"]["returncode"] == 0
     assert stages["Updating modules"]["returncode"] == 0
@@ -2025,7 +2025,7 @@ async def test_overlapping_env_vars(mocked_compiler_service, server, client, env
         )
 
 
-class Dudreport:
+class Mockreport:
 
     async def update_streams(self, out: str = "", err: str = "") -> None:
         pass
@@ -2033,39 +2033,41 @@ class Dudreport:
 
 async def test_venv_use_and_reuse(tmp_path, caplog):
     caplog.at_level(logging.DEBUG)
+
+    # Set up mock
     project = tmp_path / "project"
-
     venv = project / ".env"
-
     project.mkdir()
     run = CompileRun(None, str(project))
-    # Move for logging
-    run.stage = Dudreport()
+    run.stage = Mockreport()
 
-    # Create
+    # Create a venv
     await run.ensure_compiler_venv()
     assert venv.exists()
     log_contains(caplog, "inmanta.server.services.compilerservice", logging.INFO, "Creating new venv at")
     caplog.clear()
 
-    # re-use
+    # re-use the venv
     await run.ensure_compiler_venv()
     log_contains(caplog, "inmanta.server.services.compilerservice", logging.INFO, "Found existing venv")
-    caplog.clear()
 
 
-async def test_venv_upgrade_capture(tmp_path, caplog):
+async def test_venv_upgrade_version_match(tmp_path, caplog):
+    """
+    1. Make a venv in the old layout and upgrade it
+    2. Test we can handle re-creation of the venv
+    """
     caplog.at_level(logging.DEBUG)
 
-    # Old setup
-    project = tmp_path / "project2"
+    # Set up mock
+    project = tmp_path / "project"
     project.mkdir()
     venv = project / ".env"
-    # make venv
-    VirtualEnv(str(venv)).init_env()
-
     run = CompileRun(None, str(project))
-    run.stage = Dudreport()
+    run.stage = Mockreport()
+
+    # make venv on old location, correct version
+    VirtualEnv(str(venv)).init_env()
 
     await run.ensure_compiler_venv()
     log_contains(caplog, "inmanta.server.services.compilerservice", logging.INFO, "Moving existing venv from")
@@ -2080,11 +2082,10 @@ async def test_venv_upgrade_capture(tmp_path, caplog):
 
     await run.ensure_compiler_venv()
     log_contains(caplog, "inmanta.server.services.compilerservice", logging.INFO, "Creating new venv at")
-    caplog.clear()
     assert venv.exists()
 
 
-async def test_venv_upgrade_noncapture(tmp_path, caplog):
+async def test_venv_upgrade_version_mismatch(tmp_path, caplog):
     # Make fake venv of wrong version
 
     caplog.at_level(logging.DEBUG)
@@ -2095,12 +2096,14 @@ async def test_venv_upgrade_noncapture(tmp_path, caplog):
     venv = project / ".env"
 
     run = CompileRun(None, str(project))
-    run.stage = Dudreport()
+    run.stage = Mockreport()
 
+    # This venv is a bit broken, but the code sees it wrong version
+    # Because it doesn't have the right version
     venv.mkdir()
     (venv / "bin").mkdir()
     (venv / "bin" / "python").touch()
+
     await run.ensure_compiler_venv()
     log_contains(caplog, "inmanta.server.services.compilerservice", logging.INFO, "Discarding existing venv from")
-    caplog.clear()
     assert (project / ".env").exists()
