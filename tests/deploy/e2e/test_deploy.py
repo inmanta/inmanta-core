3f22eb33d7216659426ffdb4eeac4be36959b3f2
jptrindade <joao.pedro.trindade@inmanta.com>
2024-11-19 16:23:45 +0000

    initial commit

--- tests/deploy/e2e/test_deploy.py
+++ tests/deploy/e2e/test_deploy.py
@@ -101,7 +101,7 @@ async def test_on_disk_layout(server, agent, environment):
         assert sub_dir.exists()
 
     # Check for presence of the new disk layout marker file
-    marker_file_path = state_dir / const.INMANTA_USE_NEW_DISK_LAYOUT_FILENAME
+    marker_file_path = state_dir / const.INMANTA_DISK_LAYOUT_VERSION
     assert pathlib.Path(marker_file_path).exists()
 
 
