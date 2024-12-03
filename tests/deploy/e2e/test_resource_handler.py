5816d2a1d634ff69c1e4c37ed4941e54ede0e041
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2024-12-03 09:43:21 +0100

    Fix formatting

--- tests/deploy/e2e/test_resource_handler.py
+++ tests/deploy/e2e/test_resource_handler.py
@@ -28,7 +28,7 @@ from inmanta.agent.handler import ResourceHandler
 from inmanta.data import model
 from inmanta.protocol import SessionClient, VersionMatch, common
 from inmanta.util import get_compiler_version
-from utils import _deploy_resources, log_contains, make_random_file, wait_until_deployment_finishes, retry_limited
+from utils import _deploy_resources, log_contains, make_random_file, retry_limited, wait_until_deployment_finishes
 
 T = TypeVar("T")
 
