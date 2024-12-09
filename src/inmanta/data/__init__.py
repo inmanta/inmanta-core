bc9f23e3f7ec80ea7965e3b4f88300b644d7eb8d
jptrindade <joao.pedro.trindade@inmanta.com>
2024-12-09 12:16:54 +0000

    adjusted docstring

--- src/inmanta/data/__init__.py
+++ src/inmanta/data/__init__.py
@@ -4672,8 +4672,8 @@ class Resource(BaseDocument):
         connection: Optional[asyncpg.connection.Connection] = None,
     ) -> None:
         """
-        Update resources on the latest version of the model stuck in "deploying" state. The status will be reset to the latest
-        non deploying status.
+        Update resources on the latest released version of the model stuck in "deploying" state.
+        The status will be reset to the latest non deploying status.
 
         :param environment: The environment impacted by this
         :param connection: The connection to use
--- src/inmanta/deploy/scheduler.py
+++ src/inmanta/deploy/scheduler.py
@@ -436,8 +436,8 @@ class ResourceScheduler(TaskManager):
 
     async def reset_resource_state(self) -> None:
         """
-        Update resources on the latest version of the model stuck in "deploying" state. This can occur when the Scheduler is
-        killed in the middle of a deployment.
+        Update resources on the latest released version of the model stuck in "deploying" state.
+        This can occur when the Scheduler is killed in the middle of a deployment.
         """
 
         await data.Resource.reset_resource_state(self.environment)
