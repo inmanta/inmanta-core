28bebff0902914316315a6dc8bafef61baa954c9
hugo <hugo.thiolliere@inmanta.com>
2024-12-09 08:34:30 +0100

    wip

--- src/inmanta/agent/agent_new.py
+++ src/inmanta/agent/agent_new.py
@@ -30,7 +30,6 @@ from inmanta.agent.reporting import collect_report
 from inmanta.const import AGENT_SCHEDULER_ID
 from inmanta.data.model import DataBaseReport, SchedulerStatusReport
 from inmanta.deploy import scheduler
-from inmanta.deploy.work import TaskPriority
 from inmanta.protocol import SessionEndpoint, TypedClient, methods, methods_v2
 from inmanta.server.services.databaseservice import DatabaseMonitor
 from inmanta.types import Apireturn
