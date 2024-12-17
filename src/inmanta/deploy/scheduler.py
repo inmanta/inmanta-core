1402ec58942dc23fd3b4d967a7a2ecf4fdf0cc72
wouter <wouter.deborger@inmanta.com>
2024-12-17 16:07:35 +0100

    removed breakpoint

--- src/inmanta/deploy/scheduler.py
+++ src/inmanta/deploy/scheduler.py
@@ -300,7 +300,6 @@ class ResourceScheduler(TaskManager):
         Initialize the scheduler state and continue the deployment where we were before the server was shutdown.
         Marks scheduler as running and triggers first deploys.
         """
-        breakpoint()
         self._timer_manager.initialize()
         async with self._intent_lock, self._scheduler_lock, data.Scheduler.get_connection() as con, con.transaction():
             # Make sure there is an entry for this scheduler in the scheduler database table
