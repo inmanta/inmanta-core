10363db002722d7db11ec52ce58795b8d4c89243
hugo <hugo.thiolliere@inmanta.com>
2024-12-19 09:41:11 +0100

    fix

--- tests/test_server.py
+++ tests/test_server.py
@@ -147,7 +147,7 @@ async def test_create_too_many_versions(client, server, no_agent, n_versions_to_
 
 
 @pytest.mark.parametrize("has_released_versions", [True, False])
-async def test_purge_versions(server, client, environment, has_released_versions: bool, null_agent) -> None:
+async def test_purge_versions(server, client, environment, has_released_versions: bool, agent_no_state_check) -> None:
     """
     Verify that the `OrchestrationService._purge_versions()` method works correctly and that it doesn't cleanup
     the latest released version.
