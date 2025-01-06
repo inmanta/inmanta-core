ba17039f89db39dc1de0b536c656c4761532dcbc
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-01-06 10:25:12 +0100

    Bump inmanta-dev-dependencies[async,core,pytest] from 2.143.0 to 2.144.0 (PR #8536)
    Bumps inmanta-dev-dependencies[async,core,pytest] from 2.143.0 to 2.144.0.
    
    [![Dependabot compatibility score](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=inmanta-dev-dependencies[async,core,pytest]&package-manager=pip&previous-version=2.143.0&new-version=2.144.0)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)
    
    Dependabot will resolve any conflicts with this PR as long as you don't alter it yourself. You can also trigger a rebase manually by commenting `@dependabot rebase`.
    
    [//]: # (dependabot-automerge-start)
    [//]: # (dependabot-automerge-end)
    
    ---
    
    <details>
    <summary>Dependabot commands and options</summary>
    <br />
    
    You can trigger Dependabot actions by commenting on this PR:
    - `@dependabot rebase` will rebase this PR
    - `@dependabot recreate` will recreate this PR, overwriting any edits that have been made to it
    - `@dependabot merge` will merge this PR after your CI passes on it
    - `@dependabot squash and merge` will squash and merge this PR after your CI passes on it
    - `@dependabot cancel merge` will cancel a previously requested merge and block automerging
    - `@dependabot reopen` will reopen this PR if it is closed
    - `@dependabot close` will close this PR and stop Dependabot recreating it. You can achieve the same result by closing it manually
    - `@dependabot show <dependency name> ignore conditions` will show all of the ignore conditions of the specified dependency
    - `@dependabot ignore this major version` will close this PR and stop Dependabot creating any more for this major version (unless you reopen the PR or upgrade to it yourself)
    - `@dependabot ignore this minor version` will close this PR and stop Dependabot creating any more for this minor version (unless you reopen the PR or upgrade to it yourself)
    - `@dependabot ignore this dependency` will close this PR and stop Dependabot creating any more for this dependency (unless you reopen the PR or upgrade to it yourself)
    
    </details>

--- src/inmanta/warnings.py
+++ src/inmanta/warnings.py
@@ -20,7 +20,7 @@ import logging
 import warnings
 from collections.abc import Mapping
 from enum import Enum
-from typing import Literal, Optional, TextIO, Union
+from typing import Optional, TextIO, Union
 
 
 class InmantaWarning(Warning):
@@ -37,9 +37,9 @@ REGEX_INMANTA_MODULE: str = r"^(inmanta|inmanta\..*|inmanta_.*)$"
 
 
 class WarningBehaviour(Enum):
-    WARN: Literal["default"] = "default"
-    IGNORE: Literal["ignore"] = "ignore"
-    ERROR: Literal["error"] = "error"
+    WARN = "default"
+    IGNORE = "ignore"
+    ERROR = "error"
 
 
 class WarningRule:
