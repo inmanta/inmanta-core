e92ddf880238909fd1516ef72918bc304edb5b83
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-01-08 10:20:28 +0100

    Small enhancement

--- src/inmanta/app.py
+++ src/inmanta/app.py
@@ -930,7 +930,8 @@ def default_log_config_parser(parser: ArgumentParser, parent_parsers: abc.Sequen
     )
     parser.add_argument(
         "output_file",
-        help="The file where the logging config should be saved.",
+        help="The file where the logging config should be saved. For the scheduler component, this file must end with a .tmpl"
+             " suffix, because a logging configuration template will be generated.",
     )
 
 
