77370aae52afbe01cc02e454bd0e5c662d68b4ba
Wouter De Borger <wouter.deborger@inmanta.com>
2025-01-09 09:27:39 +0100

    Update src/inmanta/deploy/timers.py
    Co-authored-by: Hugo-Inmanta <98876549+Hugo-Inmanta@users.noreply.github.com>

--- src/inmanta/deploy/timers.py
+++ src/inmanta/deploy/timers.py
@@ -95,8 +95,7 @@ class ResourceTimer:
 
         Should not be called if the timer is already active.
 
-        :param when: In this many seconds in the future, this method will ensure the underlying
-            resource is scheduled for deploy.
+        :param when: Schedule the next deploy for this resource at this datetime. 
         :param reason: The reason argument that will be given for the deploy request.
         :param priority: The priority argument that will be given for the deploy request.
         """
