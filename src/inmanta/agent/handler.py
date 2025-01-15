98000526b46daaff9f9625fdbc1717f5d36aaf55
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2025-01-15 10:45:18 +0100

    Add typing.overload to handler cache

--- src/inmanta/agent/handler.py
+++ src/inmanta/agent/handler.py
@@ -109,6 +109,36 @@ class InvalidOperation(Exception):
     """
 
 
+@typing.overload
+def cache(
+    func: None = None,
+    ignore: list[str] = [],
+    timeout: Optional[int] = None,
+    for_version: Optional[bool] = None,
+    cache_none: bool = True,
+    cacheNone: Optional[bool] = None,  # noqa: N803
+    call_on_delete: Optional[Callable[[Any], None]] = None,
+    evict_after_creation: float = 0.0,
+    evict_after_last_access: float = 0.0,
+) -> Callable[[T_FUNC], T_FUNC]:
+    ...
+
+
+@typing.overload
+def cache(
+    func: T_FUNC,
+    ignore: list[str] = [],
+    timeout: Optional[int] = None,
+    for_version: Optional[bool] = None,
+    cache_none: bool = True,
+    cacheNone: Optional[bool] = None,  # noqa: N803
+    call_on_delete: Optional[Callable[[Any], None]] = None,
+    evict_after_creation: float = 0.0,
+    evict_after_last_access: float = 0.0,
+) -> T_FUNC:
+    ...
+
+
 @stable_api
 def cache(
     func: Optional[T_FUNC] = None,
