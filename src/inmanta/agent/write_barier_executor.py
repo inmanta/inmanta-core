bef9f8cd0f0f877b5586daa76b6a4f3c555ca3f5
wouter <wouter.deborger@inmanta.com>
2025-01-15 13:45:20 +0100

    reviewer comments

--- src/inmanta/agent/write_barier_executor.py
+++ src/inmanta/agent/write_barier_executor.py
@@ -17,7 +17,6 @@
 """
 
 import concurrent
-import logging
 import typing
 import uuid
 from collections.abc import Mapping
@@ -88,7 +87,6 @@ class WriteBarierExecutorManager(executor.ExecutorManager[WriteBarierExecutor]):
         return [WriteBarierExecutor(e) for e in await self.delegate.stop_for_agent(agent_name)]
 
     async def start(self) -> None:
-        logging.getLogger("test").info("Worker started")
         await self.delegate.start()
 
     async def stop(self) -> None:
--- src/inmanta/server/services/orchestrationservice.py
+++ src/inmanta/server/services/orchestrationservice.py
@@ -1208,7 +1208,7 @@ class OrchestrationService(protocol.ServerSlice):
 
         # filter agents
         allagents = await data.ConfigurationModel.get_agents(env.id, version_id)
-        agents_to_call: Sequence[None | str] = [None]  # None means all
+        agents_to_call: Sequence[None | str] = []  # None means all
 
         if agents is not None:
             # select specific agents
@@ -1233,8 +1233,11 @@ class OrchestrationService(protocol.ServerSlice):
 
         incremental_deploy = agent_trigger_method is const.AgentTriggerMethod.push_incremental_deploy
 
-        for agent in agents_to_call:
-            self.add_background_task(client.trigger(env.id, agent, incremental_deploy))
+        if agents is None:
+            self.add_background_task(client.trigger(env.id, None, incremental_deploy))
+        else:
+            for agent in agents_to_call:
+                self.add_background_task(client.trigger(env.id, agent, incremental_deploy))
 
         return attach_warnings(200, {"agents": sorted(allagents)}, warnings)
 
