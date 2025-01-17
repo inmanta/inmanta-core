52b63d5f4e764a928efcfee20950e9b011d8130a
hugo <hugo.thiolliere@inmanta.com>
2025-01-17 16:16:37 +0100

    wip

--- tests/compiler/test_plugins.py
+++ tests/compiler/test_plugins.py
@@ -427,7 +427,12 @@ def test_native_types(snippetcompiler: "SnippetCompilationTest", caplog) -> None
         snippetcompiler.setup_for_snippet(
             """
 import plugin_native_types
-        """
+a = "b"
+a = plugin_native_types::get_from_dict({"a":"b"}, "a")
+none = null
+none = plugin_native_types::get_from_dict({"a":"b"}, "B")
+a = plugin_native_types::many_arguments(["a","c","b"], 1)
+            """
         )
         compiler.do_compile()
 
