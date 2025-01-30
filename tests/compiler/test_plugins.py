625b1fdbc1d1ec04717c67c9595f7fe68fee52c2
hugo <hugo.thiolliere@inmanta.com>
2025-01-30 10:20:53 +0100

    wip

--- tests/compiler/test_plugins.py
+++ tests/compiler/test_plugins.py
@@ -377,9 +377,9 @@ import keyword_only_arguments
     }
 
     assert plugins["catch_all_arguments::sum_all"].get_signature() == "sum_all(a: int, *aa: int, b: int, **bb: int) -> int"
-    # assert plugins["catch_all_arguments::none_args"].get_signature() == "none_args(a: int?)"
+    assert plugins["catch_all_arguments::none_args"].get_signature() == "none_args(a: int?, b: int)"
     assert plugins["catch_all_arguments::none_args"].get_signature(dsl_types=False) == "none_args(a: int | None, b: 'int')"
-    assert plugins["keyword_only_arguments::sum_all"].get_signature() == ("sum_all(a: int, b: int, *, c: int, d: int) -> int")
+    assert plugins["keyword_only_arguments::sum_all"].get_signature() == "sum_all(a: int, b: int, *, c: int, d: int) -> int"
     assert plugins["keyword_only_arguments::sum_all"].get_signature(dsl_types=False) == (
         "sum_all(a: 'int', b: 'int' = 1, *, c: 'int', d: 'int' = 2) -> int"
     )
