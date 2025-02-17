7055b4b84c8f3eef481903dc893d85e829ef9fed
wouter <wouter.deborger@inmanta.com>
2025-02-17 11:02:02 +0100

    merge master

--- src/inmanta/plugins.py
+++ src/inmanta/plugins.py
@@ -259,6 +259,53 @@ class UnConvertibleEntity(inmanta_type.Type):
         return self.base_entity.get_location()
 
 
+class UnConvertibleEntity(inmanta_type.Type):
+    """
+    Entity that does not convert to a dataclass.
+    """
+
+    # TODO: cache
+    def __init__(self, base_entity: "Entity") -> None:
+        super().__init__()
+        self.base_entity = base_entity
+
+    def validate(self, value: Optional[object]) -> bool:
+        return self.base_entity.validate(value)
+
+    def type_string(self) -> Optional[str]:
+        return self.base_entity.type_string()
+
+    def type_string_internal(self) -> str:
+        return self.base_entity.type_string_internal()
+
+    def normalize(self) -> None:
+        pass
+
+    def is_attribute_type(self) -> bool:
+        return False
+
+    def get_base_type(self) -> "inmanta_type.Type":
+        return self.base_entity.get_base_type()
+
+    def with_base_type(self, base_type: "inmanta_type.Type") -> "inmanta_type.Type":
+        return self.base_entity.with_base_type(base_type)
+
+    def corresponds_to(self, type: "inmanta_type.Type") -> bool:
+        raise NotImplementedError()
+
+    def as_python_type_string(self) -> "str | None":
+        return self.base_entity.as_python_type_string()
+
+    def has_custom_to_python(self) -> bool:
+        return False
+
+    def to_python(self, instance: object) -> "object":
+        return self.base_entity.to_python(instance)
+
+    def get_location(self) -> Optional[Location]:
+        return self.base_entity.get_location()
+
+
 # Define some types which are used in the context of plugins.
 PLUGIN_TYPES = {
     "any": inmanta_type.Any(),  # Any value will pass validation
@@ -399,6 +446,12 @@ def to_dsl_type(python_type: type[object], location: Range, resolver: Namespace)
         else:
             bases = [to_dsl_type(arg, location, resolver) for arg in typing.get_args(python_type)]
             return inmanta_type.create_union(bases)
+
+    if dataclasses.is_dataclass(python_type):
+        entity = get_inmanta_type_for_dataclass(python_type)
+        if entity:
+            return entity
+        raise TypingException(None, f"invalid type {python_type}, this dataclass has no associated inmanta entity")
 
     if dataclasses.is_dataclass(python_type):
         entity = get_inmanta_type_for_dataclass(python_type)
