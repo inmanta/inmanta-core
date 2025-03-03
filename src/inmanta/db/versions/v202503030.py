f8e6ed715c7d0cfe6569109e8d5a55f9823a2cdf
hugo <hugo.thiolliere@inmanta.com>
2025-03-03 11:12:54 +0100

    add db schema

---
+++ src/inmanta/db/versions/v202503030.py
@@ -0,0 +1,71 @@
+"""
+Copyright 2025 Inmanta
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contact: code@inmanta.com
+"""
+
+from asyncpg import Connection
+
+
+async def update(connection: Connection) -> None:
+    """
+    * Create the module_requirements table, that keeps track of the python package requirements
+      per inmanta module (name, version).
+    * Create the files_in_module table, that keeps track of which files belong to which inmanta
+      module.
+    * Create the modules_for_agent table, that keeps track of which inmanta modules are required
+      by which agent.
+    """
+    schema = """
+        CREATE TABLE public.module_requirements (
+            module_name varchar,
+            module_version varchar,
+            environment uuid NOT NULL REFERENCES environment(id) ON DELETE CASCADE,
+            requirements character varying[] DEFAULT ARRAY[]::character varying[],
+            PRIMARY KEY(module_name, module_version, environment)
+        );
+
+        -- Create the files_in_module table.
+        CREATE TABLE public.files_in_module (
+            module_name varchar,
+            module_version varchar,
+            environment uuid NOT NULL,
+            file_content_hash varchar NOT NULL REFERENCES file(content_hash) ON DELETE CASCADE,
+            file_path varchar NOT NULL,
+            FOREIGN KEY (module_name, module_version, environment)
+            REFERENCES public.module_requirements(module_name, module_version, environment) ON DELETE CASCADE
+        );
+
+        CREATE INDEX files_in_module_module_name_module_version_environment_index ON public.files_in_module (module_name, module_version, environment);
+
+        -- Create the modules_for_agent table.
+        CREATE TABLE public.modules_for_agent (
+            cm_version integer,
+            agent_name varchar,
+            module_name varchar,
+            module_version varchar,
+            environment uuid NOT NULL,
+            FOREIGN KEY (cm_version, environment)
+            REFERENCES public.configurationmodel(version, environment) ON DELETE CASCADE,
+            FOREIGN KEY (agent_name, environment)
+            REFERENCES public.agent(name, environment) ON DELETE CASCADE,
+            FOREIGN KEY (module_name, module_version, environment)
+            REFERENCES public.module_requirements(module_name, module_version, environment) ON DELETE CASCADE
+        );
+
+        CREATE INDEX modules_for_agent_agent_name_cm_version_environment_index ON public.modules_for_agent (agent_name, cm_version, environment);
+
+    """
+    await connection.execute(schema)
---
+++ tests/db/migration_tests/test_v202501140_to_v202503030.py
@@ -0,0 +1,45 @@
+"""
+Copyright 2025 Inmanta
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contact: code@inmanta.com
+"""
+
+import os
+import re
+from collections import abc
+
+import asyncpg
+import pytest
+
+file_name_regex = re.compile("test_v([0-9]{9})_to_v[0-9]{9}")
+part = file_name_regex.match(__name__)[1]
+
+
+@pytest.mark.db_restore_dump(os.path.join(os.path.dirname(__file__), f"dumps/v{part}.sql"))
+async def test_add_tables_for_agent_code_transport_rework(
+    postgresql_client: asyncpg.Connection,
+    migrate_db_from: abc.Callable[[], abc.Awaitable[None]],
+    get_tables_in_db: abc.Callable[[], abc.Awaitable[list[str]]]
+) -> None:
+    # This migration script only creates tables and adds indexes
+
+    assert "module_requirements" not in await get_tables_in_db()
+    assert "files_in_module" not in await get_tables_in_db()
+    assert "modules_for_agent" not in await get_tables_in_db()
+    await migrate_db_from()
+    assert "module_requirements" in await get_tables_in_db()
+    assert "files_in_module" in await get_tables_in_db()
+    assert "modules_for_agent" in await get_tables_in_db()
+
