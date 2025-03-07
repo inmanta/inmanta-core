2dca509d5b361ec86877f1bf5ec4cfbe28c6fc9d
hugo <hugo.thiolliere@inmanta.com>
2025-03-07 15:39:07 +0100

    wip

--- src/inmanta/db/versions/v202503030.py
+++ src/inmanta/db/versions/v202503030.py
@@ -29,12 +29,12 @@ async def update(connection: Connection) -> None:
       by which agent.
     """
     schema = """
-        CREATE TABLE public.module_requirements (
-            module_name varchar,
-            module_version varchar,
+        CREATE TABLE public.module (
+            name varchar NOT NULL,
+            version varchar NOT NULL,
             environment uuid NOT NULL REFERENCES environment(id) ON DELETE CASCADE,
-            requirements character varying[] DEFAULT ARRAY[]::character varying[],
-            PRIMARY KEY(module_name, module_version, environment)
+            requirements character varying[] DEFAULT ARRAY[]::character varying[] NOT NULL,
+            PRIMARY KEY(environment, name, version)
         );
 
         -- Create the files_in_module table.
@@ -44,12 +44,13 @@ async def update(connection: Connection) -> None:
             environment uuid NOT NULL,
             file_content_hash varchar NOT NULL REFERENCES file(content_hash) ON DELETE CASCADE,
             file_path varchar NOT NULL,
-            FOREIGN KEY (module_name, module_version, environment)
-            REFERENCES public.module_requirements(module_name, module_version, environment) ON DELETE CASCADE
+            PRIMARY KEY(environment, module_name, module_version, file_path),
+            FOREIGN KEY (environment, module_name, module_version)
+                REFERENCES public.module(environment, name, version) ON DELETE CASCADE
         );
 
-        CREATE INDEX files_in_module_module_name_module_version_environment_index
-        ON public.files_in_module (module_name, module_version, environment);
+        CREATE INDEX files_in_module_environment_module_name_module_version_index
+        ON public.files_in_module (environment, module_name, module_version);
 
         -- Create the modules_for_agent table.
         CREATE TABLE public.modules_for_agent (
@@ -58,16 +59,21 @@ async def update(connection: Connection) -> None:
             module_name varchar,
             module_version varchar,
             environment uuid NOT NULL,
-            FOREIGN KEY (cm_version, environment)
-            REFERENCES public.configurationmodel(version, environment) ON DELETE CASCADE,
-            FOREIGN KEY (agent_name, environment)
-            REFERENCES public.agent(name, environment) ON DELETE CASCADE,
-            FOREIGN KEY (module_name, module_version, environment)
-            REFERENCES public.module_requirements(module_name, module_version, environment) ON DELETE CASCADE
+            PRIMARY KEY(environment, cm_version, agent_name, module_name, module_version),
+            FOREIGN KEY (environment, cm_version)
+                REFERENCES public.configurationmodel(environment, version) ON DELETE CASCADE,
+            FOREIGN KEY (environment, agent_name)
+                REFERENCES public.agent(environment, name) ON DELETE CASCADE,
+            FOREIGN KEY (environment, module_name, module_version)
+                REFERENCES public.module(environment, name, version) ON DELETE CASCADE
         );
 
-        CREATE INDEX modules_for_agent_agent_name_cm_version_environment_index
-        ON public.modules_for_agent (agent_name, cm_version, environment);
+        CREATE INDEX modules_for_agent_environment_cm_version_index
+        ON public.modules_for_agent (environment, cm_version);
+        CREATE INDEX modules_for_agent_environment_agent_name_index
+        ON public.modules_for_agent (environment, agent_name);
+        CREATE INDEX modules_for_agent_environment_module_name_module_version_index
+        ON public.modules_for_agent (environment, module_name, module_version);
 
     """
     await connection.execute(schema)
