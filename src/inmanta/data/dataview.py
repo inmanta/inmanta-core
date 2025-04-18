eb9f7e6f2dcfd31093e4fa7bbda2ac315511deaa
hugo <hugo.thiolliere@inmanta.com>
2025-04-18 16:40:14 +0200

    Revert "Use asyncpg instead of SQLAlchemy for database connection pooling. (Issue #8865, PR #8953)"
    This reverts commit 4c479c32b4be0df39010d51b6d44d1781250cae1.

--- src/inmanta/data/dataview.py
+++ src/inmanta/data/dataview.py
@@ -17,7 +17,6 @@ Contact: code@inmanta.com
 """
 
 import abc
-import json
 import urllib.parse
 from abc import ABC
 from collections.abc import Sequence
@@ -623,7 +622,7 @@ class ResourceView(DataView[ResourceStatusOrder, model.LatestReleasedResource]):
                 resource_version_id=resource["resource_id"] + ",v=" + str(resource["model"]),
                 id_details=data.Resource.get_details_from_resource_id(resource["resource_id"]),
                 status=resource["status"],
-                requires=json.loads(resource["attributes"]).get("requires", []),
+                requires=resource["attributes"].get("requires", []),
             )
             for resource in records
             if resource["attributes"]  # filter out bad joins
@@ -683,7 +682,7 @@ class ResourcesInVersionView(DataView[VersionedResourceOrder, model.VersionedRes
                 resource_id=versioned_resource["resource_id"],
                 resource_version_id=versioned_resource["resource_id"] + f",v={self.version}",
                 id_details=data.Resource.get_details_from_resource_id(versioned_resource["resource_id"]),
-                requires=json.loads(versioned_resource["attributes"]).get("requires", []),  # todo: broken
+                requires=versioned_resource["attributes"].get("requires", []),  # todo: broken
             )
             for versioned_resource in records
         ]
@@ -751,12 +750,10 @@ class CompileReportView(DataView[CompileReportOrder, CompileReport]):
                 version=compile["version"],
                 do_export=compile["do_export"],
                 force_update=compile["force_update"],
-                metadata=json.loads(compile["metadata"]) if compile["metadata"] else {},
-                environment_variables=(
-                    json.loads(compile["used_environment_variables"]) if compile["used_environment_variables"] else {}
-                ),
-                requested_environment_variables=json.loads(compile["requested_environment_variables"]),
-                mergeable_environment_variables=json.loads(compile["mergeable_environment_variables"]),
+                metadata=compile["metadata"] or {},
+                environment_variables=compile["used_environment_variables"] or {},
+                requested_environment_variables=compile["requested_environment_variables"],
+                mergeable_environment_variables=compile["mergeable_environment_variables"],
                 partial=compile["partial"],
                 removed_resource_sets=compile["removed_resource_sets"],
                 exporter_plugin=compile["exporter_plugin"],
@@ -915,7 +912,7 @@ class ResourceHistoryView(DataView[ResourceHistoryOrder, ResourceHistory]):
 
     def construct_dtos(self, records: Sequence[Record]) -> Sequence[ResourceHistory]:
         def get_attributes(record: Record) -> JsonType:
-            attributes: JsonType = json.loads(record["attributes"])
+            attributes = record["attributes"]
             if "version" not in attributes:
                 # Due to a bug, the version field has always been present in the attributes dictionary.
                 # This bug has been fixed in the database. For backwards compatibility reason we here make sure that the
@@ -929,7 +926,7 @@ class ResourceHistoryView(DataView[ResourceHistoryOrder, ResourceHistory]):
                 attribute_hash=record["attribute_hash"],
                 attributes=get_attributes(record),
                 date=record["date"],
-                requires=[Id.parse_id(rid).resource_str() for rid in json.loads(record["attributes"]).get("requires", [])],
+                requires=[Id.parse_id(rid).resource_str() for rid in record["attributes"].get("requires", [])],
             )
             for record in records
         ]
@@ -1018,7 +1015,7 @@ class ResourceLogsView(DataView[ResourceLogOrder, ResourceLog]):
     def construct_dtos(self, records: Sequence[Record]) -> Sequence[ResourceLog]:
         logs = []
         for record in records:
-            message = json.loads(record["unnested_message"])
+            message = record["unnested_message"]
             logs.append(
                 ResourceLog(
                     action_id=record["action_id"],
@@ -1085,7 +1082,7 @@ class FactsView(DataView[FactOrder, Fact]):
                 source=fact["source"],
                 updated=fact["updated"],
                 resource_id=fact["resource_id"],
-                metadata=json.loads(fact["metadata"]) if fact["metadata"] else None,
+                metadata=fact["metadata"],
                 environment=fact["environment"],
             )
             for fact in records
@@ -1203,7 +1200,7 @@ class ParameterView(DataView[ParameterOrder, model.Parameter]):
                 value=parameter["value"],
                 source=parameter["source"],
                 updated=parameter["updated"],
-                metadata=json.loads(parameter["metadata"]) if parameter["metadata"] else None,
+                metadata=parameter["metadata"],
                 environment=parameter["environment"],
             )
             for parameter in records
@@ -1377,7 +1374,7 @@ class DiscoveredResourceView(DataView[DiscoveredResourceOrder, model.DiscoveredR
         return [
             model.DiscoveredResource(
                 discovered_resource_id=res["discovered_resource_id"],
-                values=json.loads(res["values"]),
+                values=res["values"],
                 managed_resource_uri=(
                     f"/api/v2/resource/{urllib.parse.quote(str(res['discovered_resource_id']), safe='')}"
                     if res["managed"]
