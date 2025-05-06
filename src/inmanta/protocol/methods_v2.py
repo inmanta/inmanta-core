7005fadacc755ae759b9aed5acfd37551a67d79c
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-05-06 10:31:19 +0200

    Fix bad merge

--- src/inmanta/protocol/methods_v2.py
+++ src/inmanta/protocol/methods_v2.py
@@ -29,7 +29,6 @@ from inmanta.data.model import DataBaseReport, LinkedDiscoveredResource, PipConf
 from inmanta.protocol import methods
 from inmanta.protocol.common import ReturnValue
 from inmanta.protocol.decorators import typedmethod
-from inmanta.protocol.auth.decorators import auth
 from inmanta.protocol.openapi.model import OpenAPI
 from inmanta.types import PrimitiveTypes, ResourceIdStr
 
@@ -385,7 +384,6 @@ def reserve_version(tid: uuid.UUID) -> int:
     """
 
 
-@auth(auth_label=const.AuthorizationLabel.DOCS_READ, read_only=True)
 @typedmethod(path="/docs", operation="GET", client_types=[ClientType.api], api_version=2, token_param="token")
 def get_api_docs(
     format: Optional[ApiDocsFormat] = ApiDocsFormat.swagger, token: str | None = None
