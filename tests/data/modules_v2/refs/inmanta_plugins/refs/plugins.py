9240bcd87c695c282c82d95f0cadf528b72f6539
Sander Van Balen <git@sandervanbalen.be>
2025-06-25 09:40:07 +0200

    Made some improvements to references and dataclasses (PR #8946)
    # Description
    
    Current status: advanced draft. There are still quite some todo notes, but none of them should have a major impact on the PR, so no seems like a good time for a first review. You can ignore all TODO notes.
    
    In scope for this first stage of review:
    1. conceptual concerns
    2. implementation remarks that are not affected by major conceptual concerns. I believe the implementation (unless covered by a TODO note) is already beyond draft quality, so implementation review is already welcome.
    
    Parts to this PR below. 1 and 2 can be considered more or less independently. The rest should probably be after those two:
    1. plugin boundary validation and custom `to_python`: `inmanta.references` + `inmanta.execute.runtime` + `inmanta.ast.*` minus `inmanta.ast.statements`
    2. proxying and on-access error reporting: `inmanta.execute.proxy`
    3. plugin boundary error reporting: `plugins.py`
    4. `inmanta.resources` ~~-> this one is the most likely to need some further tweaks. There are still some aspects I need to consider here.~~
    5. Everything else
    
    Temporary files in this PR:
    - `changes.tmp` contains a list of conceptual changes in this PR. I collected that to keep track of what is actually changing, with a change entry in mind. I still have to figure out how exactly, but it may be useful to the reviewer.
    - `approach.tmp` contains an overview of the conceptual approach taken when it comes to references and plugin annotations. It is not final, and obviously not in the right place. I aim to convert this to documentation. In its current state, it may provide useful context to the reviewer.
    - The tests (in combination with `changes.tmp`) may help provide motivation for certain code changes.
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [ ] Attached issue to pull request
    - [ ] Changelog entry
    - [ ] Type annotations are present
    - [ ] Code is clear and sufficiently documented
    - [ ] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

---
+++ tests/data/modules_v2/refs/inmanta_plugins/refs/plugins.py
@@ -0,0 +1,261 @@
+from collections.abc import Mapping, Sequence
+from typing import Annotated, Any, Protocol
+
+from inmanta import plugins
+from inmanta.execute.proxy import DynamicProxy
+from inmanta.plugins import plugin, ModelType
+from inmanta.references import Reference
+from inmanta_plugins.refs import dc
+
+
+type Entity = Annotated[Any, ModelType["std::Entity"]]
+
+
+class Container[T](Protocol):
+    value: T
+
+
+type ListContainer = Annotated[Container[Sequence[str]], ModelType["refs::ListContainer"]]
+type DictContainer = Annotated[Container[Mapping[str, object]], ModelType["refs::DictContainer"]]
+
+
+@plugin
+def takes_obj(v: object) -> None: ...
+
+
+@plugin
+def iterates_obj(v: object) -> None:
+    for x in v:  # type: ignore
+        ...
+
+
+@plugin
+def takes_obj_ref(v: object | Reference[object]) -> None: ...
+
+
+@plugin
+def takes_obj_ref_only(v: Reference[object]) -> None:
+    assert isinstance(v, Reference)
+
+
+@plugin
+def takes_str(v: str) -> None: ...
+
+
+@plugin
+def takes_str_ref(v: str | Reference[str]) -> None: ...
+
+
+@plugin
+def takes_complex_union_or_ref(v: int | str | None | Reference[str | None]) -> None:
+    """
+    Takes a complex union that allows references, but not to the exact same types.
+    """
+    ...
+
+
+@plugin
+def takes_union_of_refs(v: Reference[bool] | Reference[str]) -> None: ...
+
+
+@plugin
+def iterates_obj_list(l: Sequence[object]) -> None:
+    for x in l:
+        ...
+    for i in range(len(l)):
+        l[i]
+
+
+@plugin
+def iterates_str_list(l: Sequence[str]) -> None:
+    for x in l:
+        assert isinstance(x, str), type(x)
+    for i in range(len(l)):
+        assert isinstance(l[i], str), type(l[i])
+
+
+@plugin
+def iterates_str_ref_list(l: Sequence[str | Reference[str]]) -> None:
+    for x in l:
+        assert isinstance(x, (str, Reference)), type(x)
+    for i in range(len(l)):
+        assert isinstance(l[i], (str, Reference)), type(l[i])
+
+
+@plugin
+def iterates_object_dict(l: Mapping[str, object]) -> None:
+    for v in l.values():
+        ...
+    for k in l.keys():
+        l[k]
+
+
+@plugin
+def iterates_object_ref_dict(l: Mapping[str, object | Reference[object]]) -> None:
+    for v in l.values():
+        ...
+    for k in l.keys():
+        ...
+
+
+@plugin
+def takes_entity(instance: Entity) -> None:
+    assert isinstance(instance, DynamicProxy), type(instance)
+
+
+@plugin
+def takes_dataclass(instance: dc.DataclassABC) -> None:
+    assert isinstance(instance, (dc.AllRefsDataclass, dc.NoRefsDataclass, dc.MixedRefsDataclass)), type(instance)
+
+
+@plugin
+def takes_all_refs_dataclass(instance: dc.AllRefsDataclass) -> None:
+    assert isinstance(instance, dc.AllRefsDataclass), type(instance)
+    assert isinstance(instance.maybe_ref_value, (Reference, str)), type(instance.maybe_ref_value)
+
+
+@plugin
+def takes_no_refs_dataclass(instance: dc.NoRefsDataclass) -> None:
+    assert isinstance(instance, dc.NoRefsDataclass), type(instance)
+    assert isinstance(instance.non_ref_value, str), type(instance.non_ref_value)
+
+
+@plugin
+def takes_mixed_refs_dataclass(instance: dc.MixedRefsDataclass) -> None:
+    assert isinstance(instance, dc.MixedRefsDataclass), type(instance)
+    assert isinstance(instance.maybe_ref_value, (Reference, str)), type(instance.maybe_ref_value)
+    assert isinstance(instance.non_ref_value, str), type(instance.non_ref_value)
+
+
+@plugin
+def takes_all_refs_dataclass_ref(instance: Reference[dc.AllRefsDataclass]) -> None:
+    assert isinstance(instance, Reference), type(instance)
+
+
+@plugin
+def takes_no_refs_dataclass_ref(instance: Reference[dc.NoRefsDataclass]) -> None:
+    assert isinstance(instance, Reference), type(instance)
+
+
+@plugin
+def takes_mixed_refs_dataclass_ref(instance: Reference[dc.MixedRefsDataclass]) -> None:
+    assert isinstance(instance, Reference), type(instance)
+
+
+@plugin
+def takes_all_refs_dataclass_or_ref(instance: dc.AllRefsDataclass | Reference[dc.AllRefsDataclass]) -> None:
+    assert isinstance(instance, (dc.AllRefsDataclass, Reference)), type(instance)
+
+
+@plugin
+def takes_no_refs_dataclass_or_ref(instance: dc.NoRefsDataclass | Reference[dc.NoRefsDataclass]) -> None:
+    assert isinstance(instance, (dc.NoRefsDataclass, Reference)), type(instance)
+
+
+@plugin
+def takes_mixed_refs_dataclass_or_ref(instance: dc.MixedRefsDataclass | Reference[dc.MixedRefsDataclass]) -> None:
+    assert isinstance(instance, (dc.MixedRefsDataclass, Reference)), type(instance)
+
+
+@plugin
+def read_entity_value(instance: Entity) -> None:
+    assert isinstance(instance.maybe_ref_value, str), type(instance.maybe_ref_value)
+
+
+@plugin
+def read_entity_ref_value(instance: Entity) -> None:
+    plugins.allow_reference_values(instance).maybe_ref_value
+
+
+@plugin
+def read_list_entity_value(instances: Sequence[Entity]) -> None:
+    for instance in instances:
+        assert isinstance(instance.maybe_ref_value, str), type(instance.maybe_ref_value)
+
+
+@plugin
+def read_list_entity_ref_value(instances: Sequence[Entity]) -> None:
+    for instance in instances:
+        plugins.allow_reference_values(instance).maybe_ref_value
+
+
+@plugin
+def read_entity_list_value(instance: ListContainer) -> None:
+    for x in instance.value:
+        assert isinstance(x, str), type(x)
+    for i in range(len(instance.value)):
+        assert isinstance(instance.value[i], str), type(instance.value[i])
+
+
+@plugin
+def read_entity_list_value_or_ref(instance: ListContainer) -> None:
+    with_refs = plugins.allow_reference_values(instance.value)
+    for x in with_refs:
+        ...
+    for i in range(len(instance.value)):
+        with_refs[i]
+
+
+@plugin
+def read_entity_list_value_allow_references_single_level(instance: ListContainer) -> None:
+    """
+    Sets allow_reference_values() on the instance level.
+    Test should assert that this does not allow references on the level below it
+    """
+    with_refs = plugins.allow_reference_values(instance)
+    for x in with_refs.value:
+        assert isinstance(x, str), type(x)
+    for i in range(len(instance.value)):
+        assert isinstance(with_refs.value[i], str), type(with_refs.value[i])
+
+
+@plugin
+def read_entity_list_head(instance: ListContainer) -> None:
+    assert isinstance(instance.value[0], str), type(instance.value[0])
+
+
+@plugin
+def read_entity_dict_value(instance: DictContainer) -> None:
+    for v in instance.value.values():
+        assert isinstance(v, str), type(v)
+    for k in instance.value.keys():
+        assert isinstance(instance.value[k], str), type(instance.value[k])
+
+
+@plugin
+def read_entity_dict_value_or_ref(instance: DictContainer) -> None:
+    with_refs = plugins.allow_reference_values(instance.value)
+    for v in with_refs.values():
+        ...
+    for k in instance.value.keys():
+        with_refs[k]
+
+
+@plugin
+def read_entity_dict_mykey(instance: DictContainer) -> None:
+    assert isinstance(instance.value["mykey"], str), type(instance.value["mykey"])
+
+
+@plugin
+def inheritance_return_specific() -> dc.DataclassABC:
+    return dc.NoRefsDataclass()
+
+
+@plugin
+def inheritance_return_specific_ref() -> Reference[dc.DataclassABC]:
+    return dc.NoRefsDataclassReference()
+
+
+@plugin
+def returns_entity_list(instance: ListContainer) -> list[str]:
+    return instance.value
+
+
+@plugin
+def returns_entity_ref_list(instance: ListContainer) -> list[str | Reference[str]]:
+    return instance.value
+
+
+@plugin
+def allow_references_on_non_proxy() -> None:
+    plugins.allow_reference_values([])
