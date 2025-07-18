0d01a76818438550fc4cdb165b0682c8b398060b
hugo <hugo.thiolliere@inmanta.com>
2025-07-18 07:58:04 +0200

    fix log messages

--- src/inmanta/deploy/timers.py
+++ src/inmanta/deploy/timers.py
@@ -262,7 +262,7 @@ class TimerManager:
 
         async def _action() -> None:
             await self._resource_scheduler.deploy(
-                reason=f"a global deploy was triggered due to cron expression for deploy interval: '{cron_expression}'",
+                reason="a global deploy was triggered by a cron expression",
                 priority=TaskPriority.INTERVAL_DEPLOY,
             )
 
@@ -281,7 +281,7 @@ class TimerManager:
 
         async def _action() -> None:
             await self._resource_scheduler.repair(
-                reason=f"a global repair was triggered due to cron expression for repair interval: '{cron_expression}'",
+                reason="a global repair was triggered by a cron expression",
                 priority=TaskPriority.INTERVAL_REPAIR,
             )
 
@@ -337,14 +337,14 @@ class TimerManager:
         def _setup_repair(repair_interval: int) -> None:
             self.resource_timers[resource].set_timer(
                 when=(last_deployed + timedelta(seconds=repair_interval)),
-                reason=(f"an individual repair was triggered. Previous repair happened more than {repair_interval}s ago"),
+                reason=f"previous repair happened more than {repair_interval}s ago",
                 priority=(TaskPriority.INTERVAL_REPAIR),
             )
 
         def _setup_deploy(deploy_interval: int) -> None:
             self.resource_timers[resource].set_timer(
                 when=(last_deployed + timedelta(seconds=deploy_interval)),
-                reason=(f"an individual deploy was triggered. Previous deploy happened more than {deploy_interval}s ago"),
+                reason=f"previous deploy happened more than {deploy_interval}s ago",
                 priority=(TaskPriority.INTERVAL_DEPLOY),
             )
 
