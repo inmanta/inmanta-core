10ae7c980a9ab521607c80b05d05166a7811e490
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-08-07 08:46:07 +0200

    Remove test case test__a_long_wait_should_not_corrupt_sample because it's unstable (Issue inmanta/inmanta-core#9363, PR #9408)
    # Description
    
    Remove test case `test__a_long_wait_should_not_corrupt_sample` because it's unstable.
    
    closes https://github.com/inmanta/inmanta-core/issues/9363
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation~~
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)

--- tests/pyformance_tests/test_histogram.py
+++ tests/pyformance_tests/test_histogram.py
@@ -63,28 +63,3 @@ def test__a_sample_of_100_from_10():
     assert 0 == hist.get_min()
     assert 4.5 == hist.get_mean()
     assert 9.1666 == approx(hist.get_var(), 0.0001)
-
-
-def test__a_long_wait_should_not_corrupt_sample(clock):
-    hist = Histogram(10, 0.015, clock=clock)
-
-    for i in range(1000):
-        hist.add(1000 + i)
-        clock.add(0.1)
-
-    assert hist.get_snapshot().get_size() == 10
-    for i in hist.sample.get_snapshot().values:
-        assert 1000 <= i and i <= 2000
-
-    clock.add(15 * 3600)  # 15 hours, should trigger rescale
-    hist.add(2000)
-    assert hist.get_snapshot().get_size() == 2
-    for i in hist.sample.get_snapshot().values:
-        assert 1000 <= i and i <= 3000
-
-    for i in range(1000):
-        hist.add(3000 + i)
-        clock.add(0.1)
-    assert hist.get_snapshot().get_size() == 10
-    for i in hist.sample.get_snapshot().values:
-        assert 3000 <= i and i <= 4000
