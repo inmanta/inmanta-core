272deee48f6c0f3cfa1c603d57fd07a77232bfda
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-11-03 16:25:03 +0100

    Add changelog entry

---
+++ changelogs/unreleased/9650-ensure-compatibility-python3-13.py
@@ -0,0 +1,8 @@
+---
+description: "Ensure compatiblity with python3.13"
+issue-nr: 9650
+issue-repo: inmanta-core
+change-type: patch
+destination-branches: [master]
+sections:
+  minor-improvement: "{{description}}"
