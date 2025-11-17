9a2a3427317846c2e86d33819caadefabd766631
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-11-17 12:17:47 +0100

    Fix

--- docs/scorer.js
+++ docs/scorer.js
@@ -1,6 +1,6 @@
 Scorer['score'] = result => {
   const [docName, title, anchor, descr, score, filename] = result
-  if(filename.search(/reference\/modules\/.*\/autodoc\.rst$/) != -1) {
+  if(filename.search(/^reference\/modules\/.*\/autodoc\.rst$/) != -1) {
     // This is documentation that was automatically generated.
     // Put it last in the search results to prevent that a word,
     // that is present in each of this generated pages, clutters
