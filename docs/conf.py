2f10c66800eab0f31a60cf2fc543e6763f9f93ac
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2025-11-17 09:47:43 +0100

    Fix regex

--- docs/conf.py
+++ docs/conf.py
@@ -394,7 +394,7 @@ linkcheck_ignore = [
 linkcheck_anchors_ignore=[
     # Ignore Scroll To Text Fragment anchors, because they are not supposed to be present in the HTML body.
     f"{re.escape(':~:text=')}.*",
-    f"issuecomment-\d+",
+    "issuecomment-[0-9]+",
 ]
 
 graphviz_output_format = "svg"
