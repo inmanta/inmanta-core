26a78f7edf9c64759796e7b0b28bca668c02d44e
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-11-18 08:19:09 +0100

    The search tool on the documentation pages now ranks automatically generated documentation last, to prevent that a match on a common term in these pages clutters the search results. (Issue inmanta/inmanta-core#8022, PR #9738)
    Pull request opened by the merge tool on behalf of #9738

---
+++ docs/scorer.js
@@ -0,0 +1,13 @@
+Scorer['score'] = result => {
+  const [docName, title, anchor, descr, score, filename] = result
+  if(filename.search(/^reference\/modules\/.*\/autodoc\.rst$/) != -1) {
+    // This is documentation that was automatically generated.
+    // Put it last in the search results to prevent that a word,
+    // that is present in each of this generated pages, clutters
+    // the search results.
+    return -5
+  } else {
+    // Rely on the score assigned by the default scoring algorithm.
+    return score
+  }
+};
