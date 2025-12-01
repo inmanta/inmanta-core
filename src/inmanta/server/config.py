c1391bd728b5e5de03a78174f1fe82fdefb51fab
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2025-12-01 10:41:10 +0100

    Improve the documentation of the compatibility.json file. (Issue inmanta/inmanta-core#9749, PR #9786)
    # Description
    
    Improve documentation about the `compatibility.json` file.
    
    closes #9749
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [ ] ~~Type annotations are present~~
    - [ ] ~~Code is clear and sufficiently documented~~
    - [ ] ~~No (preventable) type errors (check using make mypy or make mypy-diff)~~
    - [ ] ~~Sufficient test cases (reproduces the bug/tests the requested feature)~~
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] ~~If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)~~

--- src/inmanta/server/config.py
+++ src/inmanta/server/config.py
@@ -164,10 +164,14 @@ server_compatibility_file = Option(
     "server",
     "compatibility_file",
     None,
-    "Path to the compatibility.json file. The constraints defined in the `python_package_constraints` field will be "
-    "enforced both during project install and during agent install. For more information about this file, please refer to "
-    "the compatibility page in the Inmanta documentation. Leave blank to disable the "
-    "enforcement of the constraints during installation.",
+    """Path to the compatibility.json file. If set, the server will perform the following check:
+       1) The constraints defined in the `python_package_constraints` field of the compatibility file will be
+          enforced both during project install and during agent install.
+       The Inmanta Docker container comes with a compatibility file at /usr/share/inmanta/compatibility/compatibility.json.
+       The container doesn't set this config option by default.
+       For more information about this compatibility file, please refer to the compatibility page in the Inmanta documentation.
+       Leave blank to disable the version compatibility check and the enforcement of the constraints during installation.
+    """,
     is_str_opt,
 )
 
