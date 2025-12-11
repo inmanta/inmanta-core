5b66efdc1b66fdf0da65073c141ea72a1aca13a4
jptrindade <joao.pedro.trindade@inmanta.com>
2025-12-11 10:49:09 +0100

    Add an endpoint to delete a single discovered resource. Add an endpoint to batch delete discovered resources.  (Issue inmanta/inmanta-core#9633, PR #9816)
    # Description
    
    Added endpoints to delete discovered resources
    
    closes #9633
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )
    - [ ] If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)
    
    # Conflicts:
    #	src/inmanta/const.py
    #	src/inmanta/protocol/methods_v2.py

--- src/inmanta/protocol/methods_v2.py
+++ src/inmanta/protocol/methods_v2.py
@@ -1675,6 +1675,46 @@ def discovered_resources_get_batch(
     """
 
 
+@typedmethod(
+    path="/discovered/<discovered_resource_id>",
+    operation="DELETE",
+    arg_options=methods.ENV_OPTS,
+    client_types=[ClientType.api],
+    api_version=2,
+)
+def discovered_resource_delete(
+    tid: uuid.UUID,
+    discovered_resource_id: str,
+) -> None:
+    """
+    Delete a discovered resource.
+
+    :param tid: The id of the environment this resource belongs to
+    :param discovered_resource_id: The id of the discovered_resource
+
+    :raise NotFound: When the referenced discovered resource is not found on the environment
+    """
+
+
+@typedmethod(
+    path="/discovered/",
+    operation="DELETE",
+    arg_options=methods.ENV_OPTS,
+    client_types=[ClientType.api],
+    api_version=2,
+)
+def discovered_resource_delete_batch(tid: uuid.UUID, discovered_resource_ids: Sequence[str]) -> None:
+    """
+    Delete multiple discovered resources.
+    If one or more discovered resources are not found on the environment,
+    they will be ignored and the other discovered resources will be deleted.
+    A 200 will be returned even if any/all provided discovered resources are ignored.
+
+    :param tid: The id of the environment this resource belongs to
+    :param discovered_resource_ids: List of discovered resource ids to delete
+    """
+
+
 @typedmethod(path="/health", operation="GET", client_types=[ClientType.api], enforce_auth=False, api_version=2)
 def health() -> ReturnValue[None]:
     """
