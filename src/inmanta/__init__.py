57a980642b54fd26d450155bce3cd0d4d5f079e6
inmantaci <ci@inmanta.com>
2026-01-30 13:19:13 +0100

    Increment dev version after RC release

--- src/inmanta/__init__.py
+++ src/inmanta/__init__.py
@@ -19,7 +19,7 @@ Contact: code@inmanta.com
 OPA_VERSION = "1.3.0"
 # This version is managed by bumpversion. Should you ever update it manually, make sure to consistently update it everywhere
 # (See the bumpversion.cfg file for relevant locations).
-__version__ = "18.0.0"
+__version__ = "18.0.1"
 
 RUNNING_TESTS = False
 """
