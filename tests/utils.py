656c91ac2b70fb73f78a630749b6ece3d0536193
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2026-02-11 13:45:53 +0100

    Vendor the pip2pi package. (PR #9998)
    The pip2pi package, that we use in our test suite, is no longer maintained. Hence it's no longer compatible with the latest version of setuptools, because it removed pkg_resources. This PR vendors the part of the pip2pi package that we use and makes it compatible again with the latest version of setuptools.
    
    - [ ] ~~Attached issue to pull request~~
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation~~
    - [ ] ~~If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)~~

--- tests/utils.py
+++ tests/utils.py
@@ -62,7 +62,7 @@ from inmanta.server.bootloader import InmantaBootloader
 from inmanta.server.extensions import ProductMetadata
 from inmanta.types import Apireturn, ResourceIdStr, ResourceType
 from inmanta.util import get_compiler_version, hash_file
-from libpip2pi.commands import dir2pi
+from inmanta.vendor import libpip2pi
 
 T = TypeVar("T")
 
@@ -567,7 +567,7 @@ class PipIndex:
         return f"{self.artifact_dir}/simple"
 
     def publish(self) -> None:
-        dir2pi(argv=["dir2pi", self.artifact_dir])
+        libpip2pi.dir2pi(self.artifact_dir)
 
 
 def create_python_package(
