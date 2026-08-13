6f477b790b7b822eb103871709a94b52d764d880
jptrindade <joao.pedro.trindade@inmanta.com>
2026-08-13 11:27:13 +0100

    removed changes

--- src/inmanta/server/config.py
+++ src/inmanta/server/config.py
@@ -153,7 +153,7 @@ server_max_request_body_size = Option(
     "max-request-body-size",
     100 * 1024 * 1024,
     "The maximum size in bytes of the body of a request accepted by the server. Requests with a larger body are "
-    "rejected. This limit is applied to both compressed and decompressed bodies.",
+    "rejected. This limit is applied to the compressed bodies.",
     is_lower_bounded_int(1),
 )
 
