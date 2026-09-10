dbaff75b043f1f81f2980773646c45eaa6d916e9
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2026-09-10 15:12:48 +0200

    Add an optional `resources` argument to the internal `do_dryrun` server-to-agent endpoint, which restricts the dryrun to the given resources. (PR #10801)
    # Description
    
    Add an optional `resources` argument to the internal `do_dryrun` server-to-agent endpoint, which restricts the dryrun to the given resources.
    
    Part of https://github.com/inmanta/inmanta-core/issues/10480
    
    # Self Check:
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Type annotations are present
    - [x] Code is clear and sufficiently documented
    - [x] No (preventable) type errors (check using make mypy or make mypy-diff)
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] End user documentation is included or an issue is created for end-user documentation
    - [ ] ~~If this PR fixes a race condition in the test suite, also push the fix to the relevant stable branche(s) (see [test-fixes](https://internal.inmanta.com/development/core/tasks/build-master.html#test-fixes) for more info)~~

--- src/inmanta/protocol/methods.py
+++ src/inmanta/protocol/methods.py
@@ -639,7 +639,7 @@ def dryrun_update(tid: uuid.UUID, id: uuid.UUID, resource: str, changes: dict):
     client_types=[],
     enforce_auth=False,
 )
-def do_dryrun(tid: uuid.UUID, id: uuid.UUID, agent: str, version: int):
+def do_dryrun(tid: uuid.UUID, id: uuid.UUID, agent: str, version: int, resources: Sequence[ResourceIdStr] | None = None):
     """
     Do a dryrun on an agent
 
@@ -647,6 +647,8 @@ def do_dryrun(tid: uuid.UUID, id: uuid.UUID, agent: str, version: int):
     :param id: The id of the dryrun
     :param agent: The agent to do the dryrun for
     :param version: The version of the model to dryrun
+    :param resources: Optional, the resources to execute the dryrun on. When omitted, all resources of the given
+        version are considered.
     """
 
 
